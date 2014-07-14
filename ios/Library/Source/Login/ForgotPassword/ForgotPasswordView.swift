e2c53366cd9b46323bd44a5875675a251db847e7
JM <jose.navarro@liferay.com>
2014-07-14 23:21:04 +0200

    LMW-12 Added new forgot password widget

---
+++ ios/Library/Source/Login/ForgotPassword/ForgotPasswordView.swift
@@ -0,0 +1,55 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+import UIKit
+
+class ForgotPasswordView: BaseWidgetView, UITextFieldDelegate {
+
+	@IBOutlet var usernameLabel: UILabel
+	@IBOutlet var usernameField: UITextField
+	@IBOutlet var requestButton: UIButton
+
+	func setAuthType(authType: String) {
+		usernameLabel.text = authType
+
+        switch authType {
+		case AuthType.Email.toRaw():
+			usernameField.keyboardType = UIKeyboardType.EmailAddress
+		case AuthType.ScreenName.toRaw():
+			usernameField.keyboardType = UIKeyboardType.ASCIICapable
+		default:
+			break
+		}
+	}
+
+
+	// BaseWidgetView METHODS
+
+
+	override func becomeFirstResponder() -> Bool {
+		return usernameField.becomeFirstResponder()
+	}
+
+
+	// UITextFieldDelegate METHODS
+
+
+	func textFieldShouldReturn(textField: UITextField!) -> Bool {
+		textField.resignFirstResponder()
+
+		requestButton.sendActionsForControlEvents(UIControlEvents.TouchUpInside)
+
+		return true
+	}
+
+}
---
+++ ios/Library/Source/Login/ForgotPassword/ForgotPasswordWidget.swift
@@ -0,0 +1,121 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+import UIKit
+
+@objc protocol ForgotPasswordWidgetDelegate {
+
+	@optional func onForgotPasswordResponse()
+	@optional func onForgotPasswordError(error: NSError)
+
+}
+
+class ForgotPasswordWidget: BaseWidget {
+
+	@IBOutlet var delegate: ForgotPasswordWidgetDelegate?
+    
+	typealias ResetClosureType = (String, LRMobilewidgetsService_v6201, (NSError)->()) -> (Void)
+
+	let resetClosures = [
+		AuthType.Email.toRaw(): resetPasswordWithEmail,
+		AuthType.ScreenName.toRaw(): resetPasswordWithScreenName]
+
+	var resetClosure: ResetClosureType?
+
+	func setAuthType(authType:AuthType) {
+		forgotPasswordView().setAuthType(authType.toRaw())
+
+		resetClosure = resetClosures[authType.toRaw()]
+	}
+
+
+    // BaseWidget METHODS
+
+
+	override func onCreate() {
+		setAuthType(AuthType.Email)
+
+		forgotPasswordView().usernameField.text = LiferayContext.instance.currentSession?.username
+	}
+
+	override func onCustomAction(actionName: String, sender: UIControl) {
+		sendForgotPasswordRequest(forgotPasswordView().usernameField.text)
+	}
+
+	override func onServerError(error: NSError) {
+		//TODO get server error message
+		delegate?.onForgotPasswordError?(error)
+
+		hideHUDWithMessage("Error reseting password!", details: nil)
+	}
+
+	override func onServerResult(result: AnyObject!) {
+		if let correct = result as? Bool {
+			if correct {
+				delegate?.onForgotPasswordResponse?()
+			}
+			else {
+				onServerError(NSError(domain: "LiferayWidgets", code: -1, userInfo: nil))
+				return
+			}
+		}
+		hideHUDWithMessage("New password request sent!", details: nil)
+    }
+
+
+	// PRIVATE METHODS
+
+
+	func forgotPasswordView() -> ForgotPasswordView {
+		return widgetView as ForgotPasswordView
+	}
+
+	func sendForgotPasswordRequest(username:String) {
+		showHUDWithMessage("Sending password request...", details:"Wait few seconds...")
+
+		let session = LiferayContext.instance.createSession("test", password: "test")
+		session.callback = self
+
+		let service = LRMobilewidgetsService_v6201(session: session)
+
+		let companyId: CLongLong = (LiferayContext.instance.companyId as NSNumber).longLongValue
+
+		resetClosure!(username, LRMobilewidgetsService_v6201(session: session)) {error in
+			self.onFailure(error)
+		}
+	}
+}
+
+func resetPasswordWithEmail(email:String, service:LRMobilewidgetsService_v6201, onError:(NSError)->()) {
+	let companyId = (LiferayContext.instance.companyId as NSNumber).longLongValue
+
+	var outError: NSError?
+
+	service.resetPasswordByEmailAddressWithCompanyId(companyId, emailAddress: email, error: &outError)
+
+	if let error = outError {
+		onError(error)
+	}
+}
+
+func resetPasswordWithScreenName(screenName:String, service:LRMobilewidgetsService_v6201, onError:(NSError)->()) {
+	let companyId = (LiferayContext.instance.companyId as NSNumber).longLongValue
+
+	var outError: NSError?
+
+	service.resetPasswordByScreenNameWithCompanyId(companyId, screenName: screenName, error: &outError)
+
+	if let error = outError {
+		onError(error)
+	}
+}
