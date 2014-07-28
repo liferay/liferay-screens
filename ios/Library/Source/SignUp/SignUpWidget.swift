227761b44f2dbb3d55b4cfc2d171db08d54dec47
JM <jose.navarro@liferay.com>
2014-07-28 14:36:26 +0200

    LMW-13 Added remote service call for sign up

--- ios/Library/Source/SignUp/SignUpWidget.swift
+++ ios/Library/Source/SignUp/SignUpWidget.swift
@@ -27,6 +27,8 @@ import UIKit
 
     @IBOutlet var delegate: SignUpWidgetDelegate?
     
+	private var creatingUsername: String?
+	private var creatingPassword: String?
 
     // BaseWidget METHODS
     
@@ -38,7 +40,9 @@ import UIKit
     override func onServerError(error: NSError) {
 		delegate?.onSignUpError?(error)
 
-        hideHUDWithMessage("Error signing in!", details: nil)
+        hideHUDWithMessage("Error signing up!", details: nil)
+
+		signUpView().signUpButton!.enabled = true
     }
     
 	override func onServerResult(result: [String:AnyObject]) {
@@ -48,6 +52,7 @@ import UIKit
 		LRSession.removeStoredCredential()
 
 		hideHUDWithMessage("Sign up completed", details: nil)
+		signUpView().signUpButton!.enabled = true
     }
     
 
@@ -67,7 +72,72 @@ import UIKit
 		let session = LiferayContext.instance.createSession(creatorUsername!, password: creatorPassword!)
 		session.callback = self
 
-		// TODO service call
+		let service = LRUserService_v62(session: session)
+
+		var outError: NSError?
+
+		let companyId: Int64 = (LiferayContext.instance.companyId as NSNumber).longLongValue
+
+		// user name
+		switch authType {
+		case AuthType.Email:
+			creatingUsername = signUpView().emailAddressField!.text
+		case AuthType.Screenname:
+			creatingUsername = signUpView().screenNameField!.text
+		case AuthType.UserId:
+			println("ERROR: sign Up with User id is not supported")
+		default:
+			break;
+		}
+
+		// password
+		creatingPassword = _optionalFieldValue(signUpView().passwordField);
+		let autoPassword = (creatingPassword == "")
+
+		// screen name
+		let screenName = _optionalFieldValue(signUpView().screenNameField);
+		let autoScreenName = (screenName == "")
+
+		// names
+		let firstName = _optionalFieldValue(signUpView().firstNameField);
+		let middleName = _optionalFieldValue(signUpView().middleNameField);
+		let lastName = _optionalFieldValue(signUpView().lastNameField);
+
+		let emptyDict = []
+
+
+		service.addUserWithCompanyId(companyId,
+			autoPassword: autoPassword, password1: password, password2: password,
+			autoScreenName: autoScreenName, screenName: screenName,
+			emailAddress: signUpView().emailAddressField!.text,
+			facebookId: 0, openId: "",
+			locale: NSLocale.currentLocaleString(),
+			firstName: firstName, middleName: middleName, lastName: lastName,
+			prefixId: 0, suffixId: 0,
+			male: true,
+			birthdayMonth: 1, birthdayDay: 1, birthdayYear: 1970,
+			jobTitle: _optionalFieldValue(signUpView().jobTitleField),
+			groupIds: [LiferayContext.instance.groupId],
+			organizationIds: emptyDict,
+			roleIds: emptyDict,
+			userGroupIds: emptyDict,
+			sendEmail: true,
+			serviceContext: nil, error: &outError)
+
+		if let error = outError {
+			onFailure(error)
+		}
+		else {
+			signUpView().signUpButton!.enabled = false
+		}
+	}
+
+	private func _optionalFieldValue(textField: UITextField?) -> String {
+		if textField && textField!.text != "" {
+			return textField!.text
+		}
+
+		return ""
 	}
 
 }
