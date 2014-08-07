1c9229982550248f72b0888572c98f10c584c037
JM <jose.navarro@liferay.com>
2014-08-07 17:00:43 +0200

    LMW-35 Added new entity base class for all DDL elements

---
+++ ios/Library/Source/DDLForm/DDLElement.swift
@@ -0,0 +1,76 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+import Foundation
+
+public enum DDLElementDataType: String {
+
+	case Boolean = "boolean"
+	case Unsupported = ""
+
+	public static func from(#xmlElement:SMXMLElement) -> DDLElementDataType {
+		return fromRaw(xmlElement.attributeNamed("dataType") ?? "") ?? .Unsupported
+	}
+}
+
+
+public enum DDLElementType: String {
+
+	case Checkbox = "checkbox"
+	case Unsupported = ""
+
+	public static func from(#xmlElement:SMXMLElement) -> DDLElementType {
+		return fromRaw(xmlElement.attributeNamed("type") ?? "") ?? .Unsupported
+	}
+
+}
+
+
+public class DDLElement {
+
+	private(set) var dataType:DDLElementDataType
+	private(set) var type:DDLElementType
+
+	private(set) var name:String
+
+	private(set) var label:String
+	private(set) var tip:String
+	private(set) var predefinedValue:AnyObject?
+
+	private(set) var readOnly:Bool
+	private(set) var repeatable:Bool
+	private(set) var required:Bool
+
+	private(set) var showLabel:Bool 	// Makes sense in mobile??
+	private(set) var width:Int? 		// Makes sense in mobile??
+
+	public init(attributes:[String:String], localized:[String:String]) {
+		dataType = DDLElementDataType.fromRaw(attributes["dataType"] ?? "") ?? .Unsupported
+		type = DDLElementType.fromRaw(attributes["type"] ?? "") ?? .Unsupported
+		name = attributes["name"] ?? ""
+
+		readOnly = Bool.from(string: attributes["readOnly"] ?? "false")
+		repeatable = Bool.from(string: attributes["repeatable"] ?? "false")
+		required = Bool.from(string: attributes["required"] ?? "true")
+		showLabel = Bool.from(string: attributes["showLabel"] ?? "true")
+
+		label = localized["label"] ?? ""
+		tip = localized["tip"] ?? ""
+		predefinedValue = convert(fromString:localized["predefinedValue"])
+	}
+
+	internal func convert(fromString value:String?) -> AnyObject? {
+		return value
+	}
+
+}
