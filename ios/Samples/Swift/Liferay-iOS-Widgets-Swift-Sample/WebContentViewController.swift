02b7266d97836f4159c5b188f6a1afc3ee4693bc
JM <jose.navarro@liferay.com>
2014-09-08 16:20:13 +0200

    LMW-55 Add new screen to sample project

---
+++ ios/Samples/Swift/Liferay-iOS-Widgets-Swift-Sample/WebContentViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WebContentViewController.swift
+//  Liferay-iOS-Widgets-Swift-Sample
+//
+//  Created by jmWork on 08/09/14.
+//  Copyright (c) 2014 Liferay. All rights reserved.
+//
+
+import UIKit
+
+class WebContentViewController: UIViewController, WebContentWidgetDelegate {
+
+	@IBOutlet weak var widget: WebContentWidget?
+
+	override func viewDidLoad() {
+		widget!.delegate = self
+	}
+
+	@IBAction func loadButtonAction(sender: AnyObject) {
+		LiferayContext.instance.createSession("jose.navarro@liferay.com", password: "jm")
+		widget!.loadWebContent()
+	}
+
+}
