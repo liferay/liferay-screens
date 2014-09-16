942d1c38bf823358c34d0e12777816f266943d9c
JM <jose.navarro@liferay.com>
2014-09-16 18:01:45 +0200

    LMW-70 Class moved to inner type into an extension

--- ios/Library/Source/AssetList/AssetListWidget.swift
+++ ios/Library/Source/AssetList/AssetListWidget.swift
@@ -239,42 +239,5 @@ import UIKit
 	internal func assetListView() -> AssetListView {
 		return widgetView as AssetListView
 	}
-}
-
-internal class LoadPageOperation: NSObject, LRCallback {
-
-	internal var onOperationSuccess: ((Int, [[String:AnyObject]], Int) -> ())?
-	internal var onOperationFailure: ((Int, NSError) -> ())?
-
-	private let page:Int
-
-	internal init(page:Int) {
-		self.page = page
-	}
-
-	internal func onFailure(error: NSError!) {
-		onOperationFailure?(page, error)
-	}
-
-	internal func onSuccess(result: AnyObject!) {
-		if let responses = result as? NSArray {
-			if let entriesResponse = responses.firstObject as? NSArray {
-				if let countResponse = responses.objectAtIndex(1) as? NSNumber {
-					onOperationSuccess?(page,
-						entriesResponse as [[String:AnyObject]],
-						countResponse as Int)
-				}
-				else {
-					//TODO error handling
-				}
-			}
-			else {
-				//TODO error handling
-			}
-		}
-		else {
-			//TODO error handling
-		}
-	}
 
 }
