09472d1fc0602ee4c149aa571821dbf6f8a4ec66
JM <jose.navarro@liferay.com>
2014-09-29 13:32:50 +0200

    LMW-83 NoAutoclose case always be closed on touch

--- ios/Library/Source/Base/BaseWidget+progress.swift
+++ ios/Library/Source/Base/BaseWidget+progress.swift
@@ -64,7 +64,7 @@ extension BaseWidget {
 
 	public enum CloseMode {
 
-		case NoAutoclose(Bool)
+		case NoAutoclose
 		case AutocloseDelayed(Double, Bool)
 		case AutocloseComputedDelay(Bool)
 
@@ -76,8 +76,8 @@ extension BaseWidget {
 					result = touchClose
 				case .AutocloseDelayed(let delay, let touchClose):
 					result = touchClose
-				case .NoAutoclose(let touchClose):
-					result = touchClose
+				case .NoAutoclose:
+					result = true
 			}
 
 			return result
@@ -121,7 +121,7 @@ extension BaseWidget {
 	*/
 	public func showHUDWithMessage(message:String?,
 			details:String? = nil,
-			closeMode:CloseMode = .NoAutoclose(false),
+			closeMode:CloseMode = .NoAutoclose,
 			spinnerMode:SpinnerMode = .IndeterminateSpinner) {
 
 		synchronized(BaseWidgetHudLock) {
