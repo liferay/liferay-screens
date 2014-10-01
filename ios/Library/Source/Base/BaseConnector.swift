6363391b6c8a6385ba4342eb3f3ae3c441cdf163
Jorge Ferrer <jorge.ferrer@liferay.com>
2014-10-01 10:14:19 +0200

    LMW-82 Add missing copyright headers

--- ios/Library/Source/Base/BaseConnector.swift
+++ ios/Library/Source/Base/BaseConnector.swift
@@ -1,11 +1,16 @@
-//
-//  BaseConnector.swift
-//  Liferay-iOS-Widgets-Swift-Sample
-//
-//  Created by jmWork on 28/09/14.
-//  Copyright (c) 2014 Liferay. All rights reserved.
-//
-
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
 import UIKit
 
 
