40fd0cfa7e937b0de4d99544c0545abb0ff61e47
JM <jose.navarro@liferay.com>
2014-12-04 11:21:47 +0100

    LMW-121 New method to delay actions

--- ios/Library/Source/Extensions/Global.swift
+++ ios/Library/Source/Extensions/Global.swift
@@ -60,6 +60,12 @@ func synchronized(lock:AnyObject, closure: Void -> Void) {
 }
 
 
+func delayed(delay: NSTimeInterval, block: dispatch_block_t) {
+    let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
+    dispatch_after(time, dispatch_get_main_queue(), block)
+}
+
+
 func LocalizedString(tableName: String, key: String, obj: AnyObject) -> String {
 	let bundle = NSBundle(forClass:obj.dynamicType)
 
