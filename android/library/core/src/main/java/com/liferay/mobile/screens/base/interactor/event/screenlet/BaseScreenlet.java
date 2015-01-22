0140c82825e5b0dcb2ed4b83b94b16de368ec31e
Silvio Santos <silvio.santos@liferay.com>
2015-01-22 17:32:33 +0100

    Base screenlet core classes

---
+++ android/library/core/src/main/java/com/liferay/mobile/screens/base/interactor/event/screenlet/BaseScreenlet.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.screens.base.interactor.event.screenlet;
+
+import android.content.Context;
+
+import android.util.AttributeSet;
+
+import android.view.View;
+
+import android.widget.FrameLayout;
+
+import com.liferay.mobile.screens.base.interactor.event.interactor.Interactor;
+
+/**
+ * @author Silvio Santos
+ */
+public abstract class BaseScreenlet<V, I extends Interactor>
+	extends FrameLayout {
+
+	public BaseScreenlet(Context context) {
+		this(context, null);
+	}
+
+	public BaseScreenlet(Context context, AttributeSet attributes) {
+		this(context, attributes, 0);
+	}
+
+	public BaseScreenlet(
+		Context context, AttributeSet attributes, int defaultStyle) {
+
+		super(context, attributes, defaultStyle);
+
+		_screenletView = createScreenletView(context, attributes);
+
+		addView(_screenletView);
+	}
+
+	public I getInteractor() {
+		return _interactor;
+	}
+
+	public void setInteractor(I interactor) {
+		_interactor = interactor;
+	}
+
+	protected abstract View createScreenletView(
+		Context context, AttributeSet attributes);
+
+	protected View getScreenletView() {
+		return _screenletView;
+	}
+
+	@Override
+	protected void onAttachedToWindow() {
+		super.onAttachedToWindow();
+
+		_interactor.onScreenletAttachted(this);
+	}
+
+	@Override
+	protected void onDetachedFromWindow() {
+		super.onDetachedFromWindow();
+
+		_interactor.onScreenletDetached(this);
+	}
+
+	protected abstract void onUserAction(int id);
+
+	private I _interactor;
+	private View _screenletView;
+
+}
