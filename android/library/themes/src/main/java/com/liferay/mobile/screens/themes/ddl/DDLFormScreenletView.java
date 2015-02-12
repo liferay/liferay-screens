ba23b89b3d316bd70ae47bd968e3452093eb1a91
Silvio Santos <silvio.santos@liferay.com>
2015-02-12 10:38:20 +0100

    LMW-174 Add submit button to form

--- android/library/themes/src/main/java/com/liferay/mobile/screens/themes/ddl/DDLFormScreenletView.java
+++ android/library/themes/src/main/java/com/liferay/mobile/screens/themes/ddl/DDLFormScreenletView.java
@@ -22,6 +22,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
@@ -145,6 +146,13 @@ public class DDLFormScreenletView
 
 		_fieldsContainerView = (ViewGroup)
 			findViewById(R.id.ddlfields_container);
+
+		Button submitButton = (Button) findViewById(R.id.submit);
+		submitButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view) {
 	}
 
 	private ViewGroup _fieldsContainerView;
