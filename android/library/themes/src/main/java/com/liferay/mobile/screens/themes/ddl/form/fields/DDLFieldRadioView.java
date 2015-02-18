aa72ee89fbff66012511b94ad7a5c47439a358fb
JM <jose.navarro@liferay.com>
2015-02-18 17:02:09 +0100

    LMW-168 Add Radio field view

---
+++ android/library/themes/src/main/java/com/liferay/mobile/screens/themes/ddl/form/fields/DDLFieldRadioView.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.screens.themes.ddl.form.fields;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.liferay.mobile.screens.ddl.form.view.DDLFieldViewModel;
+import com.liferay.mobile.screens.ddl.model.StringWithOptionsField;
+import com.liferay.mobile.screens.themes.R;
+
+import java.util.List;
+
+/**
+ * @author Jose Manuel Navarro
+ */
+public class DDLFieldRadioView extends RadioGroup
+	implements DDLFieldViewModel<StringWithOptionsField>, CompoundButton.OnCheckedChangeListener {
+
+	public DDLFieldRadioView(Context context) {
+		super(context);
+	}
+
+	public DDLFieldRadioView(Context context, AttributeSet attributes) {
+		super(context, attributes);
+	}
+
+	@Override
+	public StringWithOptionsField getField() {
+		return _field;
+	}
+
+	@Override
+	public void setField(StringWithOptionsField field) {
+		_field = field;
+
+		if (_field.isShowLabel()) {
+			TextView label = (TextView) findViewById(R.id.label);
+
+			label.setText(field.getLabel());
+			label.setVisibility(VISIBLE);
+		}
+
+		LayoutParams layoutParams = new LayoutParams(
+			ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+		List<StringWithOptionsField.Option> availableOptions = field.getAvailableOptions();
+
+		for (int i = 0; i < availableOptions.size(); ++i) {
+			StringWithOptionsField.Option opt = availableOptions.get(i);
+
+			RadioButton radioButton = new RadioButton(getContext());
+
+			radioButton.setLayoutParams(layoutParams);
+			radioButton.setText(opt.label);
+			radioButton.setTag(opt);
+			radioButton.setOnCheckedChangeListener(this);
+
+			// TODO find an id to avoid collisions
+			radioButton.setId(10 + i);
+
+			addView(radioButton);
+		}
+
+		refresh();
+	}
+
+	@Override
+	public void refresh() {
+		List<StringWithOptionsField.Option> selectedOptions = _field.getCurrentValue();
+
+		if (selectedOptions != null) {
+			for (StringWithOptionsField.Option opt : selectedOptions) {
+				RadioButton radioButton = (RadioButton) findViewWithTag(opt);
+
+				if (radioButton != null) {
+					radioButton.setChecked(true);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void onPostValidation(boolean valid) {
+		if (_field.isShowLabel()) {
+			TextView label = (TextView) findViewById(R.id.label);
+
+			label.setError(getContext().getString(R.string.required_value));
+		}
+		else {
+			List<StringWithOptionsField.Option> availableOptions = _field.getAvailableOptions();
+			StringWithOptionsField.Option opt = availableOptions.get(0);
+			RadioButton radioButton = (RadioButton) findViewWithTag(opt);
+			if (radioButton != null) {
+				radioButton.setError(getContext().getString(R.string.required_value));
+			}
+		}
+	}
+
+	@Override
+	protected void onFinishInflate() {
+		super.onFinishInflate();
+
+		setSaveEnabled(false);
+	}
+
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		RadioButton radioButton = (RadioButton) buttonView;
+
+		StringWithOptionsField.Option opt = (StringWithOptionsField.Option) radioButton.getTag();
+
+		_field.selectOption(opt);
+	}
+
+	private StringWithOptionsField _field;
+
+}
