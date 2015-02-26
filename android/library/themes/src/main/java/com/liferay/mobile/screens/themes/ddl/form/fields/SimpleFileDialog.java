5f9807b5eafe2da87b5eebc82007dc074434b283
Javier Gamarra <nhpatt@gmail.com>
2015-02-26 15:02:29 +0100

    LMW-162 use custom dialog in file selection dialog

--- android/library/themes/src/main/java/com/liferay/mobile/screens/themes/ddl/form/fields/SimpleFileDialog.java
+++ android/library/themes/src/main/java/com/liferay/mobile/screens/themes/ddl/form/fields/SimpleFileDialog.java
@@ -3,14 +3,15 @@ package com.liferay.mobile.screens.themes.ddl.form.fields;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Environment;
-import android.view.Gravity;
-import android.view.ViewGroup.LayoutParams;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import com.liferay.mobile.screens.themes.R;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,6 +29,7 @@ public class SimpleFileDialog {
 	private String currentDir = "";
 
 	private static final String SD_DIRECTORY = Environment.getExternalStorageDirectory().getAbsolutePath();
+	private static final String SD_PATH = checkDir(SD_DIRECTORY);
 
 	public interface SimpleFileDialogListener {
 		public void onFileChosen(String path);
@@ -39,83 +41,81 @@ public class SimpleFileDialog {
 	}
 
 	public void chooseFile() {
-
-		final String sdPath = checkDir(SD_DIRECTORY);
 		final String defaultPath = checkDir("");
 
-		if (sdPath != null) {
-			currentDir = sdPath;
+		if (SD_PATH != null) {
+			currentDir = SD_PATH;
 		} else if (defaultPath != null) {
 			currentDir = defaultPath;
 		} else {
 			return;
 		}
 
-		final List<String> subdirs = getDirectories(currentDir, sdPath);
-
-		final ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.select_dialog_item, android.R.id.text1, subdirs);
-
-
-		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
-		TextView title = new TextView(context);
-		title.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-		title.setText("Open:");
-		title.setGravity(Gravity.CENTER_VERTICAL);
-		title.setTextColor(context.getResources().getColor(android.R.color.white));
-		LinearLayout titleLayout1 = new LinearLayout(context);
-		titleLayout1.setOrientation(LinearLayout.VERTICAL);
-		titleLayout1.addView(title);
-		LinearLayout titleLayout = new LinearLayout(context);
-		titleLayout.setOrientation(LinearLayout.VERTICAL);
-		final EditText inputText = new EditText(context);
-		inputText.setText("");
-		titleLayout.addView(inputText);
-		dialogBuilder.setView(titleLayout);
-		//dialogBuilder.setCustomTitle(R.layout.)
-		dialogBuilder.setCustomTitle(titleLayout1);
-		dialogBuilder.setCancelable(false);
-		dialogBuilder.setPositiveButton("OK", new OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				currentFile = inputText.getText() + "";
-				listener.onFileChosen(currentDir + "/" + currentFile);
-			}
-		}).setNegativeButton("Cancel", null);
-
-		class SimpleFileDialogOnClickListener implements DialogInterface.OnClickListener {
-			public void onClick(DialogInterface dialog, int item) {
-				String m_dir_old = currentDir;
-				String sel = "" + ((AlertDialog) dialog).getListView().getAdapter().getItem(item);
-				if (sel.charAt(sel.length() - 1) == '/') sel = sel.substring(0, sel.length() - 1);
-
-				// Navigate into the sub-directory
-				if (sel.equals("..")) {
-					currentDir = currentDir.substring(0, currentDir.lastIndexOf("/"));
-				} else {
-					currentDir += "/" + sel;
-				}
-				currentFile = "";
-
-				if ((new File(currentDir).isFile())) // If the selection is a regular currentFile
-				{
-					currentDir = m_dir_old;
-					currentFile = sel;
-				}
-
-				subdirs.clear();
-				subdirs.addAll(getDirectories(currentDir, sdPath));
-				adapter.notifyDataSetChanged();
-				inputText.setText(currentFile);
-			}
-		}
-
-		dialogBuilder.setSingleChoiceItems(adapter, -1, new SimpleFileDialogOnClickListener());
-
-		// Show directory chooser dialog
-		dialogBuilder.create().show();
+		AlertDialog dialog = createDialog();
+		dialog.show();
 	}
 
-	public String checkDir(String directory) {
+	private AlertDialog createDialog() {
+		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService
+				(Context.LAYOUT_INFLATER_SERVICE);
+
+		View view = inflater.inflate(R.layout.default_custom_dialog, null);
+		dialogBuilder.setView(view);
+
+		dialogBuilder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface d, int arg1) {
+				d.cancel();
+			}
+		});
+
+		final EditText editText = (EditText) view.findViewById(R.id.default_dialog_edit_text);
+		dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface d, int arg1) {
+				currentFile = editText.getText().toString();
+				listener.onFileChosen(currentDir + "/" + currentFile);
+			}
+		});
+
+		final List<String> files = getFileEntries(currentDir);
+		final ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.select_dialog_item, android.R.id.text1, files);
+		ListView listView = (ListView) view.findViewById(R.id.default_list);
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+				String selection = files.get(position);
+
+				String newPath = currentDir;
+				if ("..".equals(selection)) {
+					newPath = currentDir.substring(0, currentDir.lastIndexOf("/"));
+				} else if (selection.indexOf('/') != -1) {
+					newPath += "/" + selection.substring(0, selection.length() - 1);
+				} else {
+					newPath += "/" + selection;
+				}
+
+				currentFile = "";
+				if (new File(newPath).isFile()) {
+					currentFile = selection;
+				} else {
+					currentDir = newPath;
+				}
+
+				files.clear();
+				files.addAll(getFileEntries(currentDir));
+				adapter.notifyDataSetChanged();
+				editText.setText(currentFile);
+			}
+		});
+
+		return dialogBuilder.create();
+	}
+
+	public static String checkDir(String directory) {
 		try {
 			File file = new File(directory);
 			return file.exists() && file.isDirectory() ? file.getCanonicalPath() : null;
@@ -124,35 +124,31 @@ public class SimpleFileDialog {
 		}
 	}
 
-	private List<String> getDirectories(String dir, String sdPath) {
-		List<String> dirs = new ArrayList<String>();
-		try {
-			File dirFile = new File(dir);
+	private List<String> getFileEntries(String directory) {
+		List<String> entries = new ArrayList<String>();
 
-			// if directory is not the base sd card directory add ".." for going up one directory
-			if (!currentDir.equals(sdPath)) dirs.add("..");
-
-			if (!dirFile.exists() || !dirFile.isDirectory()) {
-				return dirs;
-			}
-
-			for (File file : dirFile.listFiles()) {
-				if (file.isDirectory()) {
-					// Add "/" to directory names to identify them in the list
-					dirs.add(file.getName() + "/");
-				} else {
-					// Add currentFile names to the list if we are doing a currentFile save or currentFile open operation
-					dirs.add(file.getName());
-				}
-			}
-		} catch (Exception e) {
+		if (!currentDir.equals(SD_PATH)) {
+			entries.add("..");
 		}
 
-		Collections.sort(dirs, new Comparator<String>() {
+		File dirFile = new File(directory);
+		if (!dirFile.exists() || !dirFile.isDirectory()) {
+			return entries;
+		}
+
+		for (File file : dirFile.listFiles()) {
+			if (file.isDirectory()) {
+				entries.add(file.getName() + "/");
+			} else {
+				entries.add(file.getName());
+			}
+		}
+
+		Collections.sort(entries, new Comparator<String>() {
 			public int compare(String o1, String o2) {
 				return o1.compareTo(o2);
 			}
 		});
-		return dirs;
+		return entries;
 	}
 }
