8a3f4c614ea91e1e18ca99b9bc403025705eec89
JM <jose.navarro@liferay.com>
2015-02-27 14:18:12 +0100

    LMW-162 generateScreenletId goes back to BaseScreenlet

--- android/library/core/src/main/java/com/liferay/mobile/screens/base/BaseScreenlet.java
+++ android/library/core/src/main/java/com/liferay/mobile/screens/base/BaseScreenlet.java
@@ -15,20 +15,15 @@
 package com.liferay.mobile.screens.base;
 
 import android.content.Context;
-
 import android.os.Bundle;
 import android.os.Parcelable;
-
 import android.util.AttributeSet;
-
 import android.view.View;
-
 import android.widget.FrameLayout;
 
 import com.liferay.mobile.screens.base.interactor.Interactor;
 import com.liferay.mobile.screens.base.view.BaseViewModel;
 import com.liferay.mobile.screens.context.LiferayScreensContext;
-import com.liferay.mobile.screens.util.ViewUtil;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -62,7 +57,7 @@ public abstract class BaseScreenlet<V extends BaseViewModel, I extends Interacto
 
 	public int getScreenletId() {
 		if (_screenletId == 0) {
-			_screenletId = ViewUtil._generateUniqueId();
+			_screenletId = _generateScreenletId();
 		}
 
 		return _screenletId;
@@ -186,10 +181,25 @@ public abstract class BaseScreenlet<V extends BaseViewModel, I extends Interacto
 	protected abstract void onUserAction(String userActionName, I interactor, Object... args);
 
 	private static final String _STATE_SCREENLET_ID = "basescreenlet-screenletId";
-
 	private static final String _STATE_SUPER = "basescreenlet-super";
-
 	private static final String _STATE_INTERACTORS = "basescreenlet-interactors";
+
+	private static int _generateScreenletId() {
+
+		// This implementation is copied from View.generateViewId() method We
+		// cannot rely on that method because it's introduced in API Level 17
+
+		while (true) {
+			final int result = sNextId.get();
+			int newValue = result + 1;
+			if (newValue > 0x00FFFFFF) newValue = 1;
+			if (sNextId.compareAndSet(result, newValue)) {
+				return result;
+			}
+		}
+	}
+
+	private static final AtomicInteger sNextId = new AtomicInteger(1);
 
 	private Map<String,I> _interactors = new HashMap<>();
 	private int _screenletId;
