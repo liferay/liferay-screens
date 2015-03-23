0edcee1beb4a394d25830573cbb6fd4f61d74386
Javier Gamarra <nhpatt@gmail.com>
2015-03-23 09:31:38 +0100

    LMW-224 refactor main activity

--- android/samples/bankofwesteros/src/main/java/com/liferay/mobile/screens/bankofwesteros/MainActivity.java
+++ android/samples/bankofwesteros/src/main/java/com/liferay/mobile/screens/bankofwesteros/MainActivity.java
@@ -31,61 +31,58 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
 		setContentView(R.layout.main);
 
-		findViewById(R.id.sign_in_button).setOnClickListener(this);
-		findViewById(R.id.sign_up_button).setOnClickListener(this);
+		findViewById(R.id.sign_in_view).setOnClickListener(this);
+		findViewById(R.id.sign_up_view).setOnClickListener(this);
 		findViewById(R.id.background).setOnClickListener(this);
 
 	}
 
 	@Override
 	public void onClick(final View view) {
-		if (view.getId() == R.id.sign_in_button) {
-			TransitionManager.beginDelayedTransition((android.view.ViewGroup) view);
-			RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-			layoutParams.height = 1050;
-			layoutParams.setMargins(0, -1150, 0, 0);
-			view.setLayoutParams(layoutParams);
+		if (view.getId() == R.id.sign_in_view) {
+			LinearLayout signInView = (LinearLayout) view;
 
+			TransitionManager.beginDelayedTransition(signInView);
+			changeMarginAndHeight(signInView, 0, -1150, 0, 0, 1050);
 
-			LinearLayout signUpView = (LinearLayout) findViewById(R.id.sign_up_button);
+			LinearLayout signUpView = (LinearLayout) findViewById(R.id.sign_up_view);
 			View signUpScreenlet = findViewById(R.id.signup_screenlet);
 			signUpScreenlet.setVisibility(View.GONE);
-			TransitionManager.beginDelayedTransition((android.view.ViewGroup) signUpView);
 
+			TransitionManager.beginDelayedTransition(signUpView);
 		}
-		else if (view.getId() == R.id.sign_up_button) {
-
-
-			LinearLayout signInView = (LinearLayout) findViewById(R.id.sign_in_button);
-
+		else if (view.getId() == R.id.sign_up_view) {
 
 			LinearLayout signUpView = (LinearLayout) view;
 			View signUpScreenlet = findViewById(R.id.signup_screenlet);
 			signUpScreenlet.setVisibility(View.VISIBLE);
-			TransitionManager.beginDelayedTransition((android.view.ViewGroup) signUpView);
 
-			changeMargin(signUpView);
+			TransitionManager.beginDelayedTransition(signUpView);
+			changeMargin(signUpView, 0, 0, 0, 0);
 
-			RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) signInView.getLayoutParams();
-			layoutParams.setMargins(20, 20, 20, 0);
-			signInView.setLayoutParams(layoutParams);
-			TransitionManager.beginDelayedTransition((android.view.ViewGroup) signInView);
+			LinearLayout signInView = (LinearLayout) findViewById(R.id.sign_in_view);
+			changeMargin(signInView, 20, 20, 20, 0);
 
+			TransitionManager.beginDelayedTransition(signInView);
 		}
 		else {
+			LinearLayout signin = (LinearLayout) findViewById(R.id.sign_in_view);
 
-			View signin = findViewById(R.id.sign_in_button);
-			TransitionManager.beginDelayedTransition((android.view.ViewGroup) signin);
-			RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) signin.getLayoutParams();
-			layoutParams.height = 200;
-			layoutParams.setMargins(0, 0, 0, 0);
-			signin.setLayoutParams(layoutParams);
+			TransitionManager.beginDelayedTransition(signin);
+			changeMarginAndHeight(signin, 0, 0, 0, 0, 200);
 		}
 	}
 
-	private void changeMargin(View view) {
+	private void changeMargin(View view, int marginLeft, int marginTop, int marginRight, int marginBottom) {
+		changeMarginAndHeight(view, marginLeft, marginTop, marginRight, marginBottom, null);
+	}
+
+	private void changeMarginAndHeight(View view, int marginLeft, int marginTop, int marginRight, int marginBottom, Integer height) {
 		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-		layoutParams.setMargins(0, 0, 0, 0);
+		if (height != null) {
+			layoutParams.height = height;
+		}
+		layoutParams.setMargins(marginLeft, marginTop, marginRight, marginBottom);
 		view.setLayoutParams(layoutParams);
 	}
 
