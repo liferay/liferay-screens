3dd9ba783e240063255027eba36c584da88bc012
JM <jose.navarro@liferay.com>
2015-03-23 12:49:20 +0100

    LMW-223 New interactor base class for interactions using ServerOperation classes

---
+++ ios/Library/Core/Base/ServerOperationInteractor.swift
@@ -0,0 +1,45 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+import UIKit
+
+class ServerOperationInteractor: Interactor {
+
+	override internal func start() -> Bool {
+		var result = false
+
+		if let operation = createOperation() {
+			result = operation.validateAndEnqueue() {
+				self.completedOperation(operation)
+
+				if let error = $0.lastError {
+					self.callOnFailure(error)
+				}
+				else {
+					self.callOnSuccess()
+				}
+			}
+		}
+
+		return result
+	}
+
+
+	func createOperation() -> ServerOperation? {
+		return nil
+	}
+
+	func completedOperation(op: ServerOperation) {
+	}
+
+}
