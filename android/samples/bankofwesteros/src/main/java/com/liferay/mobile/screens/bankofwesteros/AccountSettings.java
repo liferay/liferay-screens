9fd6e42abb23bd618bd39208ae1fb6efecd4f850
Javier Gamarra <nhpatt@gmail.com>
2015-03-25 09:38:44 +0100

    LMW-224 first draft of account settings screen

---
+++ android/samples/bankofwesteros/src/main/java/com/liferay/mobile/screens/bankofwesteros/AccountSettings.java
@@ -0,0 +1,16 @@
+package com.liferay.mobile.screens.bankofwesteros;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Javier Gamarra
+ */
+public class AccountSettings extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.account_settings);
+	}
+}
