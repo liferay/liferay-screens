f85513014580234db7be490b4dc0e09287555ee0
JM <jose.navarro@liferay.com>
2015-03-30 18:12:47 +0200

    LMW-48 New framework project from scratch

---
+++ ios/Framework/LiferayScreens.h
@@ -0,0 +1,19 @@
+//
+//  LiferayScreens.h
+//  LiferayScreens
+//
+//  Created by jmWork on 30/03/15.
+//  Copyright (c) 2015 Liferay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiferayScreens.
+FOUNDATION_EXPORT double LiferayScreensVersionNumber;
+
+//! Project version string for LiferayScreens.
+FOUNDATION_EXPORT const unsigned char LiferayScreensVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiferayScreens/PublicHeader.h>
+
+
