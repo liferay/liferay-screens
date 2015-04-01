c9440013be1a78d58d1049121606442181cf0360
JM <jose.navarro@liferay.com>
2015-04-01 17:43:21 +0200

    LMW-48 Add mock objects for credentials storage

--- ios/Framework/Tests/MockHelper.swift
+++ ios/Framework/Tests/MockHelper.swift
@@ -66,3 +66,85 @@ class Liferay62MockServer : MockServer, StubResponses {
 
 }
 
+
+class KeyChainStorageMock : KeyChainStorage {
+
+	var calledSetData = false
+	var calledRemoveItemForKey = false
+	var calledDataForKey = false
+
+	var hasData: Bool = true
+
+	func setData(data: NSData, forKey key: String) -> Bool {
+		calledSetData = true
+		return true
+	}
+
+	func dataForKey(key: String) -> NSData! {
+		calledDataForKey = true
+
+		let encodedData = NSKeyedArchiver.archivedDataWithRootObject(["k":"v"])
+		return hasData ? encodedData : nil
+	}
+
+	func removeItemForKey(key: String) -> Bool {
+		calledRemoveItemForKey = true
+		return true
+	}
+
+}
+
+
+class CredentialStorageMock : CredentialStorage {
+
+	var calledGetCredential = false
+	var calledGetServer = false
+	var calledGetSession = false
+	var calledRemoveCredential = false
+	var calledStoreCredentialForServer = false
+
+	var hasData: Bool = true
+
+	let credential = NSURLCredential(
+			user: "username",
+			password: "password",
+			persistence: NSURLCredentialPersistence.None)
+
+	func getCredential() -> NSURLCredential! {
+		calledGetCredential = true
+		return hasData ? self.credential : nil
+	}
+
+	func getServer() -> String! {
+		calledGetServer = true
+		return hasData ? LiferayServerContext.server : nil
+	}
+
+	func getSession() -> LRSession! {
+		calledGetSession = true
+
+		if hasData {
+			return LRSession(
+					server: LiferayServerContext.server,
+					authentication: LRBasicAuthentication(
+						username: credential.user,
+						password: credential.password))
+		}
+
+		return nil
+	}
+
+	func removeCredential() {
+		calledRemoveCredential = true
+	}
+
+	func storeCredentialForServer(server: String!,
+			username: String!,
+			password: String!)
+			-> NSURLCredential! {
+
+		calledStoreCredentialForServer = true
+		return hasData ? self.credential : nil
+	}
+
+}
