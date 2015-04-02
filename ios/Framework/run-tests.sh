43164e1f8b6643dfb2018ea2738d0210d569bc42
JM <jose.navarro@liferay.com>
2015-04-03 00:21:34 +0200

    LMW-48 Change simulator to iOS 8.1

--- ios/Framework/run-tests.sh
+++ ios/Framework/run-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xctool clean test -workspace LiferayScreens.xcworkspace -scheme LiferayScreens -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+xctool clean test -workspace LiferayScreens.xcworkspace -scheme LiferayScreens -sdk iphonesimulator8.1 ONLY_ACTIVE_ARCH=NO
