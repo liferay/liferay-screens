0cb6bca3851e1fcb9dc6036ffc4d7f2b48367407
Javier Gamarra <nhpatt@gmail.com>
2015-04-06 18:41:11 +0200

    LMW-224 cleaner implementation of account settings, moving user update call to another class

--- android/samples/bankofwesteros/src/main/java/com/liferay/mobile/screens/bankofwesteros/AccountSettings.java
+++ android/samples/bankofwesteros/src/main/java/com/liferay/mobile/screens/bankofwesteros/AccountSettings.java
@@ -7,9 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.android.task.callback.typed.JSONObjectAsyncTaskCallback;
-import com.liferay.mobile.android.v62.user.UserService;
 import com.liferay.mobile.screens.bankofwesteros.views.WesterosCrouton;
 import com.liferay.mobile.screens.context.SessionContext;
 import com.liferay.mobile.screens.context.User;
@@ -17,7 +15,6 @@ import com.liferay.mobile.screens.userportrait.UserPortraitListener;
 import com.liferay.mobile.screens.userportrait.UserPortraitScreenlet;
 import com.liferay.mobile.screens.util.LiferayLogger;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -32,7 +29,6 @@ public class AccountSettings extends Activity implements View.OnClickListener, U
 
 		findViewById(R.id.arrow_back_to_issues).setOnClickListener(this);
 		findViewById(R.id.account_settings_save).setOnClickListener(this);
-		findViewById(R.id.replace_portrait_image).setOnClickListener(this);
 
 		User user = SessionContext.getLoggedUser();
 		_firstName = (EditText) findViewById(R.id.first_name);
@@ -57,101 +53,15 @@ public class AccountSettings extends Activity implements View.OnClickListener, U
 			case R.id.arrow_back_to_issues:
 				finish();
 				break;
-			case R.id.replace_portrait_image:
-				_userPortraitScreenlet.updatePortraitImage();
-
-				break;
 		}
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == Activity.RESULT_OK) {
-			_userPortraitScreenlet.upload(data);
+			_userPortraitScreenlet.upload(requestCode, data);
 		}
 	}
-
-
-	private void saveUser() {
-		String firstName = _firstName.getText().toString();
-		String lastName = _lastName.getText().toString();
-		String emailAddress = _emailAddress.getText().toString();
-		String newPassword = _password.getText().toString();
-
-		if (_password.getText().toString().isEmpty()) {
-			setError(_password);
-			return;
-		}
-
-		if (SessionContext.getAuthentication().getPassword().equals(_password.getText().toString())) {
-			setError(_password);
-			WesterosCrouton.error(this, "Password should be different", null);
-			return;
-		}
-
-		saveUser(firstName, lastName, emailAddress, newPassword);
-
-
-	}
-
-	private void saveUser(String firstName, String lastName, final String emailAddress, final String newPassword) {
-		Session sessionFromCurrentSession = SessionContext.createSessionFromCurrentSession();
-		sessionFromCurrentSession.setCallback(new JSONObjectAsyncTaskCallback() {
-			@Override
-			public void onSuccess(JSONObject result) {
-
-				SessionContext.createSession(emailAddress, newPassword);
-
-				clearError(_password);
-				WesterosCrouton.info(AccountSettings.this, "User updated");
-			}
-
-			@Override
-			public void onFailure(Exception exception) {
-				LiferayLogger.e("error", exception);
-				WesterosCrouton.error(AccountSettings.this, "Error updating user", exception);
-			}
-		});
-
-		User user = SessionContext.getLoggedUser();
-
-		UserService userService = new UserService(sessionFromCurrentSession);
-		try {
-			JSONArray array = new JSONArray();
-
-
-			userService.updateUser(user.getInt("userId"), "test2", newPassword, newPassword, false,
-				user.getString("reminderQueryQuestion"), user.getString("reminderQueryAnswer"),
-				user.getString("screenName"), emailAddress, user.getInt("facebookId"),
-				user.getString("openId"), user.getString("languageId"), "", user.getString("greeting"),
-				user.getString("comments"), firstName, user.getString("middleName"),
-				lastName, 0, 0, true, 1, 1, 1900, user.getString("emailAddress"),
-				"", "", "", "", "", "", "", "", "", user.getString("jobTitle"),
-				array, array, array, array, array, null);
-		}
-		catch (Exception e) {
-			LiferayLogger.e("Error updating user", e);
-			WesterosCrouton.error(this, "Error updating user", e);
-		}
-	}
-
-
-	private void setError(EditText editText) {
-		editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_warning, 0);
-		editText.setBackground(getDrawable(R.drawable.westeros_warning_edit_text_white_drawable));
-	}
-
-	private void clearError(EditText editText) {
-		editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-		editText.setBackground(getDrawable(R.drawable.westeros_edit_text_selector));
-	}
-
-	private EditText _firstName;
-	private EditText _lastName;
-	private EditText _emailAddress;
-	private EditText _password;
-
-	private UserPortraitScreenlet _userPortraitScreenlet;
 
 	@Override
 	public Bitmap onUserPortraitLoadReceived(UserPortraitScreenlet source, Bitmap bitmap) {
@@ -172,5 +82,60 @@ public class AccountSettings extends Activity implements View.OnClickListener, U
 	public void onUserPortraitUploadFailure(UserPortraitScreenlet source, Exception e) {
 		WesterosCrouton.error(AccountSettings.this, "Error updating portrait", e);
 	}
+
+
+	private void saveUser() {
+		final String firstName = _firstName.getText().toString();
+		final String lastName = _lastName.getText().toString();
+		final String emailAddress = _emailAddress.getText().toString();
+		final String newPassword = _password.getText().toString();
+
+		if (_password.getText().toString().isEmpty()) {
+			setError(_password);
+			return;
+		}
+
+		if (SessionContext.getAuthentication().getPassword().equals(_password.getText().toString())) {
+			setError(_password);
+			WesterosCrouton.error(this, "Password should be different", null);
+			return;
+		}
+
+		UpdateUserInteractorImpl updateUserInteractor = new UpdateUserInteractorImpl();
+		updateUserInteractor.saveUser(firstName, lastName, emailAddress, newPassword, new JSONObjectAsyncTaskCallback() {
+			@Override
+			public void onSuccess(JSONObject result) {
+
+				SessionContext.createSession(emailAddress, newPassword);
+
+				clearError(_password);
+				WesterosCrouton.info(AccountSettings.this, "User updated");
+			}
+
+			@Override
+			public void onFailure(Exception exception) {
+				LiferayLogger.e("error", exception);
+				WesterosCrouton.error(AccountSettings.this, "Error updating user", exception);
+			}
+		});
+	}
+
+
+	private void setError(EditText editText) {
+		editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_warning, 0);
+		editText.setBackground(getDrawable(R.drawable.westeros_warning_edit_text_white_drawable));
+	}
+
+	private void clearError(EditText editText) {
+		editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+		editText.setBackground(getDrawable(R.drawable.westeros_edit_text_selector));
+	}
+
+	private EditText _firstName;
+	private EditText _lastName;
+	private EditText _emailAddress;
+	private EditText _password;
+
+	private UserPortraitScreenlet _userPortraitScreenlet;
 
 }
