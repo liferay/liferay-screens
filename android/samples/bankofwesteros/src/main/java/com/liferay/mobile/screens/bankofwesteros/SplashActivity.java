c6b781ac9489bbef0a3db055d503298b05d5ddda
Javier Gamarra <nhpatt@gmail.com>
2015-04-06 11:42:48 +0200

    LMW-224 source formatting

--- android/samples/bankofwesteros/src/main/java/com/liferay/mobile/screens/bankofwesteros/SplashActivity.java
+++ android/samples/bankofwesteros/src/main/java/com/liferay/mobile/screens/bankofwesteros/SplashActivity.java
@@ -2,8 +2,12 @@ package com.liferay.mobile.screens.bankofwesteros;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+
+import static com.liferay.mobile.screens.bankofwesteros.TourActivity.TOUR_VISITED;
+import static com.liferay.mobile.screens.bankofwesteros.TourActivity.WESTEROS_PREFERENCES;
 
 /**
  * @author Javier Gamarra
@@ -15,14 +19,23 @@ public class SplashActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
 
-		//todo change to back and screen orientation aware
+		final Class destination = getDestinationActivity();
+
+		//TODO change to back and screen orientation aware
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				startActivity(new Intent(SplashActivity.this, TourActivity.class));
+				startActivity(new Intent(SplashActivity.this, destination));
 			}
 		}, DELAY_MILLIS);
 
+	}
+
+	private Class getDestinationActivity() {
+		SharedPreferences preferences = getSharedPreferences(WESTEROS_PREFERENCES, MODE_PRIVATE);
+		boolean tourVisited = preferences.contains(TOUR_VISITED) &&
+			preferences.getBoolean(TOUR_VISITED, false);
+		return tourVisited ? MainActivity.class : TourActivity.class;
 	}
 
 	private static final int DELAY_MILLIS = 2000;
