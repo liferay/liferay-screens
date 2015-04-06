f9e54cc626b15968d9c6dc75099e22567ce8c2cd
Javier Gamarra <nhpatt@gmail.com>
2015-04-06 12:22:42 +0200

    LMW-224 changing crouton implementation

--- android/samples/bankofwesteros/src/main/java/com/liferay/mobile/screens/bankofwesteros/views/WesterosCrouton.java
+++ android/samples/bankofwesteros/src/main/java/com/liferay/mobile/screens/bankofwesteros/views/WesterosCrouton.java
@@ -12,27 +12,31 @@
  * details.
  */
 
-package com.liferay.mobile.screens.viewsets.defaultviews;
+package com.liferay.mobile.screens.bankofwesteros.views;
 
 import android.app.Activity;
 import android.content.Context;
-import android.view.ContextThemeWrapper;
 
-import com.liferay.mobile.screens.viewsets.R;
+import com.liferay.mobile.screens.bankofwesteros.R;
+import com.liferay.mobile.screens.viewsets.material.MaterialCrouton;
 
+import de.keyboardsurfer.android.widget.crouton.Configuration;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
 
 /**
  * @author Javier Gamarra
  */
-public class DefaultCrouton {
+public class WesterosCrouton extends MaterialCrouton {
 
-	public static int POSITION = android.R.id.content;
-	public static Style INFO = new Style.Builder().setBackgroundColor(R.color.default_light_blue).build();
-	public static Style ALERT  = new Style.Builder().setBackgroundColor(R.color.default_red).build();
+	public static final int HEIGHT = 250;
 
-	//FIXME this is wrong
+	public static Configuration CONFIGURATION = new Configuration.Builder().setInAnimation(R.anim.slide_up).setOutAnimation(R.anim.slide_down).build();
+	public static Style INFO = new Style.Builder().setHeight(HEIGHT).setBackgroundColor(R.color.material_primary_dark).setConfiguration(CONFIGURATION).build();
+	public static Style ALERT = new Style.Builder().setHeight(HEIGHT).setBackgroundColor(R.color.westeros_yellow).setConfiguration(CONFIGURATION).build();
+
+	public static int POSITION = R.id.crouton_view_anchor;
+
 	public static void error(Context context, String message, Exception e) {
 		String error = message;
 		if (e instanceof IllegalArgumentException) {
@@ -40,36 +44,12 @@ public class DefaultCrouton {
 			error = e.getMessage();
 		}
 		Activity activity = getContextFromActivity(context);
-
-
 		Crouton.showText(activity, error, ALERT, POSITION);
 	}
 
 	public static void info(Context context, String message) {
 		Activity activity = getContextFromActivity(context);
 		Crouton.showText(activity, message, INFO, POSITION);
-	}
-
-	protected static Activity getContextFromActivity(Context context) {
-		if (context instanceof Activity) {
-			return (Activity) context;
-		}
-		else {
-			Context baseContext = ((ContextThemeWrapper) context).getBaseContext();
-			return (Activity) baseContext;
-		}
-	}
-
-	public static void initDefaultCroutonStyles(Style info, Style alert, Integer position) {
-		if (info != null) {
-			INFO = info;
-		}
-		if (alert != null) {
-			ALERT = alert;
-		}
-		if (position != null) {
-			POSITION = position;
-		}
 	}
 
 }
