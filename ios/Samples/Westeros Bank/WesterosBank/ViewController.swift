7bf9e1412433613acb28d950db860001f865d094
JM <jose.navarro@liferay.com>
2015-04-21 11:50:55 +0200

    LMW-232 Base project

---
+++ ios/Samples/Westeros Bank/WesterosBank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WesterosBank
+//
+//  Created by jmWork on 21/04/15.
+//  Copyright (c) 2015 Liferay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
