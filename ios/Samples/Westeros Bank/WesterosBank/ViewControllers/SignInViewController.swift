4192ea64469a3992d4f9b05e6ccc0c22debac572
JM <jose.navarro@liferay.com>
2015-04-21 18:55:04 +0200

    LMW-232 Add SignUp and SignIn controllers

---
+++ ios/Samples/Westeros Bank/WesterosBank/ViewControllers/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignInViewController.swift
+//  WesterosBank
+//
+//  Created by jmWork on 21/04/15.
+//  Copyright (c) 2015 Liferay. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+	}
+
+	convenience init() {
+		self.init(nibName:"SignInViewController", bundle:nil)
+	}
+
+	required init(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+
+}
---
+++ ios/Samples/Westeros Bank/WesterosBank/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignUpViewController.swift
+//  WesterosBank
+//
+//  Created by jmWork on 21/04/15.
+//  Copyright (c) 2015 Liferay. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+	}
+
+	convenience init() {
+		self.init(nibName:"SignUpViewController", bundle:nil)
+	}
+
+	required init(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+
+}
