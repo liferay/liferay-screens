c62c7e607e9bfd248ce6c9791878053b6298ae07
JM <jose.navarro@liferay.com>
2015-04-29 17:48:29 +0200

    LMW-232 Use SignUpScreenlet and UserPortraitScreeninet in user profile

--- ios/Samples/WesterosBank/WesterosBank/ViewControllers/AccountSettingsViewController.swift
+++ ios/Samples/WesterosBank/WesterosBank/ViewControllers/AccountSettingsViewController.swift
@@ -7,11 +7,61 @@
 //
 
 import UIKit
+import LiferayScreens
 
-class AccountSettingsViewController: UIViewController {
+
+class AccountSettingsViewController: UIViewController, UserPortraitScreenletDelegate, SignUpScreenletDelegate {
+
+	@IBOutlet weak var portraitScreenlet: UserPortraitScreenlet!
+	@IBOutlet weak var signUpScreenlet: SignUpScreenlet!
+
+	private var initialSignUpPosition: CGFloat?
 
 	@IBAction func saveAction(sender: AnyObject) {
 		self.dismissViewControllerAnimated(true, completion: nil)
+	}
+
+	override func viewDidLoad() {
+		portraitScreenlet.delegate = self
+		signUpScreenlet.delegate = self
+
+		initialSignUpPosition = signUpScreenlet.frame.origin.y
+	}
+
+	override func viewWillAppear(animated: Bool) {
+		NSNotificationCenter.defaultCenter().addObserver(self,
+				selector: "showKeyboard:",
+				name: UIKeyboardWillChangeFrameNotification,
+				object: nil)
+		NSNotificationCenter.defaultCenter().addObserver(self,
+				selector: "hideKeyboard:",
+				name: UIKeyboardWillHideNotification,
+				object: nil)
+	}
+
+	override func viewWillDisappear(animated: Bool) {
+		NSNotificationCenter.defaultCenter().removeObserver(self,
+				name: UIKeyboardWillHideNotification,
+				object: nil)
+		NSNotificationCenter.defaultCenter().removeObserver(self,
+				name: UIKeyboardWillChangeFrameNotification,
+				object: nil)
+	}
+
+	func showKeyboard(notif: NSNotification) {
+		UIView.animateWithDuration(1.0) {
+			self.signUpScreenlet.frame = CGRectMake(
+					self.signUpScreenlet.frame.origin.x, 0,
+					self.signUpScreenlet.frame.size)
+		}
+	}
+
+	func hideKeyboard(notif: NSNotification) {
+		UIView.animateWithDuration(1.0) {
+			self.signUpScreenlet.frame = CGRectMake(
+					self.signUpScreenlet.frame.origin.x, self.initialSignUpPosition!,
+					self.signUpScreenlet.frame.size)
+		}
 	}
 
 }
