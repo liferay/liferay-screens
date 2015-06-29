2f7c8a7ded036fd972a484959c8e66cf38fac76d
JM <jose.navarro@liferay.com>
2015-06-29 18:29:19 +0200

    LSR-279 SF

--- ios/Samples/WesterosBank/Theme/Common/BorderedTextField.swift
+++ ios/Samples/WesterosBank/Theme/Common/BorderedTextField.swift
@@ -1,16 +1,27 @@
-//
-//  BorderedTextField.swift
-//  WesterosBank
-//
-//  Created by jmWork on 26/04/15.
-//  Copyright (c) 2015 Liferay. All rights reserved.
-//
-
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
 import UIKit
 
 class BorderedTextField: UITextField, UITextFieldDelegate {
 
+	@IBOutlet var chainedDelegate: UITextFieldDelegate?
+
+	@IBInspectable var borderWidth: CGFloat = 1.0
+	@IBInspectable var unfocusedBorderWidth: CGFloat = 0.0
+
 	@IBInspectable var borderColor: UIColor? = UIColor.clearColor()
+	@IBInspectable var unfocusedBorderColor: UIColor? = UIColor.clearColor()
 	@IBInspectable var focusedColor: UIColor? = UIColor.clearColor()
 	@IBInspectable var unfocusedColor: UIColor? = UIColor.clearColor()
 
