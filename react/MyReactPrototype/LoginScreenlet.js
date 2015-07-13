393335a54b17c8f958293720ff203260703507bf
JM <jose.navarro@liferay.com>
2015-07-13 10:09:14 +0200

    Add JS counterpart

---
+++ react/MyReactPrototype/LoginScreenlet.js
@@ -0,0 +1,30 @@
+var React = require('react-native');
+var { requireNativeComponent } = React;
+
+var NativeLoginScreenlet = requireNativeComponent('NativeLoginScreenlet', LoginScreenlet);
+
+
+class LoginScreenlet extends React.Component {
+  constructor() {
+  	super();
+    this._onSubmitEditing = this._onSubmitEditing.bind(this);
+  }
+  _onSubmitEditing(event: Event) {
+    if (!this.props.onLoginSuccess) {
+      return;
+    }
+    this.props.onLoginSuccess(event.nativeEvent.attributes);
+  }
+  render() {
+    return <NativeLoginScreenlet {...this.props} onSubmitEditing={this._onSubmitEditing} />;
+  }
+}
+
+LoginScreenlet.propTypes = {
+//  saveCredentials: React.PropTypes.boolean,
+  
+  onLoginSuccess: React.PropTypes.func,  
+};
+
+
+module.exports = LoginScreenlet;
---
+++ react/MyReactPrototype/index.ios.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var LoginScreenlet = require('./LoginScreenlet');
+
+
+var MyReactPrototype = React.createClass({
+  loginSucceeded(attributes) {
+    console.log("Login done! " + attributes);
+  },
+
+  render: function() {
+    return (
+      <View style={styles.container}>
+      	<LoginScreenlet style={styles.login} onLoginSuccess={this.loginSucceeded} />
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  login: {
+    flex: 1,
+    width: 300,
+    height: 300,
+    marginTop:50,
+  },
+});
+
+AppRegistry.registerComponent('MyReactPrototype', () => MyReactPrototype);
