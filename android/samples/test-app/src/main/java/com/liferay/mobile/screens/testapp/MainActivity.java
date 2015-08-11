54c6a3a97992ff454fe32a041ccabdf4b02ee5f8
Javier Gamarra <javier.gamarra@liferay.com>
2015-08-11 12:17:07 +0200

    LSR-294 rename web view to better match the screenlet name

--- android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/MainActivity.java
+++ android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/MainActivity.java
@@ -16,17 +16,17 @@ public class MainActivity extends ThemeActivity implements View.OnClickListener 
 		super.onCreate(state);
 		setContentView(R.layout.activity_main);
 
-		findViewById(R.id.login).setOnClickListener(this);
-		findViewById(R.id.asset_list).setOnClickListener(this);
 		findViewById(R.id.ddl_form).setOnClickListener(this);
 		findViewById(R.id.ddl_list).setOnClickListener(this);
+		findViewById(R.id.asset_list).setOnClickListener(this);
 		findViewById(R.id.sign_up).setOnClickListener(this);
 		findViewById(R.id.forgot_password).setOnClickListener(this);
 		findViewById(R.id.user_portrait).setOnClickListener(this);
-		findViewById(R.id.web_view).setOnClickListener(this);
+		findViewById(R.id.web_content_display).setOnClickListener(this);
 		findViewById(R.id.add_bookmark).setOnClickListener(this);
 		findViewById(R.id.login_full_screenlet).setOnClickListener(this);
 		findViewById(R.id.change_theme).setOnClickListener(this);
+		findViewById(R.id.login).setOnClickListener(this);
 	}
 
 	@Override
@@ -51,8 +51,8 @@ public class MainActivity extends ThemeActivity implements View.OnClickListener 
 			case R.id.user_portrait:
 				DefaultAnimation.startActivityWithAnimation(this, getIntentWithTheme(UserPortraitActivity.class));
 				break;
-			case R.id.web_view:
-				DefaultAnimation.startActivityWithAnimation(this, getIntentWithTheme(WebViewActivity.class));
+			case R.id.web_content_display:
+				DefaultAnimation.startActivityWithAnimation(this, getIntentWithTheme(WebContentDisplayActivity.class));
 				break;
 			case R.id.add_bookmark:
 				DefaultAnimation.startActivityWithAnimation(this, getIntentWithTheme(AddBookmarkActivity.class));
