221c9785ad45f899089d548b11d34a4824c62a84
Javier Gamarra <javier.gamarra@liferay.com>
2015-08-24 18:38:47 +0200

    LSR-623 Search strategy for user portrait

---
+++ android/library/core/src/main/java/com/liferay/mobile/screens/cache/userportrait/UserPortraitSearch.java
@@ -0,0 +1,51 @@
+package com.liferay.mobile.screens.cache.userportrait;
+
+import com.liferay.mobile.screens.cache.CachedContent;
+import com.liferay.mobile.screens.cache.sql.CacheSQL;
+import com.liferay.mobile.screens.cache.sql.CacheStrategy;
+import com.pushtorefresh.storio.sqlite.operations.put.PutResult;
+
+import java.util.List;
+
+import static com.liferay.mobile.screens.cache.sql.CacheSQL.queryGet;
+import static com.liferay.mobile.screens.cache.sql.StorIOSQLite.queryDelete;
+
+/**
+ * @author Javier Gamarra
+ */
+public class UserPortraitSearch implements CacheStrategy {
+
+	@Override
+	public UserPortraitCache getById(String id) {
+		List query = queryGet(UserPortraitCache.class, UserPortraitCache.TABLE_NAME, WHERE_BY_ID, id);
+		return query.isEmpty() ? null : (UserPortraitCache) query.get(0);
+	}
+
+	@Override
+	public List<UserPortraitSearch> get(String query, Object... args) {
+		return queryGet(UserPortraitCache.class, UserPortraitCache.TABLE_NAME, query, args);
+	}
+
+	@Override
+	public Object set(CachedContent object) {
+		PutResult putResult = (PutResult) CacheSQL.querySet(object.getTableCache());
+		if (putResult.wasInserted() || putResult.wasUpdated()) {
+			return CacheSQL.querySet(object);
+		}
+		else {
+			return putResult;
+		}
+	}
+
+	@Override
+	public void clear() {
+		queryDelete(UserPortraitCache.TABLE_NAME, null);
+	}
+
+	@Override
+	public void clear(String id) {
+		queryDelete(UserPortraitCache.TABLE_NAME, WHERE_BY_ID, id);
+	}
+
+	private static final String WHERE_BY_ID = UserPortraitCache.USER_ID + " = ?";
+}
