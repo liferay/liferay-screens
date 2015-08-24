e182cb15fdf44237eed22a23938432376418d5c4
Javier Gamarra <javier.gamarra@liferay.com>
2015-08-24 18:50:56 +0200

    LSR-623 ddl load record interactor

--- android/library/core/src/main/java/com/liferay/mobile/screens/ddl/form/interactor/recordload/DDLFormLoadRecordInteractorImpl.java
+++ android/library/core/src/main/java/com/liferay/mobile/screens/ddl/form/interactor/recordload/DDLFormLoadRecordInteractorImpl.java
@@ -15,7 +15,14 @@
 package com.liferay.mobile.screens.ddl.form.interactor.recordload;
 
 import com.liferay.mobile.android.service.Session;
-import com.liferay.mobile.screens.base.interactor.BaseRemoteInteractor;
+import com.liferay.mobile.screens.base.interactor.BaseCachedRemoteInteractor;
+import com.liferay.mobile.screens.base.interactor.CacheCallback;
+import com.liferay.mobile.screens.cache.Cache;
+import com.liferay.mobile.screens.cache.CachePolicy;
+import com.liferay.mobile.screens.cache.DefaultCachedType;
+import com.liferay.mobile.screens.cache.OfflinePolicy;
+import com.liferay.mobile.screens.cache.ddl.form.DDLRecordCache;
+import com.liferay.mobile.screens.cache.sql.CacheSQL;
 import com.liferay.mobile.screens.context.SessionContext;
 import com.liferay.mobile.screens.ddl.form.DDLFormListener;
 import com.liferay.mobile.screens.ddl.model.Record;
@@ -28,18 +35,29 @@ import org.json.JSONException;
  * @author Jose Manuel Navarro
  */
 public class DDLFormLoadRecordInteractorImpl
-	extends BaseRemoteInteractor<DDLFormListener> implements DDLFormLoadRecordInteractor {
+	extends BaseCachedRemoteInteractor<DDLFormListener, DDLFormLoadRecordEvent>
+	implements DDLFormLoadRecordInteractor {
 
-	public DDLFormLoadRecordInteractorImpl(int targetScreenletId) {
-		super(targetScreenletId);
+	public DDLFormLoadRecordInteractorImpl(int targetScreenletId, CachePolicy cachePolicy) {
+		super(targetScreenletId, cachePolicy, OfflinePolicy.NO_OFFLINE);
 	}
 
 	@Override
-	public void loadRecord(Record record) throws Exception {
+	public void loadRecord(final Record record) throws Exception {
 		validate(record);
 
-		getDDLRecordService(record).getDdlRecord(
-			record.getRecordId(), record.getLocale().toString());
+		loadWithCache(new CacheCallback() {
+			@Override
+			public void loadOnline() throws Exception {
+				getDDLRecordService(record).getDdlRecord(
+					record.getRecordId(), record.getLocale().toString());
+			}
+
+			@Override
+			public boolean retrieveFromCache() throws Exception {
+				return loadFromCache(record);
+			}
+		});
 	}
 
 	public void onEvent(DDLFormLoadRecordEvent event) {
@@ -51,6 +69,9 @@ public class DDLFormLoadRecordInteractorImpl
 			getListener().onDDLFormRecordLoadFailed(event.getException());
 		}
 		else {
+
+			storeToCache(event);
+
 			try {
 				event.getRecord().setValues(JSONUtil.toMap(event.getJSONObject()));
 
@@ -78,6 +99,23 @@ public class DDLFormLoadRecordInteractorImpl
 		if (record.getRecordId() <= 0) {
 			throw new IllegalArgumentException("Record's recordId cannot be 0 or negative");
 		}
+	}
+
+	private boolean loadFromCache(Record record) throws JSONException {
+		Cache cache = CacheSQL.getInstance();
+		DDLRecordCache recordCache = (DDLRecordCache) cache.getById(
+			DefaultCachedType.DDL_RECORD, String.valueOf(record.getRecordId()));
+
+		if (recordCache != null) {
+			onEvent(new DDLFormLoadRecordEvent(getTargetScreenletId(), recordCache.getJSONContent(), record));
+			return true;
+		}
+		return false;
+	}
+
+	private void storeToCache(DDLFormLoadRecordEvent event) {
+		Cache cache = CacheSQL.getInstance();
+		cache.set(new DDLRecordCache(event.getRecord(), event.getJSONObject(), true));
 	}
 
 }
