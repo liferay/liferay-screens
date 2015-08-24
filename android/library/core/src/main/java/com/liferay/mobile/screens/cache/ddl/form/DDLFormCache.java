c75ac8c27facdbce546c8426e005ca48ad9f44c9
Javier Gamarra <javier.gamarra@liferay.com>
2015-08-24 18:40:32 +0200

    LSR-623 ddl form cache table

---
+++ android/library/core/src/main/java/com/liferay/mobile/screens/cache/ddl/form/DDLFormCache.java
@@ -0,0 +1,34 @@
+package com.liferay.mobile.screens.cache.ddl.form;
+
+import com.liferay.mobile.screens.cache.CachedType;
+import com.liferay.mobile.screens.cache.DefaultCachedType;
+import com.liferay.mobile.screens.ddl.model.Record;
+
+import org.json.JSONObject;
+
+/**
+ * @author Javier Gamarra
+ */
+public class DDLFormCache extends RecordCache {
+
+	public static final String TABLE_NAME = "ddlform";
+
+	public DDLFormCache() {
+		super();
+	}
+
+	public DDLFormCache(Record record, JSONObject jsonObject) {
+		super(record, jsonObject);
+	}
+
+	@Override
+	public CachedType getCachedType() {
+		return DefaultCachedType.DDL_FORM;
+	}
+
+	@Override
+	public String getId() {
+		return String.valueOf(_recordSetId);
+	}
+
+}
---
+++ android/library/core/src/main/java/com/liferay/mobile/screens/cache/ddl/form/RecordCache.java
@@ -0,0 +1,97 @@
+package com.liferay.mobile.screens.cache.ddl.form;
+
+import com.liferay.mobile.screens.cache.CachedContent;
+import com.liferay.mobile.screens.cache.tablecache.TableCache;
+import com.liferay.mobile.screens.ddl.model.Record;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Locale;
+
+/**
+ * @author Javier Gamarra
+ */
+public abstract class RecordCache implements CachedContent {
+
+	public static final String STRUCTURE_ID = "structureId";
+	public static final String RECORD_SET_ID = "recordSetId";
+	public static final String RECORD_ID = "recordId";
+	public static final String LOCALE = "locale";
+	public static final String CONTENT = "content";
+	public static final String TYPE = "type";
+
+	public RecordCache() {
+		super();
+	}
+
+	public RecordCache(Record record, JSONObject jsonObject) {
+		_recordId = record.getRecordId();
+		_recordSetId = record.getRecordSetId();
+		_structureId = record.getStructureId();
+		_locale = record.getLocale().getLanguage();
+		_content = jsonObject.toString();
+	}
+
+	public Record getRecord() {
+		Record record = new Record(new Locale(_locale));
+		record.setStructureId(_structureId);
+		record.setRecordSetId(_recordSetId);
+		record.setRecordId(_recordId);
+		return record;
+	}
+
+	public long getStructureId() {
+		return _structureId;
+	}
+
+	public void setStructureId(long structureId) {
+		_structureId = structureId;
+	}
+
+	public long getRecordId() {
+		return _recordId;
+	}
+
+	public void setRecordId(long recordId) {
+		_recordId = recordId;
+	}
+
+	public long getRecordSetId() {
+		return _recordSetId;
+	}
+
+	public void setRecordSetId(long recordSetId) {
+		_recordSetId = recordSetId;
+	}
+
+	public String getLocale() {
+		return _locale;
+	}
+
+	public void setLocale(String locale) {
+		_locale = locale;
+	}
+
+	public String getContent() {
+		return _content;
+	}
+
+	public void setContent(String content) {
+		_content = content;
+	}
+
+	public JSONObject getJSONContent() throws JSONException {
+		return new JSONObject(_content);
+	}
+
+	public TableCache getTableCache() {
+		return new TableCache(String.valueOf(getId()), getCachedType(), _content);
+	}
+
+	protected long _structureId;
+	protected long _recordId;
+	protected long _recordSetId;
+	protected String _locale;
+	protected String _content;
+}
