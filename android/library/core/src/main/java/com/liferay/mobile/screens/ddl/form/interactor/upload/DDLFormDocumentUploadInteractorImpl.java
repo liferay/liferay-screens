160878d89942c935a8526aa89eb31ab559b03e0b
Javier Gamarra <javier.gamarra@liferay.com>
2015-08-24 19:22:39 +0200

    LSR-623 store reference to document upload to send it later

--- android/library/core/src/main/java/com/liferay/mobile/screens/ddl/form/interactor/upload/DDLFormDocumentUploadInteractorImpl.java
+++ android/library/core/src/main/java/com/liferay/mobile/screens/ddl/form/interactor/upload/DDLFormDocumentUploadInteractorImpl.java
@@ -2,35 +2,56 @@ package com.liferay.mobile.screens.ddl.form.interactor.upload;
 
 import android.content.Intent;
 
-import com.liferay.mobile.screens.base.interactor.BaseRemoteInteractor;
+import com.liferay.mobile.screens.base.interactor.BaseCachedRemoteInteractor;
+import com.liferay.mobile.screens.base.interactor.OfflineCallback;
+import com.liferay.mobile.screens.cache.Cache;
+import com.liferay.mobile.screens.cache.CachePolicy;
+import com.liferay.mobile.screens.cache.OfflinePolicy;
+import com.liferay.mobile.screens.cache.ddl.documentupload.DocumentUploadCache;
+import com.liferay.mobile.screens.cache.sql.CacheSQL;
 import com.liferay.mobile.screens.context.LiferayScreensContext;
 import com.liferay.mobile.screens.ddl.form.DDLFormListener;
+import com.liferay.mobile.screens.ddl.form.interactor.update.DDLFormUpdateRecordEvent;
 import com.liferay.mobile.screens.ddl.form.service.UploadService;
 import com.liferay.mobile.screens.ddl.model.DocumentField;
 
 /**
  * @author Javier Gamarra
  */
-public class DDLFormDocumentUploadInteractorImpl extends BaseRemoteInteractor<DDLFormListener>
+public class DDLFormDocumentUploadInteractorImpl
+		extends BaseCachedRemoteInteractor<DDLFormListener, DDLFormUpdateRecordEvent>
 		implements DDLFormDocumentUploadInteractor {
 
-	public DDLFormDocumentUploadInteractorImpl(int targetScreenletId) {
-		super(targetScreenletId);
+	public DDLFormDocumentUploadInteractorImpl(int targetScreenletId, CachePolicy cachePolicy, OfflinePolicy offlinePolicy) {
+		super(targetScreenletId, cachePolicy, offlinePolicy);
 	}
 
 	@Override
-	public void upload(long groupId, long userId, long repositoryId, long folderId, String filePrefix, DocumentField file) {
+	public void upload(final long groupId, final long userId, final long repositoryId, final long folderId,
+					   final String filePrefix, final DocumentField file) throws Exception {
 
-		Intent service = new Intent(LiferayScreensContext.getContext(), UploadService.class);
-		service.putExtra("file", file);
-		service.putExtra("userId", userId);
-		service.putExtra("groupId", groupId);
-		service.putExtra("repositoryId", (repositoryId != 0) ? repositoryId : groupId);
-		service.putExtra("folderId", folderId);
-		service.putExtra("screenletId", getTargetScreenletId());
-		service.putExtra("filePrefix", filePrefix);
+		storeOnError(new OfflineCallback() {
+			@Override
+			public void sendOnline() throws Exception {
+				Intent service = new Intent(LiferayScreensContext.getContext(), UploadService.class);
+				service.putExtra("file", file);
+				service.putExtra("userId", userId);
+				service.putExtra("groupId", groupId);
+				service.putExtra("repositoryId", (repositoryId != 0) ? repositoryId : groupId);
+				service.putExtra("folderId", folderId);
+				service.putExtra("screenletId", getTargetScreenletId());
+				service.putExtra("filePrefix", filePrefix);
 
-		LiferayScreensContext.getContext().startService(service);
+				LiferayScreensContext.getContext().startService(service);
+			}
+
+			@Override
+			public void storeToCache() {
+				String path = file.getCurrentValue().toString();
+				Cache cache = CacheSQL.getInstance();
+				cache.set(new DocumentUploadCache(path, userId, groupId, repositoryId, folderId, filePrefix));
+			}
+		});
 	}
 
 	public void onEventMainThread(DDLFormDocumentUploadEvent event) {
