017e23f5ced5dd7714fd943c94de1c0a7bdfa1e0
Javier Gamarra <javier.gamarra@liferay.com>
2015-08-24 18:40:49 +0200

    LSR-623 ddl form search strategy implementation

---
+++ android/library/core/src/main/java/com/liferay/mobile/screens/cache/ddl/form/DDLFormSearch.java
@@ -0,0 +1,51 @@
+package com.liferay.mobile.screens.cache.ddl.form;
+
+import com.liferay.mobile.screens.cache.CachedContent;
+import com.liferay.mobile.screens.cache.sql.CacheSQL;
+import com.liferay.mobile.screens.cache.sql.CacheStrategy;
+import com.pushtorefresh.storio.sqlite.operations.put.PutResult;
+
+import java.util.List;
+
+import static com.liferay.mobile.screens.cache.sql.CacheSQL.queryGet;
+import static com.liferay.mobile.screens.cache.sql.StorIOSQLite.queryDelete;
+
+/**
+ * @author Javier Gamarra
+ */
+public class DDLFormSearch implements CacheStrategy {
+
+	@Override
+	public DDLFormCache getById(String id) {
+		List query = queryGet(DDLFormCache.class, DDLFormCache.TABLE_NAME, WHERE_BY_ID, id);
+		return query.isEmpty() ? null : (DDLFormCache) query.get(0);
+	}
+
+	@Override
+	public List<DDLFormCache> get(String query, Object... args) {
+		return queryGet(DDLFormCache.class, DDLFormCache.TABLE_NAME, query, args);
+	}
+
+	@Override
+	public Object set(CachedContent object) {
+		PutResult putResult = (PutResult) CacheSQL.querySet(object.getTableCache());
+		if (putResult.wasInserted() || putResult.wasUpdated()) {
+			return CacheSQL.querySet(object);
+		}
+		else {
+			return putResult;
+		}
+	}
+
+	@Override
+	public void clear() {
+		queryDelete(DDLFormCache.TABLE_NAME, null);
+	}
+
+	@Override
+	public void clear(String id) {
+		queryDelete(DDLFormCache.TABLE_NAME, WHERE_BY_ID, id);
+	}
+
+	private static final String WHERE_BY_ID = DDLFormCache.RECORD_SET_ID + " = ?";
+}
