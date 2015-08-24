127f0531814667525644f95bf89365b6ffa2771e
Javier Gamarra <javier.gamarra@liferay.com>
2015-08-24 18:46:57 +0200

    LSR-623 ddlrecord table to store if the record is sent or not

---
+++ android/library/core/src/main/java/com/liferay/mobile/screens/cache/ddl/form/DDLRecordCache.java
@@ -0,0 +1,45 @@
+package com.liferay.mobile.screens.cache.ddl.form;
+
+import com.liferay.mobile.screens.cache.CachedType;
+import com.liferay.mobile.screens.cache.DefaultCachedType;
+import com.liferay.mobile.screens.ddl.model.Record;
+
+import org.json.JSONObject;
+
+/**
+ * @author Javier Gamarra
+ */
+public class DDLRecordCache extends RecordCache {
+
+	public static final String TABLE_NAME = "ddlRecord";
+	public static final String SENT = "sent";
+
+	public DDLRecordCache() {
+		super();
+	}
+
+	public DDLRecordCache(Record record, JSONObject jsonObject, boolean sent) {
+		super(record, jsonObject);
+		_sent = sent;
+	}
+
+	public boolean isSent() {
+		return _sent;
+	}
+
+	public void setSent(boolean sent) {
+		_sent = sent;
+	}
+
+	@Override
+	public CachedType getCachedType() {
+		return DefaultCachedType.DDL_RECORD;
+	}
+
+	@Override
+	public String getId() {
+		return String.valueOf(_recordId);
+	}
+
+	private boolean _sent;
+}
