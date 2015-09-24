3b67e57efe81d8a246cb3239a31c0f3dd028ecce
JM <jose.navarro@liferay.com>
2015-09-24 13:03:20 +0200

    LSR-636 Add NSCoding to form’s document

--- ios/Framework/Core/DDL/Model/DDLFieldDocument.swift
+++ ios/Framework/Core/DDL/Model/DDLFieldDocument.swift
@@ -31,11 +31,11 @@ public class DDLFieldDocument : DDLField {
 			case .Uploaded(_):
 				return 1
 			case .Failed(_):
-				return 3
+				return 2
 			case .Uploading(_,_):
-				return 4
+				return 3
 			case .Pending:
-				return 5
+				return 4
 			}
 		}
 
@@ -60,24 +60,70 @@ public class DDLFieldDocument : DDLField {
 
 	//MARK: DDLField
 
+	public override init(attributes: [String:AnyObject], locale: NSLocale) {
+		super.init(attributes: attributes, locale: locale)
+	}
+
+	public required init(coder aDecoder: NSCoder) {
+		let uploadStatusHash = aDecoder.decodeIntegerForKey("uploadStatusHash")
+
+		switch uploadStatusHash {
+		case UploadStatus.Uploaded([:]).hashValue:
+			let attributes = aDecoder.decodeObjectForKey("uploadStatusUploadedAttributes") as!  [String:AnyObject]
+			uploadStatus = .Uploaded(attributes)
+
+		case UploadStatus.Failed(nil).hashValue:
+			let err = aDecoder.decodeObjectForKey("uploadStatusFailedError") as! NSError
+			uploadStatus = .Failed(err)
+
+		case UploadStatus.Uploading(0, 0).hashValue:
+			let n1 = aDecoder.decodeObjectForKey("uploadStatusUploading1") as! NSNumber
+			let n2 = aDecoder.decodeObjectForKey("uploadStatusUploading2") as! NSNumber
+			uploadStatus = .Uploading(n1.unsignedLongValue, n2.unsignedLongValue)
+
+		default:
+			()
+		}
+
+		super.init(coder: aDecoder)
+	}
+
+	public override func encodeWithCoder(aCoder: NSCoder) {
+		super.encodeWithCoder(aCoder)
+
+		aCoder.encodeInteger(uploadStatus.hashValue, forKey: "uploadStatusHash")
+
+		switch uploadStatus {
+		case .Uploaded(let attributes):
+			aCoder.encodeObject(attributes, forKey: "uploadStatusUploadedAttributes")
+		case .Failed(let error):
+			aCoder.encodeObject(error, forKey: "uploadStatusFailedError")
+		case .Uploading(let n1, let n2):
+			aCoder.encodeObject(NSNumber(unsignedLong: n1), forKey: "uploadStatusUploading1")
+			aCoder.encodeObject(NSNumber(unsignedLong: n2), forKey: "uploadStatusUploading2")
+		case .Pending:
+			()
+		}
+	}
+
+
 	override internal func convert(fromString value:String?) -> AnyObject? {
 		var result:AnyObject?
 
 		if let valueString = value {
 			let data = valueString.dataUsingEncoding(NSUTF8StringEncoding,
 				allowLossyConversion: false)
-			if let jsonObject:AnyObject = NSJSONSerialization.JSONObjectWithData(data!,
-				options: NSJSONReadingOptions(0), error: nil) {
-					if let jsonDict = jsonObject as? NSDictionary {
-						let dict = ["groupId" : jsonDict["groupId"]!,
-							"uuid" : jsonDict["uuid"]!,
-							"version" : jsonDict["version"]!]
 
-						uploadStatus = UploadStatus.Uploaded(dict)
-						result = dict
-					}
+			let jsonObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!,
+				options: NSJSONReadingOptions(0),
+				error: nil)
+
+			if let jsonDict = jsonObject as? [String:AnyObject] {
+				uploadStatus = .Uploaded(jsonDict)
+				result = jsonDict
 			}
 			else if valueString != "" {
+				uploadStatus = .Pending
 				result = valueString
 			}
 		}
