8f02f27f87c04d73c72d32161a546ae585e6823e
Javier Gamarra <javier.gamarra@liferay.com>
2015-11-16 15:32:24 +0100

    LSR-646 add new interactor to the screenlet

--- android/library/core/src/main/java/com/liferay/mobile/screens/context/SessionContext.java
+++ android/library/core/src/main/java/com/liferay/mobile/screens/context/SessionContext.java
@@ -73,6 +73,10 @@ public class SessionContext {
 		return _loggedUser;
 	}
 
+	public static Long getUserId() {
+		return _loggedUser == null ? null : _loggedUser.getId();
+	}
+
 	public static void setLoggedUser(User value) {
 		_loggedUser = value;
 	}
