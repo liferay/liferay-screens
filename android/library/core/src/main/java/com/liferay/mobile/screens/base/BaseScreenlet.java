c2baa579810c5d1fe5facfb7be1bd4e13a38659e
Javier Gamarra <javier.gamarra@liferay.com>
2015-11-18 13:14:53 +0100

    LSR-670 adding a listener to support custom interactors, to allow recreation in each phase

--- android/library/core/src/main/java/com/liferay/mobile/screens/base/BaseScreenlet.java
+++ android/library/core/src/main/java/com/liferay/mobile/screens/base/BaseScreenlet.java
@@ -21,6 +21,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import com.liferay.mobile.screens.base.interactor.CustomInteractorListener;
 import com.liferay.mobile.screens.base.interactor.Interactor;
 import com.liferay.mobile.screens.base.view.BaseViewModel;
 import com.liferay.mobile.screens.context.LiferayScreensContext;
@@ -92,14 +93,28 @@ public abstract class BaseScreenlet<V extends BaseViewModel, I extends Interacto
 		return result;
 	}
 
+	public void addCustomInteractor(CustomInteractorListener customInteractorListener) {
+		_customInteractorListener = customInteractorListener;
+	}
+
 	protected I prepareInteractor(String actionName) {
+
+		if (_customInteractorListener != null) {
+			I customResult = (I) _customInteractorListener.createInteractor(actionName);
+
+			if (customResult != null) {
+				customResult.onScreenletAttached(this);
+				_interactors.put(actionName, customResult);
+				return customResult;
+			}
+		}
+
 		I result = createInteractor(actionName);
 
 		if (result != null) {
 			result.onScreenletAttached(this);
 			_interactors.put(actionName, result);
 		}
-
 		return result;
 	}
 
@@ -259,5 +274,7 @@ public abstract class BaseScreenlet<V extends BaseViewModel, I extends Interacto
 	private Map<String, I> _interactors = new HashMap<>();
 	private int _screenletId;
 	private View _screenletView;
+
+	private CustomInteractorListener _customInteractorListener;
 
 }
