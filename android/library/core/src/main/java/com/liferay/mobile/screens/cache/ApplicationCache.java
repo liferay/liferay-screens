a28a068172938790912d8b291fd2effef0ab5734
Javier Gamarra <javier.gamarra@liferay.com>
2015-12-28 17:32:03 +0100

    LSR-682 add permissions for sync cache

--- android/library/core/src/main/java/com/liferay/mobile/screens/cache/ApplicationCache.java
+++ android/library/core/src/main/java/com/liferay/mobile/screens/cache/ApplicationCache.java
@@ -1,8 +1,13 @@
 package com.liferay.mobile.screens.cache;
 
+import android.Manifest;
 import android.app.Application;
 
 import com.liferay.mobile.screens.context.LiferayScreensContext;
+import com.liferay.mobile.screens.util.LiferayLogger;
+import com.tbruyelle.rxpermissions.RxPermissions;
+
+import rx.functions.Action1;
 
 /**
  * @author Javier Gamarra
@@ -15,6 +20,15 @@ public class ApplicationCache extends Application {
 
 		LiferayScreensContext.init(this);
 
+		RxPermissions.getInstance(this).request(Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.WAKE_LOCK)
+			.subscribe(new Action1<Boolean>() {
+				@Override
+				public void call(Boolean result) {
+					if (!result) {
+						LiferayLogger.e("Can't use cache without network state or wake lock", null);
+					}
+				}
+			});
 
 		//class and type mapping and strategies
 //		CacheSQL.getInstance(StorIOSQLite.getInstance(), new CacheStrategyFactory());
