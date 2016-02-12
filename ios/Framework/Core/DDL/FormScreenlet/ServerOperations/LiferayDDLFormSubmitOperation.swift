a89c68667fc209dae28b2f5818986831014b5e43
JM <jose.navarro@liferay.com>
2016-02-12 23:14:21 +0100

    LSR-722 Fix wrong operation. Change validation by guard

--- ios/Framework/Core/DDL/FormScreenlet/ServerOperations/LiferayDDLFormSubmitOperation.swift
+++ ios/Framework/Core/DDL/FormScreenlet/ServerOperations/LiferayDDLFormSubmitOperation.swift
@@ -41,74 +41,25 @@ public class LiferayDDLFormSubmitOperation: ServerOperation {
 	//MARK: ServerOperation
 
 	override public func validateData() -> ValidationError? {
-		var error = super.validateData()
+		let error = super.validateData()
 
-		if error == nil {
-			if (userId ?? 0) == 0 {
-				return ValidationError("ddlform-screenlet", "undefined-user")
-			}
-
-			if groupId == nil {
-				return ValidationError("ddlform-screenlet", "undefined-group")
-			}
-
-			if recordSetId == nil {
-				return ValidationError("ddlform-screenlet", "undefined-recordset")
-			}
-
-			if values.isEmpty {
-				return ValidationError("ddlform-screenlet", "undefined-values")
-			}
-
-			if let viewModel = viewModel {
-				error = viewModel.validateForm(autoscroll: autoscrollOnValidation)
-			}
+		guard error == nil else {
+			return error
+		}
+		guard (userId ?? 0) != 0 else {
+			return ValidationError("ddlform-screenlet", "undefined-user")
+		}
+		guard groupId != nil else {
+			return ValidationError("ddlform-screenlet", "undefined-group")
+		}
+		guard recordSetId != nil else {
+			return ValidationError("ddlform-screenlet", "undefined-recordset")
+		}
+		guard !values.isEmpty else {
+			return ValidationError("ddlform-screenlet", "undefined-values")
 		}
 
-		return error
-	}
-
-	override public func doRun(session session: LRSession) {
-		let service = LRDDLRecordService_v62(session: session)
-
-		let serviceContextAttributes = [
-			"userId": NSNumber(longLong: userId!),
-			"scopeGroupId": NSNumber(longLong: groupId!)]
-
-		let serviceContextWrapper = LRJSONObjectWrapper(JSONObject: serviceContextAttributes)
-
-		do {
-			let recordDictionary: [NSObject : AnyObject]?
-
-			if recordId == nil {
-				recordDictionary = try service.addRecordWithGroupId(groupId!,
-					recordSetId: recordSetId!,
-					displayIndex: 0,
-					fieldsMap: values,
-					serviceContext: serviceContextWrapper)
-			}
-			else {
-				recordDictionary = try service.updateRecordWithRecordId(recordId!,
-					displayIndex: 0,
-					fieldsMap: values,
-					mergeFields: true,
-					serviceContext: serviceContextWrapper)
-			}
-
-			if let recordIdValue = recordDictionary?["recordId"]?.longLongValue {
-				resultRecordId = recordIdValue
-				resultAttributes = recordDictionary
-				lastError = nil
-			}
-			else {
-				lastError = NSError.errorWithCause(.InvalidServerResponse)
-			}
-		}
-		catch let error as NSError {
-			lastError = error
-			resultRecordId = nil
-			resultAttributes = nil
-		}
+		return viewModel?.validateForm(autoscroll: autoscrollOnValidation)
 	}
 
 }
@@ -187,7 +138,7 @@ public class Liferay70DDLFormSubmitOperation: LiferayDDLFormSubmitOperation {
 				recordDictionary = try service.updateRecordWithRecordId(recordId!,
 					displayIndex: 0,
 					fieldsMap: values,
-					mergeFields: true,
+					mergeFields: false,
 					serviceContext: serviceContextWrapper)
 			}
 
