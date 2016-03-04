10249c89e179b74deca3c57b35b6e5f04e4abb37
JM <jose.navarro@liferay.com>
2016-03-04 12:40:50 +0100

    LSR-776 Fix error when loading zero rows

--- ios/Framework/Core/Base/BaseListScreenlet/TableView/BaseListTableView.swift
+++ ios/Framework/Core/Base/BaseListScreenlet/TableView/BaseListTableView.swift
@@ -53,11 +53,11 @@ public class BaseListTableView: BaseListView, UITableViewDataSource, UITableView
 	override public func onChangedRows(oldRows: [AnyObject?]) {
 		super.onChangedRows(oldRows)
 
-		if oldRows.isEmpty {
-			insertFreshRows()
-		}
-		else if self.rows.isEmpty {
+		if self.rows.isEmpty {
 			clearAllRows(oldRows)
+		}
+		else if oldRows.isEmpty {
+			insertFreshRows()
 		}
 		else if let visibleRows = tableView!.indexPathsForVisibleRows {
 			updateVisibleRows(visibleRows)
