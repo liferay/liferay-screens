ba2472a2d62e7724b363065fd43d32bfc8bb7eb9
Javier Gamarra <javier.gamarra@liferay.com>
2016-03-08 12:46:06 +0100

    LSR-782 using list bookmarks in test app

---
+++ android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/ListBookmarks.java
@@ -0,0 +1,13 @@
+package com.liferay.mobile.screens.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListBookmarks extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_bookmarks);
+	}
+}
