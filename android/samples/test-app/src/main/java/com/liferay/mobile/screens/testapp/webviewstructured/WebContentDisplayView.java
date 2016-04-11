5af46c6b2ee333e7d273887a75759a86a4790881
Javier Gamarra <javier.gamarra@liferay.com>
2016-04-11 13:30:44 +0200

    LSR-804 add an example of structured content

---
+++ android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/webviewstructured/WebContentDisplayView.java
@@ -0,0 +1,34 @@
+package com.liferay.mobile.screens.testapp.webviewstructured;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.liferay.mobile.screens.ddl.model.WebContent;
+import com.liferay.mobile.screens.testapp.R;
+
+/**
+ * @author Javier Gamarra
+ */
+public class WebContentDisplayView extends com.liferay.mobile.screens.viewsets.defaultviews.webcontentdisplay.WebContentDisplayView {
+
+	public WebContentDisplayView(Context context) {
+		super(context);
+	}
+
+	public WebContentDisplayView(Context context, AttributeSet attributes) {
+		super(context, attributes);
+	}
+
+	public WebContentDisplayView(Context context, AttributeSet attributes, int defaultStyle) {
+		super(context, attributes, defaultStyle);
+	}
+
+	@Override
+	public void showFinishOperation(WebContent webContent) {
+		super.showFinishOperation(webContent);
+
+		TextView contentField = (TextView) findViewById(R.id.web_content_field);
+		contentField.setText(webContent.getDDMStructure().getField(0).getCurrentValue().toString());
+	}
+}
