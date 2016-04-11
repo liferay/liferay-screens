57f349042cbc1346bbf394721a4f092bb6177cf7
Javier Gamarra <javier.gamarra@liferay.com>
2016-04-11 10:27:20 +0200

    LSR-804 new interactor

---
+++ android/library/src/main/java/com/liferay/mobile/screens/webcontentdisplay/interactor/WebContentDisplayFromStructureInteractorImpl.java
@@ -0,0 +1,108 @@
+package com.liferay.mobile.screens.webcontentdisplay.interactor;
+
+import com.liferay.mobile.android.service.BatchSessionImpl;
+import com.liferay.mobile.android.service.Session;
+import com.liferay.mobile.screens.cache.DefaultCachedType;
+import com.liferay.mobile.screens.cache.OfflinePolicy;
+import com.liferay.mobile.screens.cache.sql.CacheSQL;
+import com.liferay.mobile.screens.cache.tablecache.TableCache;
+import com.liferay.mobile.screens.context.SessionContext;
+import com.liferay.mobile.screens.ddl.form.connector.DDMStructureConnector;
+import com.liferay.mobile.screens.util.ServiceProvider;
+import com.liferay.mobile.screens.webcontentdisplay.connector.JournalContentConnector;
+
+import java.util.Locale;
+
+/**
+ * @author Javier Gamarra
+ */
+public class WebContentDisplayFromStructureInteractorImpl
+	extends WebContentDisplayBaseInteractorImpl
+	implements WebContentDisplayFromStructureInteractor {
+
+	public WebContentDisplayFromStructureInteractorImpl(int targetScreenletId, OfflinePolicy offlinePolicy) {
+		super(targetScreenletId, offlinePolicy);
+	}
+
+	@Override
+	public void load(Long structureId, long groupId, String articleId, Locale locale) throws Exception {
+		validate(structureId, groupId, articleId, locale);
+
+		processWithCache(structureId, groupId, articleId, locale);
+	}
+
+	public void onEvent(WebContentDisplayEvent event) {
+		if (!isValidEvent(event)) {
+			return;
+		}
+
+		onEventWithCache(event, event.getStructureId(), event.getArticleId(), event.getLocale());
+
+		if (!event.isFailed()) {
+			getListener().onWebContentReceived(null, event.getHtml());
+		}
+	}
+
+	@Override
+	protected void online(Object... args) throws Exception {
+
+		Long structureId = (Long) args[0];
+		Long groupId = (Long) args[1];
+		String articleId = (String) args[2];
+		Locale locale = (Locale) args[3];
+
+		Session session = SessionContext.createSessionFromCurrentSession();
+		BatchSessionImpl batchSession = new BatchSessionImpl(session);
+		batchSession.setCallback(new WebContentDisplayFromStructureCallback(getTargetScreenletId(), structureId, articleId, locale));
+
+		JournalContentConnector journalArticleConnector = ServiceProvider.getInstance().getJournalContentConnector(batchSession);
+		journalArticleConnector.getArticle(groupId, articleId);
+
+		DDMStructureConnector structureConnector = ServiceProvider.getInstance().getDDMStructureConnector(batchSession);
+		structureConnector.getStructure(structureId);
+
+		batchSession.invoke();
+	}
+
+	@Override
+	protected boolean cached(Object... args) throws Exception {
+
+		Long structureId = (Long) args[0];
+		Long groupId = (Long) args[1];
+		String articleId = (String) args[2];
+		Locale locale = (Locale) args[3];
+
+		String id = structureId + articleId;
+		Long userId = SessionContext.getUserId();
+		TableCache webContent = (TableCache) CacheSQL.getInstance().getById(DefaultCachedType.WEB_CONTENT, id, groupId, userId, locale);
+		if (webContent != null) {
+			//FIXME !
+//			onEvent(new WebContentDisplayEvent(getTargetScreenletId(), structureId, articleId, locale, webContent.getContent()));
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	protected void storeToCache(WebContentDisplayEvent event, Object... args) {
+		String webContentId = event.getStructureId() + event.getArticleId();
+		CacheSQL.getInstance().set(new TableCache(webContentId, DefaultCachedType.WEB_CONTENT,
+			event.getHtml(), event.getGroupId(), null, event.getLocale()));
+	}
+
+	private void validate(Long structureId, long groupId, String articleId, Locale locale) {
+		super.validate(locale);
+
+		if (groupId == 0) {
+			throw new IllegalArgumentException("GroupId cannot be null");
+		}
+
+		if (structureId == null || structureId == 0L) {
+			throw new IllegalArgumentException("StructureId cannot be null");
+		}
+
+		if (articleId == null || articleId.isEmpty()) {
+			throw new IllegalArgumentException("ArticleId cannot be null");
+		}
+	}
+}
