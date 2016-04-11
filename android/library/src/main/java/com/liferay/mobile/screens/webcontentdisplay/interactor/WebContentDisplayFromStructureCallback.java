a5f8e58bb94af9d60f976cce0ccd60f4e85bdec6
Javier Gamarra <javier.gamarra@liferay.com>
2016-04-11 10:36:05 +0200

    LSR-804 add manual callback for structureId petition

---
+++ android/library/src/main/java/com/liferay/mobile/screens/webcontentdisplay/interactor/WebContentDisplayFromStructureCallback.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.screens.webcontentdisplay.interactor;
+
+import com.liferay.mobile.screens.base.list.interactor.GenericBatchAsyncTaskCallback;
+import com.liferay.mobile.screens.ddl.model.WebContent;
+import com.liferay.mobile.screens.util.EventBusUtil;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * @author Jose Manuel Navarro
+ */
+public class WebContentDisplayFromStructureCallback
+	extends GenericBatchAsyncTaskCallback<WebContent> {
+
+	public WebContentDisplayFromStructureCallback(int targetScreenletId, Long structureId, String articleId, Locale locale) {
+		_targetScreenletId = targetScreenletId;
+		_structureId = structureId;
+		_articleId = articleId;
+		_locale = locale;
+	}
+
+	@Override
+	public WebContent transform(Object obj) throws Exception {
+
+		WebContent result = new WebContent(new HashMap<String, Object>(), _locale);
+		JSONArray jsonArray = ((JSONArray) obj).getJSONArray(0);
+		List<WebContent> entries = new ArrayList<>();
+
+		return result;
+	}
+
+	@Override
+	public void onSuccess(WebContent webContent) {
+		EventBusUtil.post(new WebContentDisplayEvent(_targetScreenletId, _structureId, _articleId, _locale, webContent));
+	}
+
+	@Override
+	public void onSuccess(ArrayList<WebContent> results) {
+	}
+
+	@Override
+	public void onFailure(Exception exception) {
+		EventBusUtil.post(new WebContentDisplayEvent(_targetScreenletId, _structureId, _articleId, _locale, exception));
+	}
+
+	private final Long _structureId;
+	private final String _articleId;
+	private final Locale _locale;
+
+	private int _targetScreenletId;
+}
