a647ebb4e185dc738ab64130781325071c083d59
Alejandro Hernández <info@alejandrohdezma.com>
2016-06-20 09:17:26 +0200

    LSR-850 create viewmodel for ratings screenlet

---
+++ android/library/src/main/java/com/liferay/mobile/screens/rating/view/RatingViewModel.java
@@ -0,0 +1,12 @@
+package com.liferay.mobile.screens.rating.view;
+
+import com.liferay.mobile.screens.base.view.BaseViewModel;
+
+/**
+ * @author Alejandro Hernández
+ */
+public interface RatingViewModel extends BaseViewModel {
+    double getScore();
+
+    void setScore(double score);
+}
