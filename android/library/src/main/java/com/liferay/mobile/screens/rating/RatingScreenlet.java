02e2054fc34cc39990ff9e2f061d82cc5ea7b5ce
Alejandro Hernández <info@alejandrohdezma.com>
2016-06-21 10:30:03 +0200

    LSR-850 create screenlet's main class

---
+++ android/library/src/main/java/com/liferay/mobile/screens/rating/RatingScreenlet.java
@@ -0,0 +1,93 @@
+package com.liferay.mobile.screens.rating;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import com.liferay.mobile.screens.R;
+import com.liferay.mobile.screens.base.BaseScreenlet;
+import com.liferay.mobile.screens.rating.interactor.RatingInteractor;
+import com.liferay.mobile.screens.rating.interactor.RatingInteractorImpl;
+import com.liferay.mobile.screens.rating.view.RatingViewModel;
+import java.util.List;
+
+/**
+ * Created by liferay on 21/06/16.
+ */
+
+public class RatingScreenlet extends BaseScreenlet<RatingViewModel, RatingInteractor> implements RatingListener {
+
+  public RatingScreenlet(Context context) {
+    super(context);
+  }
+
+  public RatingScreenlet(Context context, AttributeSet attributes) {
+    super(context, attributes);
+  }
+
+  public RatingScreenlet(Context context, AttributeSet attributes, int defaultStyle) {
+    super(context, attributes, defaultStyle);
+  }
+
+  @Override protected View createScreenletView(Context context, AttributeSet attributes) {
+    TypedArray typedArray = context.getTheme().obtainStyledAttributes(attributes, R.styleable.RatingScreenlet, 0, 0);
+
+    int layoutId = typedArray.getResourceId(R.styleable.RatingScreenlet_layoutId, 0);
+
+    View view = LayoutInflater.from(context).inflate(layoutId, null);
+
+    _entryId = typedArray.getInteger(R.styleable.RatingScreenlet_entryId, 0);
+
+    typedArray.recycle();
+
+    return view;
+  }
+
+  @Override protected RatingInteractor createInteractor(String actionName) {
+    return new RatingInteractorImpl(getScreenletId());
+  }
+
+  @Override protected void onFinishInflate() {
+    super.onFinishInflate();
+
+    try {
+      getInteractor().load(_entryId);
+    } catch (Exception e) {
+      this.onRetrieveRatingEntriesFailure(e);
+    }
+  }
+
+  @Override
+  protected void onUserAction(String userActionName, RatingInteractor interactor, Object... args) {
+
+  }
+
+  @Override public void onRetrieveRatingEntriesFailure(Exception exception) {
+    getViewModel().showFailedOperation(null, exception);
+
+    if (_listener != null) {
+      _listener.onRetrieveRatingEntriesFailure(exception);
+    }
+  }
+
+  @Override public void onRetrieveRatingEntriesSuccess(List<RatingEntry> ratings) {
+    getViewModel().showFinishOperation(ratings);
+
+    if (_listener != null) {
+      _listener.onRetrieveRatingEntriesSuccess(ratings);
+    }
+  }
+
+  public RatingListener getListener() {
+    return _listener;
+  }
+
+  public void setListener(RatingListener listener) {
+    this._listener = listener;
+  }
+
+  private long _entryId;
+
+  private RatingListener _listener;
+}
--- android/library/src/main/java/com/liferay/mobile/screens/rating/view/RatingViewModel.java
+++ android/library/src/main/java/com/liferay/mobile/screens/rating/view/RatingViewModel.java
@@ -1,10 +1,12 @@
 package com.liferay.mobile.screens.rating.view;
 
 import com.liferay.mobile.screens.base.view.BaseViewModel;
+import com.liferay.mobile.screens.rating.RatingEntry;
+import java.util.List;
 
 /**
  * @author Alejandro Hernández
  */
 public interface RatingViewModel extends BaseViewModel {
-    void setScore(double score);
+    void showFinishOperation(List<RatingEntry> ratings);
 }
