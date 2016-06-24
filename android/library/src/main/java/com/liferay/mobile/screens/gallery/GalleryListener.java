b11cf214af01e53483fac7f768ffa31e1dd20b19
Victor Galan <victor.galan@liferay.com>
2016-06-24 09:25:02 +0200

    LSR-851 Screenlet class

---
+++ android/library/src/main/java/com/liferay/mobile/screens/gallery/GalleryListener.java
@@ -0,0 +1,12 @@
+package com.liferay.mobile.screens.gallery;
+
+import com.liferay.mobile.screens.base.list.BaseListListener;
+import com.liferay.mobile.screens.gallery.model.ImageEntry;
+
+/**
+ * @author Víctor Galán Grande
+ */
+public interface GalleryListener extends BaseListListener<ImageEntry>{
+
+
+}
