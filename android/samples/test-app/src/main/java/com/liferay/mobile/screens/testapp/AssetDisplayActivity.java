33efe7ad03d5a8d123981f8eaa2327b4244e7aef
Sarai Díaz <dgarciasarai@gmail.com>
2016-06-24 11:04:37 +0200

    LSR-848 asset display in test-app

---
+++ android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/AssetDisplayActivity.java
@@ -0,0 +1,29 @@
+package com.liferay.mobile.screens.testapp;
+
+import android.os.Bundle;
+import com.liferay.mobile.screens.assetdisplay.AssetDisplayListener;
+import com.liferay.mobile.screens.assetdisplay.AssetDisplayScreenlet;
+import com.liferay.mobile.screens.assetlist.AssetEntry;
+
+/**
+ * @author Sarai Díaz García
+ */
+public class AssetDisplayActivity extends ThemeActivity implements AssetDisplayListener {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.asset_display);
+
+    ((AssetDisplayScreenlet) findViewById(R.id.asset_display_screenlet)).setListener(this);
+  }
+
+  @Override public void onRetrieveAssetFailure(Exception e) {
+    error("Could not receive asset entry", e);
+  }
+
+  @Override public void onRetrieveAssetSuccess(AssetEntry assetEntry) {
+    info("Asset entry received!");
+  }
+
+}
