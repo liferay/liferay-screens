86f88c2eb8c9d362e061c7cfa1c6f57ae243a452
Victor Galan <victor.galan@liferay.com>
2016-06-28 12:12:56 +0200

    LSR-851 Move url generation to interactor callback

--- android/library/src/main/java/com/liferay/mobile/screens/gallery/interactor/GalleryCallback.java
+++ android/library/src/main/java/com/liferay/mobile/screens/gallery/interactor/GalleryCallback.java
@@ -9,46 +9,50 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Locale;
 import java.util.Map;
-import org.json.JSONException;
 
 /**
  * @author Víctor Galán Grande
  */
 public class GalleryCallback extends BaseListCallback<ImageEntry> {
 
-  public GalleryCallback(int targetScreenletId, Pair<Integer, Integer> rowsRange, Locale locale) {
-    super(targetScreenletId, rowsRange, locale);
-  }
+	public GalleryCallback(int targetScreenletId, Pair<Integer, Integer> rowsRange, Locale locale) {
+		super(targetScreenletId, rowsRange, locale);
+	}
 
-  @Override public ImageEntry createEntity(Map<String, Object> stringObjectMap) {
+	@Override
+	public ImageEntry createEntity(Map<String, Object> stringObjectMap) {
 
-    stringObjectMap.put("imageUrl", createImageUrl(stringObjectMap));
-    stringObjectMap.put("thumbnailUrl", createThumbnailUrl(stringObjectMap));
+		stringObjectMap.put("imageUrl", createImageUrl(stringObjectMap));
+		stringObjectMap.put("thumbnailUrl", createThumbnailUrl(stringObjectMap));
 
-    return new ImageEntry(stringObjectMap);
-  }
+		return new ImageEntry(stringObjectMap);
+	}
 
-  private String createThumbnailUrl(Map<String, Object> stringObjectMap) {
-    return createImageUrl(stringObjectMap)
-        + "?version="
-        + stringObjectMap.get("version")
-        + "&imageThumbnail=1";
-  }
+	private String createThumbnailUrl(Map<String, Object> stringObjectMap) {
+		return createImageUrl(stringObjectMap)
+			+ "?version="
+			+ stringObjectMap.get("version")
+			+ "&imageThumbnail=1";
+	}
 
-  private String createImageUrl(Map<String, Object> stringObjectMap) {
-    try {
-      return LiferayServerContext.getServer()
-          + "/documents/"
-          + stringObjectMap.get("groupId")
-          + "/"
-          + stringObjectMap.get("folderId")
-          + "/"
-          + URLEncoder.encode((String) stringObjectMap.get("title"), "UTF-8")
-          + "/"
-          + stringObjectMap.get("uuid");
-    } catch (UnsupportedEncodingException ex) {
-      LiferayLogger.e(ex.getMessage());
-      return "";
-    }
-  }
+	private String createImageUrl(Map<String, Object> stringObjectMap) {
+		return LiferayServerContext.getServer()
+			+ "/documents/"
+			+ stringObjectMap.get("groupId")
+			+ "/"
+			+ stringObjectMap.get("folderId")
+			+ "/"
+			+ encodeUrlString((String) stringObjectMap.get("title"))
+			+ "/"
+			+ stringObjectMap.get("uuid");
+	}
+
+	private String encodeUrlString(String urlToEncode) {
+		try {
+			return URLEncoder.encode(urlToEncode, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			LiferayLogger.e("Error encoding string: " + e.getMessage());
+			return "";
+		}
+	}
 }
