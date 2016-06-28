10d44afd921a0faa5ddc0e15aa698826ddc9c40c
Victor Galan <victor.galan@liferay.com>
2016-06-28 12:16:54 +0200

    LSR-851 Add cache support

--- android/library/src/main/java/com/liferay/mobile/screens/gallery/interactor/GalleryInteractor.java
+++ android/library/src/main/java/com/liferay/mobile/screens/gallery/interactor/GalleryInteractor.java
@@ -1,11 +1,12 @@
 package com.liferay.mobile.screens.gallery.interactor;
 
 import com.liferay.mobile.screens.base.interactor.Interactor;
+import java.util.Locale;
 
 /**
  * @author Víctor Galán Grande
  */
 public interface GalleryInteractor extends Interactor<GalleryInteractorListener> {
 
-  void loadRows(int startRow, int endRow, long groupId, long folderId) throws Exception;
+	void loadRows(long groupId, long folderId, int startRow, int endRow, Locale locale) throws Exception;
 }
