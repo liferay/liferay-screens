86d3389dd5a45928b523a23b806cc3bf8ae0da72
Victor Galan <victor.galan@liferay.com>
2016-07-04 11:18:33 +0200

    LSR-854 Check valid event

--- android/library/src/main/java/com/liferay/mobile/screens/userportrait/interactor/upload/UserPortraitUploadInteractorImpl.java
+++ android/library/src/main/java/com/liferay/mobile/screens/userportrait/interactor/upload/UserPortraitUploadInteractorImpl.java
@@ -101,7 +101,9 @@ public class UserPortraitUploadInteractorImpl
 	}
 
 	public void onEvent(MediaStoreEvent event) {
-		getListener().onPicturePathReceived(event.getFilePath());
+		if(isValidEvent(event)) {
+			getListener().onPicturePathReceived(event.getFilePath());
+		}
 	}
 
 	@Override
