4ece60de058b243f67fa47c22a88b36983126d82
Sarai Díaz <dgarciasarai@gmail.com>
2016-07-04 09:12:42 +0200

    LSR-848 added file entry model

---
+++ android/library/src/main/java/com/liferay/mobile/screens/assetdisplay/model/FileEntry.java
@@ -0,0 +1,30 @@
+package com.liferay.mobile.screens.assetdisplay.model;
+
+import com.liferay.mobile.screens.assetlist.AssetEntry;
+import java.util.Map;
+
+/**
+ * @author Sarai Díaz García
+ */
+public class FileEntry extends AssetEntry {
+
+	public FileEntry(Map<String, Object> map) {
+		super(map);
+		_map = (Map<String, Object>) map.get("object");
+		_values = (Map<String, Object>) _map.get("fileEntry");
+	}
+
+	public String getTitle() {
+		return (String) _values.get("title");
+	}
+
+	public String getUrl() {
+		String url = (String) _map.get("url");
+		int index = url.lastIndexOf("/");
+		String result = url.substring(0, index);
+		return result;
+	}
+
+	private final Map<String, Object> _values;
+	private final Map<String, Object> _map;
+}
