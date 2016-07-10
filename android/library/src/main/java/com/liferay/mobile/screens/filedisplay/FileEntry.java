a2bf6d400d2a5b4ebc2440409be964f1641411b3
Sarai Díaz <dgarciasarai@gmail.com>
2016-07-10 20:07:14 +0200

    LSR-848 fixed redundant data in FileEntry model

--- android/library/src/main/java/com/liferay/mobile/screens/filedisplay/FileEntry.java
+++ android/library/src/main/java/com/liferay/mobile/screens/filedisplay/FileEntry.java
@@ -10,21 +10,11 @@ public class FileEntry extends AssetEntry {
 
 	public FileEntry(Map<String, Object> map) {
 		super(map);
-		_map = (Map<String, Object>) map.get("object");
-		_values = (Map<String, Object>) _map.get("fileEntry");
-	}
-
-	public String getTitle() {
-		return (String) _values.get("title");
 	}
 
 	public String getUrl() {
-		String url = (String) _map.get("url");
+		String url = (String) _values.get("url");
 		int index = url.lastIndexOf("/");
-		String result = url.substring(0, index);
-		return result;
+		return url.substring(0, index);
 	}
-
-	private final Map<String, Object> _values;
-	private final Map<String, Object> _map;
 }
