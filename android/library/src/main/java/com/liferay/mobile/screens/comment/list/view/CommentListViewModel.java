0d4065a2e7c1c635a6d90c588e6fd1ff85fd132e
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-15 11:43:19 +0200

    LSR-849 create comments viewmodel

---
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/list/view/CommentListViewModel.java
@@ -0,0 +1,10 @@
+package com.liferay.mobile.screens.comment.list.view;
+
+import com.liferay.mobile.screens.base.list.view.BaseListViewModel;
+import com.liferay.mobile.screens.models.CommentEntry;
+
+/**
+ * @author Alejandro Hernández
+ */
+public interface CommentListViewModel extends BaseListViewModel<CommentEntry>{
+}
