777bb2a87fc422ac62d68b9fd85bd003637efd11
Sarai Díaz <dgarciasarai@gmail.com>
2016-07-18 10:27:38 +0200

    LSR-860 override createInteractor method for video screenlet

--- ios/Framework/Core/FileDisplay/Video/VideoDisplayScreenlet.swift
+++ ios/Framework/Core/FileDisplay/Video/VideoDisplayScreenlet.swift
@@ -28,6 +28,8 @@ public class VideoDisplayScreenlet: BaseScreenlet {
 
 	@IBInspectable public var autoLoad: Bool = true
 
+	public var fileEntry: FileEntry?
+
 	public var videoDisplayDelegate: VideoDisplayScreenletDelegate? {
 		return delegate as? VideoDisplayScreenletDelegate
 	}
@@ -38,6 +40,28 @@ public class VideoDisplayScreenlet: BaseScreenlet {
 		if autoLoad && entryId != 0 {
 			loadVideoAssetFromEntryId()
 		}
+	}
+
+	override public func createInteractor(name name: String, sender: AnyObject?) -> Interactor? {
+		let interactor = AssetDisplayInteractor(
+			screenlet: self,
+			entryId: self.entryId)
+
+
+		interactor.onSuccess = {
+			if let resultAsset = interactor.assetEntry {
+				self.fileEntry = FileEntry(attributes: resultAsset.attributes)
+				self.videoDisplayDelegate?.screenlet?(self, onVideoAssetResponse: self.fileEntry!)
+
+				(self.screenletView as! VideoDisplayViewModel).fileEntry = self.fileEntry!
+			}
+		}
+
+		interactor.onFailure = {
+			self.videoDisplayDelegate?.screenlet?(self, onVideoAssetError: $0)
+		}
+
+		return interactor
 	}
 
 	public func loadVideoAssetFromEntryId() -> Bool {
