f3ad3d03722b1f3d2c414943bf20c76960494469
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-18 11:37:20 +0200

    LSR-849 add method implementation to interactor implementation

--- android/library/src/main/java/com/liferay/mobile/screens/comment/list/interactor/CommentListInteractorImpl.java
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/list/interactor/CommentListInteractorImpl.java
@@ -10,9 +10,15 @@ import com.liferay.mobile.screens.cache.OfflinePolicy;
 import com.liferay.mobile.screens.cache.tablecache.TableCache;
 import com.liferay.mobile.screens.comment.list.CommentListListener;
 import com.liferay.mobile.screens.models.CommentEntry;
+import com.liferay.mobile.screens.service.v70.CommentmanagerjsonwsService;
 import com.liferay.mobile.screens.util.LiferayLocale;
+import com.liferay.mobile.screens.util.JSONUtil;
 import java.util.Locale;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.liferay.mobile.screens.cache.DefaultCachedType.ASSET_LIST;
+import static com.liferay.mobile.screens.cache.DefaultCachedType.ASSET_LIST_COUNT;
 
 /**
  * @author Alejandro Hernández
@@ -37,32 +43,45 @@ public class CommentListInteractorImpl
 	}
 
 	@NonNull @Override protected CommentEntry getElement(TableCache tableCache) throws JSONException {
-		return null;
+		return new CommentEntry(JSONUtil.toMap(new JSONObject(tableCache.getContent())));
 	}
 
-	@Override protected String getContent(CommentEntry object) {
-		return null;
+	@Override protected String getContent(CommentEntry commentEntry) {
+		return new JSONObject(commentEntry.getValues()).toString();
 	}
 
 	@Override protected BaseListCallback<CommentEntry> getCallback(Pair<Integer, Integer> rowsRange, Locale locale) {
-		return null;
+		return new CommentListCallback(getTargetScreenletId(), rowsRange, locale);
 	}
 
 	@Override protected void getPageRowsRequest(Session session, int startRow, int endRow, Locale locale)
 		throws Exception {
-
+		CommentmanagerjsonwsService service = getCommentsService(session);
+		service.getComments(_groupId, _className, _classPK, startRow, endRow);
 	}
 
 	@Override protected void getPageRowCountRequest(Session session) throws Exception {
-
+		CommentmanagerjsonwsService service = getCommentsService(session);
+		service.getCommentsCount(_groupId, _className, _classPK);
 	}
 
 	@Override protected boolean cached(Object... args) throws Exception {
-		return false;
+		final int startRow = (int) args[0];
+		final int endRow = (int) args[1];
+
+		return recoverRows(getId(), ASSET_LIST, ASSET_LIST_COUNT, _groupId, null, null, startRow, endRow);
 	}
 
 	@Override protected void storeToCache(BaseListEvent event, Object... args) {
+		storeRows(getId(), ASSET_LIST, ASSET_LIST_COUNT, _groupId, null, event);
+	}
 
+	private CommentmanagerjsonwsService getCommentsService(Session session) {
+		return new CommentmanagerjsonwsService(session);
+	}
+
+	@NonNull private String getId() {
+		return _className + "-" + String.valueOf(_classPK);
 	}
 
 	private long _groupId;
