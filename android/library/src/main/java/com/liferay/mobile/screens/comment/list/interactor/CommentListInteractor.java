1bfbe6a302a0147ac8fbaedf20f8532c8a9f3eb4
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-18 09:05:56 +0200

    LSR-849 create list interactor interface

---
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/list/interactor/CommentListInteractor.java
@@ -0,0 +1,16 @@
+package com.liferay.mobile.screens.comment.list.interactor;
+
+import com.liferay.mobile.screens.base.interactor.Interactor;
+import com.liferay.mobile.screens.comment.list.CommentListListener;
+
+/**
+ * @author Alejandro Hernández
+ */
+public interface CommentListInteractor extends Interactor<CommentListListener> {
+
+	void loadRows(
+		long groupId, String className, long classPK,
+		int startRow, int endRow)
+		throws Exception;
+
+}
