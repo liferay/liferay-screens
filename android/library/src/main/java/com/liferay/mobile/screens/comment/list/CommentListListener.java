055aa1fe3a9b64c217af6463cda8499ce29364d3
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-18 09:07:23 +0200

    LSR-849 create comment list listener

---
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/list/CommentListListener.java
@@ -0,0 +1,11 @@
+package com.liferay.mobile.screens.comment.list;
+
+import com.liferay.mobile.screens.base.list.interactor.BaseListInteractorListener;
+import com.liferay.mobile.screens.models.CommentEntry;
+
+/**
+ * @author Alejandro Hernández
+ */
+public interface CommentListListener extends BaseListInteractorListener<CommentEntry> {
+
+}
