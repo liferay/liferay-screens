aafde2ff8ad4d415c1322012467e17ce4a1426c1
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-19 09:51:24 +0200

    LSR-849 update adapter with viewholder class

--- android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/comment/list/CommentListAdapter.java
+++ android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/comment/list/CommentListAdapter.java
@@ -2,6 +2,8 @@ package com.liferay.mobile.screens.viewsets.defaultviews.comment.list;
 
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.TextView;
+import com.liferay.mobile.screens.R;
 import com.liferay.mobile.screens.base.list.BaseListAdapter;
 import com.liferay.mobile.screens.base.list.BaseListAdapterListener;
 import com.liferay.mobile.screens.models.CommentEntry;
@@ -9,17 +11,35 @@ import com.liferay.mobile.screens.models.CommentEntry;
 /**
  * @author Alejandro Hernández
  */
-public class CommentListAdapter extends BaseListAdapter<CommentEntry, BaseListAdapter.ViewHolder> {
+public class CommentListAdapter extends BaseListAdapter<CommentEntry, CommentListAdapter.CommentViewHolder> {
 
 	public CommentListAdapter(int layoutId, int progressLayoutId, BaseListAdapterListener listener) {
 		super(layoutId, progressLayoutId, listener);
 	}
 
-	@NonNull @Override public ViewHolder createViewHolder(View view, BaseListAdapterListener listener) {
-		return new ViewHolder(view, listener);
+	@NonNull @Override public CommentViewHolder createViewHolder(View view, BaseListAdapterListener listener) {
+		return new CommentViewHolder(view, listener);
 	}
 
-	@Override protected void fillHolder(CommentEntry entry, ViewHolder holder) {
-		holder.textView.setText(entry.getBody());
+	@Override protected void fillHolder(CommentEntry entry, CommentViewHolder holder) {
+		holder.bind(entry);
+	}
+
+	public class CommentViewHolder extends BaseListAdapter.ViewHolder {
+
+		public CommentViewHolder(View view, BaseListAdapterListener listener) {
+			super(view, listener);
+
+			_userNameTextView = (TextView) view.findViewById(R.id.comment_user_name);
+			_bodyTextView = (TextView) view.findViewById(R.id.comment_body);
+		}
+
+		public void bind(CommentEntry entry) {
+			_userNameTextView.setText(entry.getUserName());
+			_bodyTextView.setText(entry.getBody());
+		}
+
+		private final TextView _userNameTextView;
+		private final TextView _bodyTextView;
 	}
 }
