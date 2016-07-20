539d8527032dc448513dc9c26e783681aa5c1c05
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-20 16:28:52 +0200

    LSR-849 create custom view class

---
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/list/view/CommentView.java
@@ -0,0 +1,18 @@
+package com.liferay.mobile.screens.comment.list.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class CommentView extends RelativeLayout {
+	public CommentView(Context context) {
+		super(context);
+	}
+
+	public CommentView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
