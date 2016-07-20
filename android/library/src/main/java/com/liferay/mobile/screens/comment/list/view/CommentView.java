4daca7ee16a3a778fd1efd315306d8f24ea63ea9
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-20 16:30:24 +0200

    LSR-849 add method to set comment data in view

--- android/library/src/main/java/com/liferay/mobile/screens/comment/list/view/CommentView.java
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/list/view/CommentView.java
@@ -1,12 +1,15 @@
 package com.liferay.mobile.screens.comment.list.view;
 
 import android.content.Context;
+import android.text.Html;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.liferay.mobile.screens.R;
+import com.liferay.mobile.screens.context.SessionContext;
+import com.liferay.mobile.screens.models.CommentEntry;
 import com.liferay.mobile.screens.userportrait.UserPortraitScreenlet;
 
 /**
@@ -24,6 +27,42 @@ public class CommentView extends RelativeLayout {
 		inflater.inflate(R.layout.comment_view, this, true);
 	}
 
+	public void setCommentEntry(CommentEntry commentEntry) {
+		_userPortraitScreenlet.setUserId(commentEntry.getUserId());
+
+		_userNameTextView.setText(commentEntry.getUserName());
+
+		_createDateTextView.setText(commentEntry.getCreateDateAsTimeSpan());
+
+		if (commentEntry.getModifiedDate() != commentEntry.getCreateDate()) {
+			_editedTextView.setVisibility(VISIBLE);
+		} else {
+			_editedTextView.setVisibility(GONE);
+		}
+
+		if (_htmlBody) {
+			_bodyTextView.setText(Html.fromHtml(commentEntry.getBody()));
+		} else {
+			_bodyTextView.setText(Html.fromHtml(commentEntry.getBody()).toString().replaceAll("\n", "").trim());
+		}
+
+		int childCount = commentEntry.getChildCount();
+
+		if (childCount > 0) {
+			_childCountTextView.setVisibility(VISIBLE);
+			_childCountTextView.setText(getContext().getResources().getQuantityString(
+				R.plurals.number_replys, childCount, childCount));
+		} else {
+			_childCountTextView.setVisibility(GONE);
+		}
+
+		if (commentEntry.getUserId() == SessionContext.getUserId()) {
+			_editImageButton.setVisibility(VISIBLE);
+		} else {
+			_editImageButton.setVisibility(GONE);
+		}
+	}
+
 	@Override protected void onFinishInflate() {
 		super.onFinishInflate();
 
@@ -34,6 +73,18 @@ public class CommentView extends RelativeLayout {
 		_editedTextView = (TextView) findViewById(R.id.comment_edited);
 		_childCountTextView = (TextView) findViewById(R.id.comment_child_number);
 		_editImageButton = (ImageButton) findViewById(R.id.comment_edit_button);
+	}
+
+	public void setHtmlBody(boolean htmlBody) {
+		this._htmlBody = htmlBody;
+	}
+
+	public void reloadUserPortrait() {
+		_userPortraitScreenlet.load();
+	}
+
+	public void hideRepliesCounter() {
+		_childCountTextView.setVisibility(GONE);
 	}
 
 	private TextView _userNameTextView;
