433189a65ff7297dfada381ff75998b953aa48ee
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-20 12:39:53 +0200

    LSR-849 manage user clicks in view and back button clicks

--- android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/comment/list/CommentListView.java
+++ android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/comment/list/CommentListView.java
@@ -2,8 +2,12 @@ package com.liferay.mobile.screens.viewsets.defaultviews.comment.list;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.View;
 import com.liferay.mobile.screens.R;
+import com.liferay.mobile.screens.base.BaseScreenlet;
 import com.liferay.mobile.screens.base.list.BaseListScreenletView;
+import com.liferay.mobile.screens.comment.list.CommentListScreenlet;
 import com.liferay.mobile.screens.comment.list.view.CommentListViewModel;
 import com.liferay.mobile.screens.models.CommentEntry;
 
@@ -26,6 +30,15 @@ public class CommentListView
 		super(context, attributes, defaultStyle);
 	}
 
+	@Override public boolean onKeyUp(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && _discussionComment != null) {
+			clearAdapterEntries();
+			getScreenlet().performUserAction(BaseScreenlet.DEFAULT_ACTION, CommentListScreenlet.OUT_DISCUSSION_ACTION);
+			return true;
+		}
+		return super.onKeyUp(keyCode, event);
+	}
+
 	@Override protected CommentListAdapter createListAdapter(int itemLayoutId, int itemProgressLayoutId) {
 		return new CommentListAdapter(itemLayoutId, itemProgressLayoutId, this, getContext());
 	}
@@ -41,6 +54,23 @@ public class CommentListView
 	@Override
 	public void changeToCommentDiscussion(CommentEntry newRootComment) {
 		_discussionComment = newRootComment;
+
+	@Override
+	public void onItemClick(int position, View view) {
+		super.onItemClick(position, view);
+
+		setFocusableInTouchMode(true);
+		requestFocus();
+
+		CommentEntry newRootComment = getAdapter().getEntries().get(position);
+
+		clearAdapterEntries();
+
+		getScreenlet().performUserAction(BaseScreenlet.DEFAULT_ACTION, CommentListScreenlet.IN_DISCUSSION_ACTION, newRootComment);
+	}
+
+	private void clearAdapterEntries() {
+		getAdapter().getEntries().clear();
 	}
 
 	private CommentEntry _discussionComment;
