9fe8b8d435275a28917b98ee5ca350d36ba7fd8f
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-20 16:29:43 +0200

    LSR-849 inflate view and find subviews

--- android/library/src/main/java/com/liferay/mobile/screens/comment/list/view/CommentView.java
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/list/view/CommentView.java
@@ -2,7 +2,12 @@ package com.liferay.mobile.screens.comment.list.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.liferay.mobile.screens.R;
+import com.liferay.mobile.screens.userportrait.UserPortraitScreenlet;
 
 /**
  * @author Alejandro Hernández
@@ -14,5 +19,29 @@ public class CommentView extends RelativeLayout {
 
 	public CommentView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.comment_view, this, true);
 	}
+
+	@Override protected void onFinishInflate() {
+		super.onFinishInflate();
+
+		_userNameTextView = (TextView) findViewById(R.id.comment_user_name);
+		_bodyTextView = (TextView) findViewById(R.id.comment_body);
+		_userPortraitScreenlet = (UserPortraitScreenlet) findViewById(R.id.comment_user_portrait);
+		_createDateTextView = (TextView) findViewById(R.id.comment_create_date);
+		_editedTextView = (TextView) findViewById(R.id.comment_edited);
+		_childCountTextView = (TextView) findViewById(R.id.comment_child_number);
+		_editImageButton = (ImageButton) findViewById(R.id.comment_edit_button);
+	}
+
+	private TextView _userNameTextView;
+	private TextView _bodyTextView;
+	private UserPortraitScreenlet _userPortraitScreenlet;
+	private TextView _createDateTextView;
+	private TextView _editedTextView;
+	private TextView _childCountTextView;
+	private ImageButton _editImageButton;
+	private boolean _htmlBody;
 }
