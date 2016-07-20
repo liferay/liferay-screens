fcb4342b0ceed6d7a980d720fe487806ace61f0e
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-20 13:30:38 +0200

    LSR-849 add discussion comment implementation to view

--- android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/comment/list/CommentListView.java
+++ android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/comment/list/CommentListView.java
@@ -1,15 +1,19 @@
 package com.liferay.mobile.screens.viewsets.defaultviews.comment.list;
 
 import android.content.Context;
+import android.text.Html;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import com.liferay.mobile.screens.R;
 import com.liferay.mobile.screens.base.BaseScreenlet;
 import com.liferay.mobile.screens.base.list.BaseListScreenletView;
 import com.liferay.mobile.screens.comment.list.CommentListScreenlet;
 import com.liferay.mobile.screens.comment.list.view.CommentListViewModel;
 import com.liferay.mobile.screens.models.CommentEntry;
+import com.liferay.mobile.screens.userportrait.UserPortraitScreenlet;
 
 /**
  * @author Alejandro Hernández
@@ -30,6 +34,19 @@ public class CommentListView
 		super(context, attributes, defaultStyle);
 	}
 
+	@Override protected void onFinishInflate() {
+		super.onFinishInflate();
+		_discussionCommentLayout = (ViewGroup) findViewById(R.id.discussion_comment);
+
+		_userNameTextView = (TextView) _discussionCommentLayout.findViewById(R.id.comment_user_name);
+		_bodyTextView = (TextView) _discussionCommentLayout.findViewById(R.id.comment_body);
+		_userPortraitScreenlet = (UserPortraitScreenlet) _discussionCommentLayout.findViewById(R.id.comment_user_portrait);
+		_createDateTextView = (TextView) _discussionCommentLayout.findViewById(R.id.comment_create_date);
+		_editedTextView = (TextView) _discussionCommentLayout.findViewById(R.id.comment_edited);
+
+		_discussionCommentLayout.findViewById(R.id.row_arrow).setVisibility(GONE);
+	}
+
 	@Override public boolean onKeyUp(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK && _discussionComment != null) {
 			clearAdapterEntries();
@@ -48,12 +65,39 @@ public class CommentListView
 	}
 
 	@Override public void setHtmlBody(boolean htmlBody) {
+		_htmlBody = htmlBody;
 		getAdapter().setHtmlBody(htmlBody);
 	}
 
 	@Override
 	public void changeToCommentDiscussion(CommentEntry newRootComment) {
 		_discussionComment = newRootComment;
+
+		if (newRootComment == null) {
+			_discussionCommentLayout.setVisibility(GONE);
+		} else {
+			_userPortraitScreenlet.setUserId(newRootComment.getUserId());
+			_userPortraitScreenlet.load();
+
+			_userNameTextView.setText(newRootComment.getUserName());
+
+			_createDateTextView.setText(newRootComment.getCreateDateAsTimeSpan());
+
+			if (newRootComment.getModifiedDate() != newRootComment.getCreateDate()) {
+				_editedTextView.setVisibility(View.VISIBLE);
+			} else {
+				_editedTextView.setVisibility(View.GONE);
+			}
+
+			if (_htmlBody) {
+				_bodyTextView.setText(Html.fromHtml(newRootComment.getBody()));
+			} else {
+				_bodyTextView.setText(Html.fromHtml(newRootComment.getBody()).toString().replaceAll("\n", "").trim());
+			}
+
+			_discussionCommentLayout.setVisibility(VISIBLE);
+		}
+	}
 
 	@Override
 	public void onItemClick(int position, View view) {
@@ -74,4 +118,12 @@ public class CommentListView
 	}
 
 	private CommentEntry _discussionComment;
+	private ViewGroup _discussionCommentLayout;
+	private TextView _userNameTextView;
+	private TextView _bodyTextView;
+	private UserPortraitScreenlet _userPortraitScreenlet;
+	private TextView _createDateTextView;
+	private TextView _editedTextView;
+
+	private boolean _htmlBody;
 }
