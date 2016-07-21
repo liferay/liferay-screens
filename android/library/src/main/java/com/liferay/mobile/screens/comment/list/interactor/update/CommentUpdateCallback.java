98f0de73aabc2ffac5684360cf35df64af2b2c69
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-21 13:39:54 +0200

    LSR-849 added comment update callback and event

---
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/list/interactor/update/CommentUpdateCallback.java
@@ -0,0 +1,28 @@
+package com.liferay.mobile.screens.comment.list.interactor.update;
+
+import com.liferay.mobile.screens.base.interactor.BasicEvent;
+import com.liferay.mobile.screens.base.interactor.JSONObjectCallback;
+import com.liferay.mobile.screens.comment.list.interactor.delete.CommentDeleteEvent;
+import org.json.JSONObject;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class CommentUpdateCallback extends JSONObjectCallback {
+
+	public CommentUpdateCallback(int targetScreenletId) {
+		super(targetScreenletId);
+	}
+
+	@Override protected BasicEvent createEvent(int targetScreenletId, Exception e) {
+		return new CommentUpdateEvent(targetScreenletId, e);
+	}
+
+	@Override public JSONObject transform(Object obj) throws Exception {
+		return new JSONObject();
+	}
+
+	@Override protected BasicEvent createEvent(int targetScreenletId, JSONObject result) {
+		return new CommentUpdateEvent(targetScreenletId, result);
+	}
+}
---
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/list/interactor/update/CommentUpdateEvent.java
@@ -0,0 +1,18 @@
+package com.liferay.mobile.screens.comment.list.interactor.update;
+
+import com.liferay.mobile.screens.base.interactor.JSONObjectEvent;
+import org.json.JSONObject;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class CommentUpdateEvent extends JSONObjectEvent {
+
+	public CommentUpdateEvent(int targetScreenletId, Exception e) {
+		super(targetScreenletId, e);
+	}
+
+	public CommentUpdateEvent(int targetScreenletId, JSONObject jsonObject) {
+		super(targetScreenletId, jsonObject);
+	}
+}
