9092b9d998162a7dd6b663d33fb33fe179c55b80
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-21 09:46:31 +0200

    LSR-849 comform activity with new listener

--- android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/CommentsActivity.java
+++ android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/CommentsActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.View;
 import com.liferay.mobile.screens.base.list.BaseListListener;
 import com.liferay.mobile.screens.base.list.BaseListScreenlet;
+import com.liferay.mobile.screens.comment.list.CommentListListener;
+import com.liferay.mobile.screens.comment.list.interactor.CommentListInteractorListener;
 import com.liferay.mobile.screens.comment.list.CommentListScreenlet;
 import com.liferay.mobile.screens.context.LiferayServerContext;
 import com.liferay.mobile.screens.models.CommentEntry;
@@ -12,7 +14,7 @@ import java.util.List;
 /**
  * @author Alejandro Hernández
  */
-public class CommentsActivity extends ThemeActivity implements BaseListListener<CommentEntry> {
+public class CommentsActivity extends ThemeActivity implements CommentListListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,29 +34,33 @@ public class CommentsActivity extends ThemeActivity implements BaseListListener<
 		_screenlet.loadPage(0);
 	}
 
+	@Override public void onDeleteCommentFailure(long commentId, Exception e) {
+		error(String.format("Error deleting comment with id: %d%n", commentId), e);
+	}
+
+	@Override public void onDeleteCommentSuccess(long commentId) {
+		info(String.format("Comment with id: %d%n succesfully deleted", commentId));
+	}
+
 	@Override public void onListPageFailed(BaseListScreenlet source, int page, Exception e) {
-		error("There was an error retrieving page #" + page, e);
+		error(String.format("Error receiving page: %d", page), e);
 	}
 
 	@Override
 	public void onListPageReceived(BaseListScreenlet source, int page, List<CommentEntry> entries, int rowCount) {
-		info("Page #" + page + " retrieved succesfully");
+		info(String.format("Page %d succesfully received", page));
 	}
 
 	@Override public void onListItemSelected(CommentEntry element, View view) {
-
 	}
 
 	@Override public void loadingFromCache(boolean success) {
-
 	}
 
 	@Override public void retrievingOnline(boolean triedInCache, Exception e) {
-
 	}
 
 	@Override public void storingToCache(Object object) {
-
 	}
 
 	private CommentListScreenlet _screenlet;
