9b3a336179ae7462e3bcad3967cd192eb9692834
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-25 14:08:26 +0200

    LSR-849 create add interactor and implementation

---
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/interactor/add/CommentAddInteractor.java
@@ -0,0 +1,12 @@
+package com.liferay.mobile.screens.comment.interactor.add;
+
+import com.liferay.mobile.screens.base.interactor.Interactor;
+import com.liferay.mobile.screens.comment.interactor.CommentListInteractorListener;
+
+/**
+ * @author Alejandro Hernández
+ */
+public interface CommentAddInteractor extends Interactor<CommentListInteractorListener> {
+	void addComment(long groupId, String className, long classPK, long parentCommentId, String body)
+		throws Exception;
+}
---
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/interactor/add/CommentAddInteractorImpl.java
@@ -0,0 +1,78 @@
+package com.liferay.mobile.screens.comment.interactor.add;
+
+import com.liferay.mobile.android.service.Session;
+import com.liferay.mobile.screens.base.interactor.BaseRemoteInteractor;
+import com.liferay.mobile.screens.comment.interactor.CommentListInteractorListener;
+import com.liferay.mobile.screens.context.SessionContext;
+import com.liferay.mobile.screens.models.CommentEntry;
+import com.liferay.mobile.screens.service.v70.CommentmanagerjsonwsService;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class CommentAddInteractorImpl extends BaseRemoteInteractor<CommentListInteractorListener>
+	implements CommentAddInteractor {
+
+	public CommentAddInteractorImpl(int targetScreenletId) {
+		super(targetScreenletId);
+	}
+
+	public void onEvent(CommentAddEvent event) {
+		if (!isValidEvent(event)) {
+			return;
+		}
+
+		if (event.isFailed()) {
+			getListener().onAddCommentFailure(_body, event.getException());
+		}
+		else {
+			getListener().onAddCommentSuccess(new CommentEntry(event.getCommentId(), _body));
+		}
+	}
+
+	@Override public void addComment(long groupId, String className, long classPK, long parentCommentId, String body)
+		throws Exception {
+
+		validate(groupId, className, classPK, parentCommentId, body);
+
+		_body = body;
+
+		CommentmanagerjsonwsService service = getCommentsService();
+
+		if (parentCommentId != 0) {
+			service.addComment(parentCommentId, body);
+		} else {
+			service.addComment(groupId, className, classPK, body);
+		}
+	}
+
+	protected CommentmanagerjsonwsService getCommentsService() {
+
+		Session session = SessionContext.createSessionFromCurrentSession();
+
+		session.setCallback(new CommentAddCallback(getTargetScreenletId()));
+
+		return new CommentmanagerjsonwsService(session);
+	}
+
+	protected void validate(long groupId, String className, long classPK, long parentCommentId, String body) {
+
+		if (body.isEmpty()) {
+			throw new IllegalArgumentException(
+				"comment body cannot be empty");
+		} else if (parentCommentId <= 0) {
+			if (groupId <= 0) {
+				throw new IllegalArgumentException(
+					"groupId must be greater than 0");
+			} else if (className.isEmpty()) {
+				throw new IllegalArgumentException(
+					"className cannot be empty");
+			} else if (classPK <= 0) {
+				throw new IllegalArgumentException(
+					"classPK must be greater than 0");
+			}
+		}
+	}
+
+	private String _body;
+}
