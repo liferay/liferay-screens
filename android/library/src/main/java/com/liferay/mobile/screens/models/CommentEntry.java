59a44523937655a2d9f7e713f248c8b80858b827
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-25 14:11:06 +0200

    LSR-849 update comment entry with new constructor

--- android/library/src/main/java/com/liferay/mobile/screens/models/CommentEntry.java
+++ android/library/src/main/java/com/liferay/mobile/screens/models/CommentEntry.java
@@ -3,6 +3,8 @@ package com.liferay.mobile.screens.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.text.format.DateUtils;
+import com.liferay.mobile.screens.context.SessionContext;
+import com.liferay.mobile.screens.context.User;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,13 +33,27 @@ public class CommentEntry implements Parcelable {
 			}
 		};
 
-	protected CommentEntry(Parcel in, ClassLoader loader) {
+	public CommentEntry(Parcel in, ClassLoader loader) {
 		_values = new HashMap<>();
 
 		in.readMap(_values, loader);
 	}
 
 	public CommentEntry(Map<String, Object> values) {
+		_values = values;
+	}
+
+	public CommentEntry(long commentId, String body) {
+		HashMap<String, Object> values = new HashMap<>();
+		values.put("commentId", String.valueOf(commentId));
+		values.put("body", body);
+		values.put("userId", String.valueOf(SessionContext.getUserId()));
+		User currentUser = SessionContext.getCurrentUser();
+		String userName = currentUser.getFirstName() + " " + currentUser.getLastName();
+		values.put("userName", userName);
+		values.put("descendantCommentsCount", 0);
+		values.put("createDate", System.currentTimeMillis());
+		values.put("modifiedDate", System.currentTimeMillis());
 		_values = values;
 	}
 
