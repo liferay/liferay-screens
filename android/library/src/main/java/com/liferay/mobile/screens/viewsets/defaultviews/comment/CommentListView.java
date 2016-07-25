a94528556072dc0930f9e630fdf6f2f12a618e5b
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-25 14:31:34 +0200

    LSR-849 add "add comment" implementation to list view

--- android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/comment/CommentListView.java
+++ android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/comment/CommentListView.java
@@ -5,6 +5,7 @@ import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.liferay.mobile.screens.R;
@@ -22,7 +23,7 @@ import java.util.List;
  */
 public class CommentListView
 	extends BaseListScreenletView<CommentEntry, CommentListAdapter.CommentViewHolder, CommentListAdapter>
-	implements CommentListViewModel, CommentViewListener {
+	implements CommentListViewModel, CommentViewListener, View.OnClickListener {
 
 	public CommentListView(Context context) {
 		super(context);
@@ -42,14 +43,11 @@ public class CommentListView
 		_discussionSeparator = (ImageView) findViewById(R.id.comment_separator);
 		_emptyListTextView = (TextView) findViewById(R.id.comment_empty_list);
 		_goBackButton = (Button) findViewById(R.id.comment_go_back);
+		_sendButton = (Button) findViewById(R.id.comment_send);
+		_addCommentEditText = (EditText) findViewById(R.id.comment_add);
 
-		_goBackButton.setOnClickListener(new OnClickListener() {
-			@Override public void onClick(View v) {
-				if (_discussionComment != null) {
-					goBackToList();
-				}
-			}
-		});
+		_goBackButton.setOnClickListener(this);
+		_sendButton.setOnClickListener(this);
 
 		_commentView.setListener(this);
 	}
@@ -68,6 +66,8 @@ public class CommentListView
 
 	@Override public void showFinishOperation(int page, List<CommentEntry> serverEntries, int rowCount) {
 		super.showFinishOperation(page, serverEntries, rowCount);
+
+		_sendButton.setEnabled(true);
 
 		if (getAdapter().getEntries().isEmpty()) {
 			_emptyListTextView.setText(_discussionComment != null ? R.string.empty_replies : R.string.empty_comments);
@@ -94,6 +94,8 @@ public class CommentListView
 			_goBackButton.setVisibility(GONE);
 			_commentView.setVisibility(GONE);
 			_discussionSeparator.setVisibility(GONE);
+			_addCommentEditText.setHint(R.string.type_your_comment);
+			_sendButton.setText(R.string.send);
 		} else {
 			_goBackButton.setVisibility(VISIBLE);
 			_commentView.setCommentEntry(newRootComment);
@@ -101,6 +103,8 @@ public class CommentListView
 			_commentView.hideRepliesCounter();
 			_commentView.setVisibility(VISIBLE);
 			_discussionSeparator.setVisibility(VISIBLE);
+			_addCommentEditText.setHint(R.string.type_your_reply);
+			_sendButton.setText(R.string.reply);
 		}
 	}
 
@@ -138,10 +142,27 @@ public class CommentListView
 		getScreenlet().performUserAction(CommentListScreenlet.DELETE_COMMENT_ACTION, commentId);
 	}
 
+	@Override public void onClick(View v) {
+		int i = v.getId();
+		if (i == R.id.comment_go_back && _discussionComment != null) {
+			goBackToList();
+		} else if (i == R.id.comment_send) {
+			String body = _addCommentEditText.getText().toString();
+			if (!body.isEmpty()) {
+				clearAdapterEntries();
+				_addCommentEditText.setText("");
+				_sendButton.setEnabled(false);
+				getScreenlet().performUserAction(CommentListScreenlet.ADD_COMMENT_ACTION, body);
+			}
+		}
+	}
+
 	private CommentEntry _discussionComment;
 
 	private CommentView _commentView;
 	private ImageView _discussionSeparator;
 	private TextView _emptyListTextView;
 	private Button _goBackButton;
+	private Button _sendButton;
+	private EditText _addCommentEditText;
 }
