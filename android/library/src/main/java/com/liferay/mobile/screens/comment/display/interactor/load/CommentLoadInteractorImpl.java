fed8f20696c56b68010d46170d15c83474fac2ca
Alejandro Hernández <info@alejandrohdezma.com>
2016-08-02 11:48:45 +0200

    LSR-849 add implementation to load interactor

--- android/library/src/main/java/com/liferay/mobile/screens/comment/display/interactor/load/CommentLoadInteractorImpl.java
+++ android/library/src/main/java/com/liferay/mobile/screens/comment/display/interactor/load/CommentLoadInteractorImpl.java
@@ -22,5 +22,59 @@ public class CommentLoadInteractorImpl extends BaseCachedRemoteInteractor<Commen
 		super(screenletId, offlinePolicy);
 	}
 
-	
+	@Override public void load(long groupId, long commentId) throws Exception {
+		validate(groupId, commentId);
+
+		processWithCache(groupId, commentId);
+	}
+
+	public void onEvent(CommentLoadEvent event) {
+		if (!isValidEvent(event)) {
+			return;
+		}
+
+		if (event.isFailed()) {
+			getListener().onLoadCommentFailure(event.getException());
+		}
+		else {
+			getListener().onLoadCommentSuccess(event.getCommentEntry());
+		}
+	}
+
+	@Override
+	protected void online(Object[] args) throws Exception {
+
+		long groupId = (long) args[0];
+		long commentId = (long) args[1];
+
+		getCommentService().getComment(groupId, commentId);
+	}
+
+	@Override protected boolean cached(Object... args) throws Exception {
+		return false;
+	}
+
+	@Override protected void storeToCache(CommentLoadEvent event, Object... args) {
+
+	}
+
+	@Override protected void notifyError(CommentLoadEvent event) {
+		getListener().onLoadCommentFailure(event.getException());
+	}
+
+	protected CommentmanagerjsonwsService getCommentService() {
+		Session session = SessionContext.createSessionFromCurrentSession();
+
+		session.setCallback(new CommentLoadCallback(getTargetScreenletId()));
+
+		return new CommentmanagerjsonwsService(session);
+	}
+
+	private void validate(long groupId, long commentId) {
+		if (groupId <= 0) {
+			throw new IllegalArgumentException("groupId must be greater than 0");
+		} else if (commentId <= 0) {
+			throw new IllegalArgumentException("commentId cannot be 0 or negative");
+		}
+	}
 }
