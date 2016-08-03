ce38a51a34ac2fb95e7dd7693f88070f87c05cb1
Javier Gamarra <javier.gamarra@liferay.com>
2016-08-03 09:41:36 +0200

    LSR-850 sort and reformat

--- android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/rating/StarBarRatingView.java
+++ android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/rating/StarBarRatingView.java
@@ -2,18 +2,18 @@ package com.liferay.mobile.screens.viewsets.defaultviews.rating;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import com.liferay.mobile.screens.R;
 import com.liferay.mobile.screens.rating.AssetRating;
-import com.liferay.mobile.screens.rating.RatingScreenlet;
+
+import static com.liferay.mobile.screens.rating.RatingScreenlet.UPDATE_RATING_ACTION;
 
 /**
  * @author Alejandro Hernández
  */
 public class StarBarRatingView extends BaseRatingView implements RatingBar.OnRatingBarChangeListener {
+
 	public StarBarRatingView(Context context) {
 		super(context);
 	}
@@ -29,53 +29,53 @@ public class StarBarRatingView extends BaseRatingView implements RatingBar.OnRat
 	@Override
 	public void showFinishOperation(String action, Object argument) {
 		if (_progressBar != null) {
-			_progressBar.setVisibility(View.GONE);
+			_progressBar.setVisibility(GONE);
 		}
 		if (_content != null) {
-			_content.setVisibility(View.VISIBLE);
+			_content.setVisibility(VISIBLE);
 
 			final AssetRating assetRating = (AssetRating) argument;
 
-			setRatingBarRate(_userRatingBar, (float) assetRating.getUserScore());
-			setRatingBarRate(_averageRatingBar, (float) assetRating.getAverage());
-			_totalCountTextView.setText(getContext().getString(R.string.rating_count, assetRating.getTotalCount()));
+			userRatingBar.setRating(getRating(assetRating.getTotalScore(), userRatingBar.getNumStars()));
+			averageRatingBar.setRating(getRating(assetRating.getAverage(), averageRatingBar.getNumStars()));
+			totalCountTextView.setText(getResources().getString(R.string.rating_count, assetRating.getTotalCount()));
 		}
 	}
 
 	@Override
 	public int getDefaultStepCount() {
-		return _userRatingBar.getNumStars();
+		return userRatingBar.getNumStars();
 	}
 
 	@Override
 	public void updateView() {
-		_userRatingBar.setEnabled(isEditable());
-	}
-
-	@Override
-	protected void onFinishInflate() {
-		super.onFinishInflate();
-
-		_userRatingBar = (RatingBar) findViewById(R.id.userRatingBar);
-		_averageRatingBar = (RatingBar) findViewById(R.id.averageRatingBar);
-		_totalCountTextView = (TextView) findViewById(R.id.totalCountTextView);
-
-		_userRatingBar.setOnRatingBarChangeListener(this);
+		userRatingBar.setEnabled(isEditable());
 	}
 
 	@Override
 	public void onRatingChanged(RatingBar ratingBar, float score, boolean fromUser) {
 		if (fromUser) {
 			double normalizedScore = score / ratingBar.getNumStars();
-			getScreenlet().performUserAction(RatingScreenlet.UPDATE_RATING_ACTION, (double) normalizedScore);
+			getScreenlet().performUserAction(UPDATE_RATING_ACTION, normalizedScore);
 		}
 	}
 
-	private void setRatingBarRate(RatingBar ratingBar, float rating) {
-		ratingBar.setRating(rating * ratingBar.getNumStars());
+	@Override
+	protected void onFinishInflate() {
+		super.onFinishInflate();
+
+		userRatingBar = (RatingBar) findViewById(R.id.userRatingBar);
+		averageRatingBar = (RatingBar) findViewById(R.id.averageRatingBar);
+		totalCountTextView = (TextView) findViewById(R.id.totalCountTextView);
+
+		userRatingBar.setOnRatingBarChangeListener(this);
 	}
 
-	private TextView _totalCountTextView;
-	private RatingBar _averageRatingBar;
-	private RatingBar _userRatingBar;
+	private float getRating(double score, int numStars) {
+		return (float) score * numStars;
+	}
+
+	private TextView totalCountTextView;
+	private RatingBar averageRatingBar;
+	private RatingBar userRatingBar;
 }
