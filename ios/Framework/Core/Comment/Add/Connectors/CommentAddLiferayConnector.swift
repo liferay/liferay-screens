958403ab8cb208e187f645a260fccf388c234d3c
Alejandro Hernández <info@alejandrohdezma.com>
2016-08-04 13:11:05 +0200

    LSR-859 create comment add connector

---
+++ ios/Framework/Core/Comment/Add/Connectors/CommentAddLiferayConnector.swift
@@ -0,0 +1,55 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+import UIKit
+
+public class CommentAddLiferayConnector: ServerConnector {
+
+	public let groupId: Int64
+	public let className: String
+	public let classPK: Int64
+	public let body: String?
+
+	public init(groupId: Int64, className: String, classPK: Int64, body: String?) {
+		self.groupId = groupId
+		self.className = className
+		self.classPK = classPK
+		self.body = body
+		super.init()
+	}
+
+
+	override public func validateData() -> ValidationError? {
+		let error = super.validateData()
+
+		if error == nil {
+			if groupId <= 0 {
+				return ValidationError("comment-add-screenlet", "undefined-groupId")
+			}
+
+			if className.isEmpty {
+				return ValidationError("comment-add-screenlet", "undefined-className")
+			}
+
+			if classPK <= 0 {
+				return ValidationError("comment-add-screenlet", "undefined-classPK")
+			}
+
+			if body == nil || body!.isEmpty {
+				return ValidationError("comment-add-screenlet", "empty-body")
+			}
+		}
+
+		return error
+	}
+}
