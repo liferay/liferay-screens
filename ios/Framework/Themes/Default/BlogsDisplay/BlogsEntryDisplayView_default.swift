469b2422d5bea1e806a4aa93a234c8be93b131d6
Sarai Díaz <dgarciasarai@gmail.com>
2016-08-09 13:44:24 +0200

    LSR-860 updated blogs entry view

--- ios/Framework/Themes/Default/BlogsDisplay/BlogsEntryDisplayView_default.swift
+++ ios/Framework/Themes/Default/BlogsDisplay/BlogsEntryDisplayView_default.swift
@@ -16,9 +16,66 @@ import UIKit
 
 public class BlogsEntryDisplayView_default: BaseScreenletView, BlogsDisplayViewModel {
 
-	
+	@IBOutlet weak var imageDisplayScreenlet: ImageDisplayScreenlet?
+	@IBOutlet weak var userPortraitScreenlet: UserPortraitScreenlet?
+
+	@IBOutlet weak var usernameLabel: UILabel?
+	@IBOutlet weak var dateLabel: UILabel?
+
+	@IBOutlet weak var titleLabel: UILabel?
+	@IBOutlet weak var subtitleLabel: UILabel?
+	@IBOutlet weak var contentLabel: UILabel?
+
+	@IBOutlet weak var imageHeightConstraint: NSLayoutConstraint?
+
+	public var contentStyle: String = "font-size:17"
+
 	public var blogsEntry: BlogsEntry? {
 		didSet {
+			if let blogsEntry = blogsEntry {
+
+				//Set image blog if exist
+				let imageId = blogsEntry.coverImageFileEntryId
+				if imageId != 0 {
+					imageHeightConstraint?.constant = 125
+
+					imageDisplayScreenlet?.className = FileEntry.className
+					imageDisplayScreenlet?.classPK = imageId
+					imageDisplayScreenlet?.load()
+				}
+				else {
+					imageHeightConstraint?.constant = 0
+				}
+
+				//Set user portrait image
+				userPortraitScreenlet?.load(userId: blogsEntry.userId)
+
+				//Set username
+				usernameLabel?.text = blogsEntry.userName
+
+				//Set blog display date from timestamp
+				let timeStamp = NSTimeInterval(blogsEntry.displayDate)/1000.0
+				let date = NSDate(timeIntervalSince1970: timeStamp)
+				let dateFormatter = NSDateFormatter()
+				dateFormatter.dateStyle = NSDateFormatterStyle.LongStyle
+				dateFormatter.locale = NSLocale(localeIdentifier: NSLocale.currentLocaleString)
+				let dateString = dateFormatter.stringFromDate(date)
+				dateLabel?.text = dateString
+
+				//Set blog title and subtitle
+				titleLabel?.text = blogsEntry.title
+				subtitleLabel?.text = blogsEntry.subtitle
+
+				//Set html content
+				let content = "<span style=\"\(contentStyle)\">\(blogsEntry.content)</span>"
+
+				let encodedData = content.dataUsingEncoding(NSUTF8StringEncoding)!
+				let attributedOptions : [String: AnyObject] = [
+					NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+					NSCharacterEncodingDocumentAttribute: NSUTF8StringEncoding
+				]
+				contentLabel?.attributedText = try! NSAttributedString(data: encodedData, options: attributedOptions, documentAttributes: nil)
+			}
 		}
 	}
 }
