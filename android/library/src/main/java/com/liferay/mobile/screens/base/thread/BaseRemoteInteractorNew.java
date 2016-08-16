ee961a4ba5a91662a66f5afd34a55d361965a34d
Javier Gamarra <javier.gamarra@liferay.com>
2016-08-16 18:34:06 +0200

    adapt forgot password

---
+++ android/library/src/main/java/com/liferay/mobile/screens/base/thread/BaseRemoteInteractorNew.java
@@ -0,0 +1,11 @@
+package com.liferay.mobile.screens.base.thread;
+
+import com.liferay.mobile.screens.base.thread.event.BasicThreadEvent;
+
+public abstract class BaseRemoteInteractorNew<L, E extends BasicThreadEvent>
+	extends BaseThreadInteractor<L, E> {
+
+	public void onEventMainThread(E event) {
+		super.onEventMainThread(event);
+	}
+}
