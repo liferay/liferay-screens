f11f09c69adc18a6457612ac34222f3220e5c7d6
Javier Gamarra <javier.gamarra@liferay.com>
2016-08-17 07:21:39 +0200

    adapt asset display

--- android/library/src/main/java/com/liferay/mobile/screens/asset/display/interactor/AssetDisplayInteractorImpl.java
+++ android/library/src/main/java/com/liferay/mobile/screens/asset/display/interactor/AssetDisplayInteractorImpl.java
@@ -4,54 +4,42 @@ import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.screens.asset.display.AssetDisplayListener;
 import com.liferay.mobile.screens.asset.list.AssetEntry;
 import com.liferay.mobile.screens.asset.list.interactor.AssetFactory;
-import com.liferay.mobile.screens.base.interactor.BaseRemoteInteractor;
 import com.liferay.mobile.screens.base.interactor.JSONObjectCallback;
-import com.liferay.mobile.screens.base.interactor.JSONObjectEvent;
+import com.liferay.mobile.screens.base.thread.BaseRemoteInteractorNew;
+import com.liferay.mobile.screens.base.thread.event.BasicThreadEvent;
 import com.liferay.mobile.screens.context.SessionContext;
 import com.liferay.mobile.screens.service.v70.ScreensassetentryService;
 import com.liferay.mobile.screens.util.JSONUtil;
 import java.util.Locale;
 import java.util.Map;
-import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * @author Sarai Díaz García
  */
-public class AssetDisplayInteractorImpl extends BaseRemoteInteractor<AssetDisplayListener>
-	implements AssetDisplayInteractor {
+public class AssetDisplayInteractorImpl extends BaseRemoteInteractorNew<AssetDisplayListener, BasicThreadEvent> {
 
-	public AssetDisplayInteractorImpl(int targetScreenletId) {
-		super(targetScreenletId);
+	@Override
+	public BasicThreadEvent execute(Object... args) throws Exception {
+
+		long entryId = (long) args[0];
+
+		Session session = SessionContext.createSessionFromCurrentSession();
+		session.setCallback(new JSONObjectCallback(getTargetScreenletId()));
+		ScreensassetentryService service = new ScreensassetentryService(session);
+		JSONObject jsonObject = service.getAssetEntry(entryId, Locale.getDefault().getLanguage());
+		return new BasicThreadEvent(jsonObject);
 	}
 
 	@Override
-	public void getAssetEntry(long entryId) {
-
-		try {
-			Session session = SessionContext.createSessionFromCurrentSession();
-			session.setCallback(new JSONObjectCallback(getTargetScreenletId()));
-			ScreensassetentryService service = new ScreensassetentryService(session);
-			service.getAssetEntry(entryId, Locale.getDefault().getLanguage());
-		} catch (Exception e) {
-			getListener().onRetrieveAssetFailure(e);
-		}
+	public void onSuccess(BasicThreadEvent event) throws Exception {
+		Map<String, Object> map = JSONUtil.toMap(event.getJSONObject());
+		AssetEntry assetEntry = AssetFactory.createInstance(map);
+		getListener().onRetrieveAssetSuccess(assetEntry);
 	}
 
-	public void onEvent(JSONObjectEvent event) {
-		if (!isValidEvent(event)) {
-			return;
-		}
-
-		if (event.isFailed()) {
-			getListener().onRetrieveAssetFailure(event.getException());
-		} else {
-			try {
-				Map<String, Object> map = JSONUtil.toMap(event.getJSONObject());
-				AssetEntry assetEntry = AssetFactory.createInstance(map);
-				getListener().onRetrieveAssetSuccess(assetEntry);
-			} catch (JSONException e) {
-				getListener().onRetrieveAssetFailure(e);
-			}
-		}
+	@Override
+	public void onFailure(Exception e) {
+		getListener().onRetrieveAssetFailure(e);
 	}
 }
