0c7e0e92499104009b46ee309f74379b9a2acf65
Javier Gamarra <javier.gamarra@liferay.com>
2016-08-17 07:20:26 +0200

    adapt comment load screenlet

--- android/library/src/main/java/com/liferay/mobile/screens/comment/display/interactor/load/CommentLoadInteractor.java
+++
@@ -1,9 +0,0 @@
-package com.liferay.mobile.screens.comment.display.interactor.load;
-
-/**
- * @author Alejandro Hernández
- */
-public interface CommentLoadInteractor {
-
-	void load(long groupId, long commentId);
-}
