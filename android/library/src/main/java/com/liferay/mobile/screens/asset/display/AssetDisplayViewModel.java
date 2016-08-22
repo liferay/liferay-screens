61d4e9574fc2dcc3cb0788dcc9514e7591554b86
Sarai Díaz <dgarciasarai@gmail.com>
2016-08-22 15:52:32 +0200

    LSR-886 added new interface method

--- android/library/src/main/java/com/liferay/mobile/screens/asset/display/AssetDisplayViewModel.java
+++ android/library/src/main/java/com/liferay/mobile/screens/asset/display/AssetDisplayViewModel.java
@@ -1,9 +1,11 @@
 package com.liferay.mobile.screens.asset.display;
 
+import com.liferay.mobile.screens.base.BaseScreenlet;
 import com.liferay.mobile.screens.base.view.BaseViewModel;
 
 /**
  * @author Sarai Díaz García
  */
 public interface AssetDisplayViewModel extends BaseViewModel {
+	void showFinishOperation(BaseScreenlet screenlet);
 }
