28d7987ce5a1c220165c040fc16e5e6a649fbae7
Victor Galan <victor.galan@liferay.com>
2016-08-23 11:10:45 +0200

    LSR-890 Base project

---
+++ ios/Samples/WesterosEmployees/App/WesterosEmployees/Misc/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  WesterosBank
+//
+//  Created by jmWork on 23/04/15.
+//  Copyright (c) 2015 Liferay. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+
+   static let mainRed = UIColor(red: 228.0/255.0, green: 71.0/255.0, blue: 64.0/255.0, alpha: 1)
+
+   static let background = UIColor(red: 214.0/255.0, green: 214.0/255.0, blue: 214.0/255.0, alpha: 1)
+
+}
