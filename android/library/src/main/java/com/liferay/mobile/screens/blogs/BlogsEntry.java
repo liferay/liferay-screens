381267cfe1a3938caf6b392d08e15cf3c2de8a20
Sarai Díaz <dgarciasarai@gmail.com>
2016-08-23 16:23:54 +0200

    LSR-886 created BlogsEntry model class

---
+++ android/library/src/main/java/com/liferay/mobile/screens/blogs/BlogsEntry.java
@@ -0,0 +1,15 @@
+package com.liferay.mobile.screens.blogs;
+
+import com.liferay.mobile.screens.asset.list.AssetEntry;
+import java.util.Map;
+
+/**
+ * @author Sarai Díaz García
+ */
+
+public class BlogsEntry extends AssetEntry {
+
+	public BlogsEntry(Map<String, Object> map) {
+		super(map);
+	}
+}
