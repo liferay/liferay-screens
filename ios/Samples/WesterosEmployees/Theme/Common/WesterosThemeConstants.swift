41813d48b2f534e0a574d72aeccadc17de1812df
Victor Galan <victor.galan@liferay.com>
2016-08-23 11:07:03 +0200

    LSR-890 Add westeros theme

---
+++ ios/Samples/WesterosEmployees/Theme/Common/WesterosThemeConstants.swift
@@ -0,0 +1,18 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+import UIKit
+
+
+let WesterosThemeBasicRed = UIColor(red: 228.0/255.0, green: 71.0/255.0, blue: 64.0/255.0, alpha: 1)
+
