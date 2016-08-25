3e2fe3fdc641c215367d23e4e226808d8f28dc2a
Javier Gamarra <javier.gamarra@liferay.com>
2016-08-25 10:24:33 +0200

    adapt web content List to cache

--- android/library/src/main/java/com/liferay/mobile/screens/webcontent/list/interactor/WebContentListInteractorImpl.java
+++ android/library/src/main/java/com/liferay/mobile/screens/webcontent/list/interactor/WebContentListInteractorImpl.java
@@ -1,19 +1,22 @@
 package com.liferay.mobile.screens.webcontent.list.interactor;
 
+import com.liferay.mobile.android.service.JSONObjectWrapper;
 import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.screens.base.list.interactor.BaseListInteractor;
 import com.liferay.mobile.screens.base.list.interactor.Query;
-import com.liferay.mobile.screens.context.SessionContext;
 import com.liferay.mobile.screens.util.ServiceProvider;
 import com.liferay.mobile.screens.webcontent.WebContent;
 import com.liferay.mobile.screens.webcontent.display.connector.JournalContentConnector;
+import com.liferay.mobile.screens.webcontent.display.interactor.WebContentDisplayEvent;
 import java.util.Map;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * @author Javier Gamarra
  */
-public class WebContentListInteractorImpl extends BaseListInteractor<WebContent, WebContentListInteractorListener> {
+public class WebContentListInteractorImpl
+	extends BaseListInteractor<WebContentListInteractorListener, WebContentDisplayEvent> {
 
 	@Override
 	protected String getIdFromArgs(Object... args) throws Exception {
@@ -29,10 +32,12 @@ public class WebContentListInteractorImpl extends BaseListInteractor<WebContent,
 
 		Session session = getSession();
 
+		JSONObjectWrapper comparator = new JSONObjectWrapper(new JSONObject(query.getComparator()));
+
 		JournalContentConnector journalContentConnector =
 			ServiceProvider.getInstance().getJournalContentConnector(session);
 		return journalContentConnector.getJournalArticles(groupId, _folderId, query.getStartRow(), query.getEndRow(),
-			query.getObjC());
+			comparator);
 	}
 
 	@Override
@@ -46,7 +51,7 @@ public class WebContentListInteractorImpl extends BaseListInteractor<WebContent,
 	}
 
 	@Override
-	protected WebContent createEntity(Map<String, Object> stringObjectMap) {
-		return new WebContent(stringObjectMap, locale);
+	protected WebContentDisplayEvent createEntity(Map<String, Object> stringObjectMap) {
+		return new WebContentDisplayEvent(new WebContent(stringObjectMap, locale));
 	}
 }
