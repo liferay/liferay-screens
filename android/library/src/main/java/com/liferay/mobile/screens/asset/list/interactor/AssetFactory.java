2ede94e20af868f510f3e9acf07545bfc1f33526
Sarai Díaz <dgarciasarai@gmail.com>
2016-08-26 10:18:15 +0200

    LSR-886 added new class to factory

--- android/library/src/main/java/com/liferay/mobile/screens/asset/list/interactor/AssetFactory.java
+++ android/library/src/main/java/com/liferay/mobile/screens/asset/list/interactor/AssetFactory.java
@@ -4,6 +4,7 @@ import com.liferay.mobile.screens.asset.list.AssetEntry;
 import com.liferay.mobile.screens.blogs.BlogsEntry;
 import com.liferay.mobile.screens.ddl.model.Record;
 import com.liferay.mobile.screens.dlfile.display.FileEntry;
+import com.liferay.mobile.screens.user.display.UserAsset;
 import com.liferay.mobile.screens.util.LiferayLocale;
 import com.liferay.mobile.screens.webcontent.WebContent;
 import java.util.Locale;
@@ -33,6 +34,8 @@ public class AssetFactory {
 					return new FileEntry(map);
 				} else if (className.endsWith("BlogsEntry")) {
 					return new BlogsEntry(map);
+				} else if (className.endsWith("User")) {
+					return new UserAsset(map);
 				}
 			}
 		}
