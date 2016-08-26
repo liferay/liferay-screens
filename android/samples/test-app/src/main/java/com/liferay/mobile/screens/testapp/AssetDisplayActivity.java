fc52926b423e8cb14a903584bb4423d29abd7e4a
Sarai Díaz <dgarciasarai@gmail.com>
2016-08-26 10:20:51 +0200

    LSR-886 added new listener examples
    This is an example how to render a custom asset and how to configure an inner screenlet in AssetDisplayScreenlet.

--- android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/AssetDisplayActivity.java
+++ android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/AssetDisplayActivity.java
@@ -43,10 +43,39 @@ public class AssetDisplayActivity extends ThemeActivity implements AssetDisplayL
 	@Override
 	public void onConfigureChildScreenlet(AssetDisplayScreenlet screenlet, BaseScreenlet innerScreenlet,
 		AssetEntry assetEntry) {
+		if (assetEntry.getObjectType().equals("blogsEntry")) {
+			innerScreenlet.setBackgroundColor(ContextCompat.getColor(this, R.color.light_gray_westeros));
+		}
+		info("Configure your screenlet here! -> " + assetEntry.getTitle());
 	}
 
 	@Override
 	public View onRenderCustomAsset(AssetEntry assetEntry) {
+		if (assetEntry.getObjectType().equals("user")) {
+			View view = getLayoutInflater().inflate(R.layout.user_display, null);
+			UserAsset user = (UserAsset) assetEntry;
+
+			TextView greeting = (TextView) view.findViewById(R.id.liferay_user_greeting);
+			UserPortraitScreenlet userPortraitScreenlet =
+				(UserPortraitScreenlet) view.findViewById(R.id.user_portrait_screenlet);
+			TextView name = (TextView) view.findViewById(R.id.liferay_user_name);
+			TextView jobTitle = (TextView) view.findViewById(R.id.liferay_user_jobtitle);
+			TextView email = (TextView) view.findViewById(R.id.liferay_user_email);
+			TextView nickname = (TextView) view.findViewById(R.id.liferay_user_nickname);
+
+			greeting.setText(user.getGreeting());
+
+			userPortraitScreenlet.setUserId(user.getId());
+			userPortraitScreenlet.load();
+
+			name.setText(user.getFirstName() + " " + user.getLastName());
+			jobTitle.setText(user.getJobTitle());
+			email.setText(user.getEmail());
+			nickname.setText(user.getScreenName());
+
+			return view;
+		}
+		return null;
 	}
 
 	private AssetDisplayScreenlet screenlet;
