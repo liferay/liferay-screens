7846fbadd9e042fc425c65b8dee97a5269cff840
Victor Galan <victor.galan@liferay.com>
2016-08-26 09:18:30 +0200

    LSR-890 Add UserActivity

---
+++ android/samples/westerosemployees/src/main/java/com/liferay/mobile/screens/westerosemployees/activities/UserActivity.java
@@ -0,0 +1,9 @@
+package com.liferay.mobile.screens.westerosemployees.activities;
+
+import android.app.Activity;
+
+/**
+ * @author Víctor Galán Grande
+ */
+public class UserActivity extends Activity {
+}
