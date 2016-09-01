607205f04f6ef2de55a30cb7463b0d511f6bc26f
Javier Gamarra <javier.gamarra@liferay.com>
2016-09-01 09:23:33 +0200

    readd sync code

--- android/library/src/main/java/com/liferay/mobile/screens/cache/CacheSyncService.java
+++ android/library/src/main/java/com/liferay/mobile/screens/cache/CacheSyncService.java
@@ -5,7 +5,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import com.liferay.mobile.screens.base.thread.BaseCachedWriteThreadRemoteInteractor;
+import com.liferay.mobile.screens.base.thread.event.OfflineEventNew;
+import com.liferay.mobile.screens.comment.add.interactor.CommentAddInteractorImpl;
+import com.liferay.mobile.screens.comment.display.interactor.CommentEvent;
+import com.liferay.mobile.screens.comment.display.interactor.delete.CommentDeleteInteractorImpl;
+import com.liferay.mobile.screens.comment.display.interactor.update.CommentUpdateInteractorImpl;
+import com.liferay.mobile.screens.context.LiferayServerContext;
 import com.liferay.mobile.screens.context.SessionContext;
+import com.liferay.mobile.screens.ddl.form.interactor.DDLFormEvent;
+import com.liferay.mobile.screens.ddl.form.interactor.add.DDLFormAddRecordInteractorImpl;
+import com.liferay.mobile.screens.ddl.form.interactor.update.DDLFormUpdateRecordInteractorImpl;
+import com.liferay.mobile.screens.ddl.form.interactor.upload.DDLFormDocumentUploadEvent;
+import com.liferay.mobile.screens.ddl.form.interactor.upload.DDLFormDocumentUploadInteractorImpl;
+import com.liferay.mobile.screens.ddl.model.Record;
+import com.liferay.mobile.screens.rating.interactor.RatingEvent;
+import com.liferay.mobile.screens.rating.interactor.delete.RatingDeleteInteractorImpl;
+import com.liferay.mobile.screens.rating.interactor.update.RatingUpdateInteractorImpl;
+import com.liferay.mobile.screens.userportrait.interactor.upload.UserPortraitUploadEvent;
+import com.liferay.mobile.screens.userportrait.interactor.upload.UserPortraitUploadInteractorImpl;
+import com.liferay.mobile.screens.util.EventBusUtil;
+import com.liferay.mobile.screens.util.LiferayLocale;
+import com.liferay.mobile.screens.util.LiferayLogger;
+import java.util.Date;
+import java.util.Locale;
+
+import static com.liferay.mobile.screens.cache.Cache.SEPARATOR;
+import static com.liferay.mobile.screens.comment.display.CommentDisplayScreenlet.DELETE_COMMENT_ACTION;
+import static com.liferay.mobile.screens.rating.RatingScreenlet.DELETE_RATING_ACTION;
 
 /**
  * @author Javier Gamarra
@@ -26,111 +54,130 @@ public class CacheSyncService extends IntentService {
 
 		if (isConnected && SessionContext.isLoggedIn() && SessionContext.getCurrentUser() != null) {
 
-			//Long groupId = LiferayServerContext.getGroupId();
-			//Long userId = SessionContext.getUserId();
+			sync(DDLFormEvent.class, new SyncProvider<DDLFormEvent>() {
+				@Override
+				public DDLFormEvent getOfflineEventNew(DDLFormEvent event) throws Exception {
+					Record record = event.getRecord();
+					BaseCachedWriteThreadRemoteInteractor interactor =
+						record.getRecordId() == 0 ? new DDLFormAddRecordInteractorImpl()
+							: new DDLFormUpdateRecordInteractorImpl();
+					event = (DDLFormEvent) interactor.execute(event);
+					event.setCacheKey(record.getStructureId() + SEPARATOR + record.getRecordId());
+					return event;
+				}
+			});
 
-			//try {
-			//	DB snappyDB = Cache.openDatabase(groupId, userId);
-			//
-			//	String simpleName = getFullId(DDLFormEvent.class);
-			//	String[] keys = snappyDB.findKeys(simpleName);
-			//
-			//	for (String key : keys) {
-			//		DDLFormEvent event = snappyDB.getObject(key, DDLFormEvent.class);
-			//		if (event.isDirty()) {
-			//			Record record = event.getRecord();
-			//			BaseCachedWriteThreadRemoteInteractor interactor =
-			//				record.getRecordId() == 0 ? new DDLFormAddRecordInteractorImpl()
-			//					: new DDLFormUpdateRecordInteractorImpl();
-			//			event = (DDLFormEvent) interactor.execute(event);
-			//
-			//			key += record.getStructureId() + SEPARATOR + record.getRecordId();
-			//			event.setDirty(false);
-			//			event.setSyncDate(new Date());
-			//			snappyDB.put(key, event);
-			//		}
-			//	}
-			//} catch (Exception e) {
-			//	LiferayLogger.e("Error syncing " + DDLFormEvent.class.getSimpleName() + " resources", e);
-			//}
+			sync(CommentEvent.class, new SyncProvider<CommentEvent>() {
+				@Override
+				public CommentEvent getOfflineEventNew(CommentEvent event) throws Exception {
+					BaseCachedWriteThreadRemoteInteractor interactor = getCommentInteractor(event);
+					event = (CommentEvent) interactor.execute(event);
+					event.setCacheKey(String.valueOf(event.getCommentId()));
+					event.setDeleted(DELETE_COMMENT_ACTION.equals(event.getActionName()));
+					return event;
+				}
 
-			//sync(snappyDB, DDLFormEvent.class, new SyncConsumer() {
-			//	@Override
-			//	public void sync(OfflineEventNew event) throws Exception {
-			//		BaseCachedWriteThreadRemoteInteractor interactor =
-			//			((DDLFormEvent) event).getRecord().getRecordId() == 0 ? new DDLFormAddRecordInteractorImpl()
-			//				: new DDLFormUpdateRecordInteractorImpl();
-			//		interactor.execute(event);
-			//	}
-			//});
-			//
-			//sync(snappyDB, UserPortraitUploadEvent.class, new SyncConsumer() {
-			//	@Override
-			//	public void sync(OfflineEventNew event) throws Exception {
-			//		UserPortraitUploadInteractorImpl interactor = new UserPortraitUploadInteractorImpl();
-			//		interactor.online((UserPortraitUploadEvent) event);
-			//	}
-			//});
-			//
-			//sync(snappyDB, DDLFormDocumentUploadEvent.class, new SyncConsumer() {
-			//	@Override
-			//	public void sync(OfflineEventNew event) throws Exception {
-			//		BaseCachedWriteThreadRemoteInteractor interactor = new DDLFormDocumentUploadInteractorImpl();
-			//		interactor.execute(event);
-			//	}
-			//});
-			//
-			//sync(snappyDB, CommentEvent.class, new SyncConsumer() {
-			//	@Override
-			//	public void sync(OfflineEventNew event) throws Exception {
-			//		CommentEvent commentEvent = (CommentEvent) event;
-			//		BaseCachedWriteThreadRemoteInteractor interactor = getCommentInteractor(commentEvent);
-			//		interactor.execute(commentEvent);
-			//	}
-			//
-			//	@NonNull
-			//	private BaseCachedWriteThreadRemoteInteractor getCommentInteractor(CommentEvent commentEvent) {
-			//		if (DELETE_COMMENT_ACTION.equals(commentEvent.getActionName())) {
-			//			return new CommentDeleteInteractorImpl();
-			//		} else if (commentEvent.getCommentId() == 0) {
-			//			return new CommentAddInteractorImpl();
-			//		} else {
-			//			return new CommentUpdateInteractorImpl();
-			//		}
-			//	}
-			//});
-			//
-			//sync(snappyDB, RatingEvent.class, new SyncConsumer() {
-			//	@Override
-			//	public void sync(OfflineEventNew event) throws Exception {
-			//		BaseCachedWriteThreadRemoteInteractor interactor =
-			//			DELETE_RATING_ACTION.equals(event.getActionName()) ? new RatingDeleteInteractorImpl()
-			//				: new RatingUpdateInteractorImpl();
-			//		interactor.execute(event);
-			//	}
-			//});
-			//
-			//snappyDB.close();
+				@NonNull
+				private BaseCachedWriteThreadRemoteInteractor getCommentInteractor(CommentEvent commentEvent) {
+					if (DELETE_COMMENT_ACTION.equals(commentEvent.getActionName())) {
+						return new CommentDeleteInteractorImpl();
+					} else if (commentEvent.getCommentId() == 0) {
+						return new CommentAddInteractorImpl();
+					} else {
+						return new CommentUpdateInteractorImpl();
+					}
+				}
+			});
+
+			sync(RatingEvent.class, new SyncProvider<RatingEvent>() {
+				@Override
+				public RatingEvent getOfflineEventNew(RatingEvent event) throws Exception {
+					BaseCachedWriteThreadRemoteInteractor interactor =
+						DELETE_RATING_ACTION.equals(event.getActionName()) ? new RatingDeleteInteractorImpl()
+							: new RatingUpdateInteractorImpl();
+					event = (RatingEvent) interactor.execute(event);
+					event.setCacheKey(event.getClassName() + SEPARATOR + event.getClassPK());
+					event.setDeleted(DELETE_RATING_ACTION.equals(event.getActionName()));
+					return event;
+				}
+			});
+
+			sync(UserPortraitUploadEvent.class, new SyncProvider<UserPortraitUploadEvent>() {
+				@Override
+				public UserPortraitUploadEvent getOfflineEventNew(UserPortraitUploadEvent event) throws Exception {
+					UserPortraitUploadInteractorImpl interactor = new UserPortraitUploadInteractorImpl();
+					EventBusUtil.register(this);
+					interactor.execute(event);
+					return null;
+				}
+			});
+
+			sync(DDLFormDocumentUploadEvent.class, new SyncProvider<DDLFormDocumentUploadEvent>() {
+				@Override
+				public DDLFormDocumentUploadEvent getOfflineEventNew(DDLFormDocumentUploadEvent event)
+					throws Exception {
+					DDLFormDocumentUploadInteractorImpl interactor = new DDLFormDocumentUploadInteractorImpl();
+					EventBusUtil.register(this);
+					interactor.execute(event);
+					return null;
+				}
+			});
 		}
 		CacheReceiver.completeWakefulIntent(intent);
 	}
 
-	//private void sync(DB snappyDB, Class aClass, SyncConsumer syncConsumer) {
-	//	try {
-	//		String simpleName = getFullId(aClass);
-	//		String[] keys = snappyDB.findKeys(simpleName);
-	//
-	//		for (String key : keys) {
-	//			OfflineEventNew event = (OfflineEventNew) snappyDB.getObject(key, aClass);
-	//			if (event.isDirty()) {
-	//				syncConsumer.sync(event);
-	//				event.setDirty(false);
-	//				event.setSyncDate(new Date());
-	//				snappyDB.put(key, event);
-	//			}
-	//		}
-	//	} catch (Exception e) {
-	//		LiferayLogger.e("Error syncing " + aClass.getSimpleName() + " resources", e);
-	//	}
-	//}
+	private void sync(Class aClass, SyncProvider syncProvider) {
+		Long groupId = LiferayServerContext.getGroupId();
+		Long userId = SessionContext.getUserId();
+		Locale locale = LiferayLocale.getDefaultLocale();
+
+		try {
+			String[] keys = Cache.findKeys(aClass, groupId, userId, locale, 0, Integer.MAX_VALUE);
+			for (String key : keys) {
+
+				OfflineEventNew event = Cache.getObject(aClass, groupId, userId, key);
+				if (event.isDirty()) {
+					event = syncProvider.getOfflineEventNew(event);
+					if (event != null) {
+						event.setLocale(locale);
+						event.setDirty(false);
+						event.setSyncDate(new Date());
+						if (event.isDeleted()) {
+							Cache.deleteObject(event);
+						} else {
+							Cache.storeObject(event);
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			LiferayLogger.e("Error syncing " + aClass.getSimpleName() + " resources", e);
+		}
+	}
+
+	public void onEventMainThread(UserPortraitUploadEvent event) {
+		event.setCacheKey(String.valueOf(event.getUserId()));
+		storeEvent(event);
+	}
+
+	public void onEventMainThread(DDLFormDocumentUploadEvent event) {
+		//FIXME !
+		//event.setCacheKey(event.getDocumentField());
+		storeEvent(event);
+	}
+
+	private void storeEvent(OfflineEventNew event) {
+		try {
+			event.setDirty(false);
+			event.setSyncDate(new Date());
+			Cache.storeObject(event);
+		} catch (Exception e) {
+			LiferayLogger.e("Error syncing " + event.getClass().getSimpleName() + " resources", e);
+		}
+	}
+
+	private interface SyncProvider<E extends OfflineEventNew> {
+
+		E getOfflineEventNew(E event) throws Exception;
+	}
 }
