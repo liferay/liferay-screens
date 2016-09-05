c5091c74a707022d43b5b3acf37914d10a7133e0
JM <jose.navarro@liferay.com>
2016-09-05 14:57:57 +0200

    LSR-893 Simpler

---
+++ ios/Samples/Showcase-swift/LiferayScreens-Showcase-Swift/ViewControllers/CommentDisplayScreenletViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CommentDisplayScreenletViewController.swift
+//  LiferayScreens-Showcase-Swift
+//
+//  Created by jmWork on 05/09/16.
+//  Copyright © 2016 Liferay. All rights reserved.
+//
+
+import Cocoa
+
+class CommentDisplayScreenletViewController: UIViewController {
+
+}
