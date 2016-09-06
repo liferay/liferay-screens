0c027e0034f1e1f142dbca43d1c55e439a53583b
Victor Galan <victor.galan@liferay.com>
2016-09-06 16:22:21 +0200

    LSR-890 Fling listener only in the “header” of the card

--- android/samples/westerosemployees/src/main/java/com/liferay/mobile/screens/westerosemployees/gestures/FlingTouchListener.java
+++ android/samples/westerosemployees/src/main/java/com/liferay/mobile/screens/westerosemployees/gestures/FlingTouchListener.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import com.liferay.mobile.screens.util.LiferayLogger;
+import com.liferay.mobile.screens.westerosemployees.Views.Card;
+import com.liferay.mobile.screens.westerosemployees.utils.PixelUtil;
 
 /**
  * @author Javier Gamarra
@@ -11,26 +14,31 @@ import android.view.View;
 public class FlingTouchListener implements View.OnTouchListener {
 
 	public FlingTouchListener(Context context, FlingListener flingListener) {
-		_gestureDetector = new GestureDetector(context, new GestureDetectorListener());
-		_flingListener = flingListener;
+		gestureDetector = new GestureDetector(context, new GestureDetectorListener());
+		this.flingListener = flingListener;
+		cardSizeMinimizedPx = PixelUtil.pixelFromDp(context, Card.CARD_SIZE);
 	}
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
-		_gestureDetector.onTouchEvent(event);
+		gestureDetector.onTouchEvent(event);
 		return true;
 	}
 
-	private GestureDetector _gestureDetector;
-	private FlingListener _flingListener;
+	private GestureDetector gestureDetector;
+	private FlingListener flingListener;
 	private static final float SWIPE_VELOCITY_THRESHOLD = 10f;
 	private static final float SWIPE_MOVEMENT_THRESHOLD = 100f;
+	private int cardSizeMinimizedPx;
 
 	private class GestureDetectorListener extends GestureDetector.SimpleOnGestureListener {
 
 		@Override
 		public boolean onSingleTapConfirmed(MotionEvent e) {
-			_flingListener.onFling(FlingListener.Movement.TOUCH);
+			if (e.getY() >= cardSizeMinimizedPx) {
+				return true;
+			}
+			flingListener.onFling(FlingListener.Movement.TOUCH);
 			return true;
 		}
 
@@ -41,22 +49,25 @@ public class FlingTouchListener implements View.OnTouchListener {
 
 		@Override
 		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			if (e1.getY() >= cardSizeMinimizedPx && e2.getY() >= cardSizeMinimizedPx) {
+				return true;
+			}
 			float swipeY = e2.getY() - e1.getY();
 			float swipeX = e2.getX() - e1.getX();
 			if (Math.abs(swipeY) > SWIPE_MOVEMENT_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
 				if (swipeY > 0) {
-					_flingListener.onFling(FlingListener.Movement.DOWN);
+					flingListener.onFling(FlingListener.Movement.DOWN);
 				}
 				else {
-					_flingListener.onFling(FlingListener.Movement.UP);
+					flingListener.onFling(FlingListener.Movement.UP);
 				}
 			}
 			else if (Math.abs(e2.getX() - e1.getX()) > SWIPE_MOVEMENT_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
 				if (swipeX > 0) {
-					_flingListener.onFling(FlingListener.Movement.LEFT);
+					flingListener.onFling(FlingListener.Movement.LEFT);
 				}
 				else {
-					_flingListener.onFling(FlingListener.Movement.RIGHT);
+					flingListener.onFling(FlingListener.Movement.RIGHT);
 				}
 			}
 
