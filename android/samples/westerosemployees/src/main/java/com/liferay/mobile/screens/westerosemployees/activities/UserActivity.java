1271e0a7f7226904d86b44533d01f78887c322e4
Victor Galan <victor.galan@liferay.com>
2016-09-12 15:19:20 +0200

    LSR-890 Add user portrait on user activity

--- android/samples/westerosemployees/src/main/java/com/liferay/mobile/screens/westerosemployees/activities/UserActivity.java
+++ android/samples/westerosemployees/src/main/java/com/liferay/mobile/screens/westerosemployees/activities/UserActivity.java
@@ -1,16 +1,40 @@
 package com.liferay.mobile.screens.westerosemployees.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.liferay.mobile.screens.context.SessionContext;
 import com.liferay.mobile.screens.westerosemployees.R;
 
 /**
  * @author Víctor Galán Grande
  */
-public class UserActivity extends WesterosActivity {
-
+public class UserActivity extends WesterosActivity implements View.OnClickListener {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.user);
+
+		bindViews();
+	}
+
+	private void bindViews() {
+		TextView userNameTextView = (TextView) findViewById(R.id.liferay_username);
+		userNameTextView.setOnClickListener(this);
+
+		findViewById(R.id.userscreenlet_home).setOnClickListener(this);
+
+		userNameTextView.setText(SessionContext.getCurrentUser().getFullName());
+	}
+
+	@Override
+	public void onBackPressed() {
+		Intent intent = new Intent(this, MainActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		startActivity(intent);
+		finish();
+	}
 	}
 }
