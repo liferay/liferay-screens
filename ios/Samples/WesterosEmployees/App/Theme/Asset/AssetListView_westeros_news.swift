298b92ff42a96345b4b1ad91ab5cb0fd1c1f5f91
Alejandro Hernández <info@alejandrohdezma.com>
2016-09-14 14:50:03 +0200

    LSR-890 fix bug in news list

--- ios/Samples/WesterosEmployees/App/Theme/Asset/AssetListView_westeros_news.swift
+++ ios/Samples/WesterosEmployees/App/Theme/Asset/AssetListView_westeros_news.swift
@@ -19,6 +19,8 @@ class AssetListView_westeros_news: AssetListView_default {
 	let BlogCellId = "blogsEntryCell"
 	let DocumentCellId = "documentCell"
 
+	static let MinimumTimeDifference: Double = 5000
+
 	//MARK: BaseScreenletView
 
 	override func createProgressPresenter() -> ProgressPresenter {
@@ -80,25 +82,21 @@ class AssetListView_westeros_news: AssetListView_default {
 			cell.accessoryView = nil
 
 			if asset.classNameId == AssetClasses.getClassNameId(AssetClassNameKey_BlogsEntry) {
-				guard let blogCell = cell as? BlogsEntryTableViewCell_westeros else {
-					return
-				}
+				guard let blogCell = cell as? BlogsEntryTableViewCell_westeros else { return }
 
 				//Load blog information
 				let blog = BlogsEntry(attributes: asset.attributes)
 				blogCell.imageEntryId = blog.coverImageFileEntryId
 				blogCell.title = blog.title
-				blogCell.subtitle = blog.modifiedDate == blog.createDate ?
-					"New blog" : "Updated blog"
+				blogCell.subtitle = blog.modifiedDate.timeIntervalSinceDate(blog.createDate)
+					< AssetListView_westeros_news.MinimumTimeDifference ? "New blog" : "Updated blog"
 			} else {
-				guard let docCell = cell as? DocumentationTableViewCell_westeros else {
-					return
-				}
+				guard let docCell = cell as? DocumentationTableViewCell_westeros else { return }
 
 				//Load blog information
 				let document = FileEntry(attributes: asset.attributes)
-				docCell.title = document.modifiedDate == document.createDate ?
-					"New file" : "Updated file"
+				docCell.title = document.modifiedDate.timeIntervalSinceDate(document.createDate)
+					< AssetListView_westeros_news.MinimumTimeDifference ? "New file" : "Updated file"
 				docCell.fileDescription = document.title
 				docCell.fileExtension = document.fileExtension!
 			}
