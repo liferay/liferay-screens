901c2387ebdf3734ebfb4cbd7069ef3f85d0b414
Victor Galan <victor.galan@liferay.com>
2016-09-14 12:20:16 +0200

    LSR-890 Comment list westeros theme

---
+++ android/viewsets/westeros/src/main/java/com/liferay/mobile/screens/viewsets/westeros/comment/list/CommentListView.java
@@ -0,0 +1,35 @@
+package com.liferay.mobile.screens.viewsets.westeros.comment.list;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+import com.liferay.mobile.screens.viewsets.westeros.R;
+
+/**
+ * @author Alejandro Hernández
+ */
+public class CommentListView extends com.liferay.mobile.screens.viewsets.defaultviews.comment.list.CommentListView {
+
+	private TextView emptyListTextView;
+
+	public CommentListView(Context context) {
+		super(context);
+	}
+
+	public CommentListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public CommentListView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	public CommentListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+	}
+
+	@Override
+	protected int getItemLayoutId() {
+		return R.layout.comment_row_westeros;
+	}
+}
