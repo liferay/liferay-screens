5a1bd24f519c62e5f469036ae6c79a92fddec4fa
Alejandro Hernández <info@alejandrohdezma.com>
2016-10-13 14:11:15 +0200

    LSR-918 created UITextField child for default theme

---
+++ ios/Framework/Themes/Default/CustomViews/DefaultTextField.swift
@@ -0,0 +1,81 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+import UIKit
+
+public class DefaultTextField: UITextField {
+	
+	//MARK: IBInspectable
+	
+	@IBInspectable public var defaultColor: UIColor = UIColor.lightGrayColor()
+	
+	@IBInspectable public var highlightColor: UIColor = DefaultThemeBasicBlue
+	
+	@IBInspectable public var leftImage: UIImage? {
+		didSet {
+			if let image = leftImage {
+				
+				let icon = UIImageView(image: image)
+				
+				icon.frame = CGRectMake(0, 0, self.frame.height, image.size.height)
+				icon.contentMode = .Center
+				
+				self.leftViewMode = .Always
+				self.leftView = icon
+			}
+		}
+	}
+	
+	
+	//MARK: Initializers
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		setup()
+	}
+	
+	public required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		
+		setup()
+	}
+	
+	public override func prepareForInterfaceBuilder() {
+		setup()
+	}
+	
+	
+	//MARK: Internal methods
+	
+	internal func setup() {
+		self.layer.cornerRadius = 4.0
+		self.layer.borderWidth = 1.0
+		self.layer.borderColor = defaultColor.CGColor
+	}
+	
+	
+	//MARK: UITextField
+	
+	public override func resignFirstResponder() -> Bool {
+		self.layer.borderColor = defaultColor.CGColor
+		
+		return super.resignFirstResponder()
+	}
+	
+	public override func becomeFirstResponder() -> Bool {
+		self.layer.borderColor = highlightColor.CGColor
+		
+		return super.becomeFirstResponder()
+	}
+}
