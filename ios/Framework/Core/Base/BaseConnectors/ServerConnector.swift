fe2dfc81d339d6c68dff06191dbfe9ee0ee6b2c0
Victor Galan <victor.galan@liferay.com>
2016-10-14 11:53:13 +0200

    Change concurrent operations number

--- ios/Framework/Core/Base/BaseConnectors/ServerConnector.swift
+++ ios/Framework/Core/Base/BaseConnectors/ServerConnector.swift
@@ -23,7 +23,7 @@ import UIKit
 		static func addConnector(connector: ServerConnector) {
 			if queue == nil {
 				queue = NSOperationQueue()
-				queue!.maxConcurrentOperationCount = 1
+				queue!.maxConcurrentOperationCount = 4
 			}
 
 			queue!.addOperation(connector)
