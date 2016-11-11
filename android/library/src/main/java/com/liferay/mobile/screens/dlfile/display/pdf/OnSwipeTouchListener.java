7da58b49ef3edc0b5cc35cad148b3856a52eaa04
Sarai Díaz <dgarciasarai@gmail.com>
2016-11-11 13:36:04 +0100

    LSR-928 Created class for swipe left and right

---
+++ android/library/src/main/java/com/liferay/mobile/screens/dlfile/display/pdf/OnSwipeTouchListener.java
@@ -0,0 +1,64 @@
+package com.liferay.mobile.screens.dlfile.display.pdf;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * @author Sarai Díaz García
+ */
+
+public class OnSwipeTouchListener implements View.OnTouchListener {
+
+	private final GestureDetector gestureDetector;
+
+	protected OnSwipeTouchListener(Context ctx) {
+		gestureDetector = new GestureDetector(ctx, new GestureListener());
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		return gestureDetector.onTouchEvent(event);
+	}
+
+	private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
+
+		private static final int SWIPE_THRESHOLD = 100;
+		private static final int SWIPE_VELOCITY = 100;
+
+		@Override
+		public boolean onDown(MotionEvent e) {
+			return true;
+		}
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			boolean result = false;
+			try {
+				float diffY = e2.getY() - e1.getY();
+				float diffX = e2.getX() - e1.getX();
+				if (Math.abs(diffX) > Math.abs(diffY)) {
+					if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY) {
+						if (diffX > 0) {
+							onSwipeRight();
+						} else {
+							onSwipeLeft();
+						}
+					}
+					result = true;
+				}
+				result = true;
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+			return result;
+		}
+	}
+
+	public void onSwipeRight() {
+	}
+
+	public void onSwipeLeft() {
+	}
+}
