4e7e70c647765e61d4446f8d93b4663819e72489
Victor Galan <victor.galan@liferay.com>
2016-11-11 12:54:31 +0100

    LSR-927 Subclass of table cell default for change cell background color

---
+++ ios/Samples/WesterosBank/Theme/DDL/FormScreenlet/DDLCustomFieldSeverityTableCell_westeros.swift
@@ -0,0 +1,19 @@
+//
+//  DDLCustomFieldSeverityTableCell_westeros.swift
+//  WesterosBank
+//
+//  Created by Victor Galán on 11/11/16.
+//  Copyright © 2016 Liferay. All rights reserved.
+//
+
+import UIKit
+import LiferayScreens
+
+class DDLCustomFieldSeverityTableCell_westeros : DDLFieldSelectTableCell_default {
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+
+		cellBackgroundColor = WesterosThemeBasicRed
+	}
+}
