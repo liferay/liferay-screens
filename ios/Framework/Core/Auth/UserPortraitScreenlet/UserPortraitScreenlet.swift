8725cbfd49657267b64b736bc5d74b9f7e7c0954
Sarai Díaz <dgarciasarai@gmail.com>
2017-01-05 09:09:53 +0100

    LSR-935 Added comments to UserPortraitScreenlet

--- ios/Framework/Core/Auth/UserPortraitScreenlet/UserPortraitScreenlet.swift
+++ ios/Framework/Core/Auth/UserPortraitScreenlet/UserPortraitScreenlet.swift
@@ -16,21 +16,50 @@ import UIKit
 
 @objc public protocol UserPortraitScreenletDelegate : BaseScreenletDelegate {
 
+	/// Called when an image is received from the server. You can then apply image filters 
+	/// (grayscale, for example) and return the new image. You can return the original image 
+	/// supplied as the argument if you don’t want to modify it.
+	///
+	/// - Parameters:
+	///   - screenlet
+	///   - image: user portrait image.
+	/// - Returns: original or modify image.
 	optional func screenlet(screenlet: UserPortraitScreenlet,
 			onUserPortraitResponseImage image: UIImage) -> UIImage
 
+	/// Called when an error occurs in the process.
+	/// The NSError object describes the error.
+	///
+	/// - Parameters:
+	///   - screenlet
+	///   - error: error while retrieving user portrait image.
 	optional func screenlet(screenlet: UserPortraitScreenlet,
 			onUserPortraitError error: NSError)
 
+	/// Called when a new portrait is uploaded to the server. You receive the user 
+	/// attributes as a parameter.
+	///
+	/// - Parameters:
+	///   - screenlet
+	///   - attributes: user portrait attributes.
 	optional func screenlet(screenlet: UserPortraitScreenlet,
 			onUserPortraitUploaded attributes: [String:AnyObject])
 
+	/// Called when an error occurs in the upload process.
+	/// The NSError object describes the error.
+	///
+	/// - Parameters:
+	///   - screenlet
+	///   - error: error while uploading the user portrait image.
 	optional func screenlet(screenlet: UserPortraitScreenlet,
 			onUserPortraitUploadError error: NSError)
 }
 
 
 public class UserPortraitScreenlet: BaseScreenlet {
+
+
+	//MARK: Inspectables
 
 	@IBInspectable public var borderWidth: CGFloat = 1.0 {
 		didSet {
@@ -78,68 +107,6 @@ public class UserPortraitScreenlet: BaseScreenlet {
 		screenletView?.editable = self.editable
 	}
 
-	public func loadLoggedUserPortrait() -> Bool {
-		guard let userId = SessionContext.currentContext?.user.userId else {
-			return false
-		}
-
-		let interactor = DownloadUserPortraitInteractor(
-			screenlet: self,
-			userId: userId)
-
-		loadedUserId = userId
-
-		return performAction(name: "load-portrait", sender: interactor)
-	}
-
-	public func load(portraitId portraitId: Int64, uuid: String, male: Bool = true) -> Bool {
-		let interactor = DownloadUserPortraitInteractor(
-				screenlet: self,
-				portraitId: portraitId,
-				uuid: uuid,
-				male: male)
-
-		loadedUserId = nil
-
-		return performAction(name: "load-portrait", sender: interactor)
-	}
-
-	public func load(userId userId: Int64) -> Bool {
-		let interactor = DownloadUserPortraitInteractor(
-				screenlet: self,
-				userId: userId)
-
-		loadedUserId = userId
-
-		return performAction(name: "load-portrait", sender: interactor)
-	}
-
-	public func load(companyId companyId: Int64, emailAddress: String) -> Bool {
-		let interactor = DownloadUserPortraitInteractor(
-				screenlet: self,
-				companyId: companyId,
-				emailAddress: emailAddress)
-
-		loadedUserId = nil
-
-		return performAction(name: "load-portrait", sender: interactor)
-	}
-
-	public func load(companyId companyId: Int64, screenName: String) -> Bool {
-		let interactor = DownloadUserPortraitInteractor(
-				screenlet: self,
-				companyId: companyId,
-				screenName: screenName)
-
-		loadedUserId = nil
-
-		return performAction(name: "load-portrait", sender: interactor)
-	}
-
-	public func loadPlaceholder() {
-		viewModel.image = nil
-	}
-
 	override public func createInteractor(name name: String, sender: AnyObject?) -> Interactor? {
 		let interactor: Interactor?
 
@@ -163,8 +130,8 @@ public class UserPortraitScreenlet: BaseScreenlet {
 				}
 				else {
 					self.userPortraitDelegate?.screenlet?(self,
-							onUserPortraitError: NSError.errorWithCause(.InvalidServerResponse,
-									message: "Could not load user portrait image."))
+					                                      onUserPortraitError: NSError.errorWithCause(.InvalidServerResponse,
+															message: "Could not load user portrait image."))
 
 					self.loadedUserId = nil
 					self.setPortraitImage(nil)
@@ -192,9 +159,9 @@ public class UserPortraitScreenlet: BaseScreenlet {
 			}
 
 			let uploadInteractor = UploadUserPortraitInteractor(
-					screenlet: self,
-					userId: userId,
-					image: image)
+				screenlet: self,
+				userId: userId,
+				image: image)
 			interactor = uploadInteractor
 
 			uploadInteractor.cacheStrategy = CacheStrategyType(rawValue: self.offlinePolicy ?? "") ?? .RemoteFirst
@@ -209,12 +176,104 @@ public class UserPortraitScreenlet: BaseScreenlet {
 			uploadInteractor.onFailure = {
 				self.userPortraitDelegate?.screenlet?(self, onUserPortraitUploadError: $0)
 			}
-
+			
 		default:
 			interactor = nil
 		}
-
+		
 		return interactor
+	}
+
+
+	//MARK: Public methods
+
+	/// Loads the user portrait that correspond to the user logged.
+	///
+	/// - Returns: true if suceed, false otherwise.
+	public func loadLoggedUserPortrait() -> Bool {
+		guard let userId = SessionContext.currentContext?.user.userId else {
+			return false
+		}
+
+		let interactor = DownloadUserPortraitInteractor(
+			screenlet: self,
+			userId: userId)
+
+		loadedUserId = userId
+
+		return performAction(name: "load-portrait", sender: interactor)
+	}
+
+	/// Loads the user portrait image.
+	///
+	/// - Parameters:
+	///   - portraitId: portrait identifier.
+	///   - uuid: user portrait unique identifier.
+	///   - male: true if the user is male, false otherwise.
+	/// - Returns: true if succeed, false otherwise.
+	public func load(portraitId portraitId: Int64, uuid: String, male: Bool = true) -> Bool {
+		let interactor = DownloadUserPortraitInteractor(
+				screenlet: self,
+				portraitId: portraitId,
+				uuid: uuid,
+				male: male)
+
+		loadedUserId = nil
+
+		return performAction(name: "load-portrait", sender: interactor)
+	}
+
+	/// Loads the user portrait image throught user identifier.
+	///
+	/// - Parameter userId: user identifier.
+	/// - Returns: true if succeed, false otherwise.
+	public func load(userId userId: Int64) -> Bool {
+		let interactor = DownloadUserPortraitInteractor(
+				screenlet: self,
+				userId: userId)
+
+		loadedUserId = userId
+
+		return performAction(name: "load-portrait", sender: interactor)
+	}
+
+	/// Loads the user portrait throught company identifier and email address.
+	///
+	/// - Parameters:
+	///   - companyId: company identifier.
+	///   - emailAddress: user email.
+	/// - Returns: true if succeed, false otherwise.
+	public func load(companyId companyId: Int64, emailAddress: String) -> Bool {
+		let interactor = DownloadUserPortraitInteractor(
+				screenlet: self,
+				companyId: companyId,
+				emailAddress: emailAddress)
+
+		loadedUserId = nil
+
+		return performAction(name: "load-portrait", sender: interactor)
+	}
+
+	/// Loads the user portrait throught company identifier and user screen name.
+	///
+	/// - Parameters:
+	///   - companyId: company identifier.
+	///   - screenName: user screen name.
+	/// - Returns: <#return value description#>
+	public func load(companyId companyId: Int64, screenName: String) -> Bool {
+		let interactor = DownloadUserPortraitInteractor(
+				screenlet: self,
+				companyId: companyId,
+				screenName: screenName)
+
+		loadedUserId = nil
+
+		return performAction(name: "load-portrait", sender: interactor)
+	}
+
+	/// Loads the user portrait placeholder when the user portrait is empty.
+	public func loadPlaceholder() {
+		viewModel.image = nil
 	}
 
 
