237ed3035d31770df28bea1f0b45a1d88809cc73
Sarai Díaz <dgarciasarai@gmail.com>
2017-01-10 14:36:03 +0100

    SF

--- android/library/src/main/java/com/liferay/mobile/screens/user/interactor/GetUserInteractor.java
+++ android/library/src/main/java/com/liferay/mobile/screens/user/interactor/GetUserInteractor.java
@@ -1,77 +1,68 @@
 package com.liferay.mobile.screens.user.interactor;
 
 import android.support.annotation.NonNull;
-
-import com.liferay.mobile.screens.user.GetUserListener;
 import com.liferay.mobile.screens.auth.login.connector.UserConnector;
 import com.liferay.mobile.screens.base.interactor.BaseRemoteInteractor;
 import com.liferay.mobile.screens.base.interactor.event.BasicEvent;
 import com.liferay.mobile.screens.context.LiferayServerContext;
-import com.liferay.mobile.screens.context.SessionContext;
 import com.liferay.mobile.screens.context.User;
+import com.liferay.mobile.screens.user.GetUserListener;
 import com.liferay.mobile.screens.util.ServiceProvider;
-
 import org.json.JSONObject;
 
-public class GetUserInteractor extends BaseRemoteInteractor<GetUserListener, BasicEvent>
-{
-    private String textValue;
-    private String getUserByAttribute;
+public class GetUserInteractor extends BaseRemoteInteractor<GetUserListener, BasicEvent> {
 
-    @Override
-    public BasicEvent execute(Object... args) throws Exception
-    {
-        textValue = (String) args[0];
-        getUserByAttribute = (String) args[1];
+	private String textValue;
+	private String getUserByAttribute;
 
-        validate();
+	@Override
+	public BasicEvent execute(Object... args) throws Exception {
+		textValue = (String) args[0];
+		getUserByAttribute = (String) args[1];
 
-        JSONObject jsonObject = getJSONObject();
-        return new BasicEvent(jsonObject);
-    }
+		validate();
 
-    @Override
-    public void onSuccess(BasicEvent event)
-    {
-        getListener().onGetUserSuccess(new User(event.getJSONObject()));
-    }
+		JSONObject jsonObject = getJSONObject();
+		return new BasicEvent(jsonObject);
+	}
 
-    @Override
-    public void onFailure(BasicEvent event)
-    {
-        getListener().onGetUserFailure(event.getException());
-    }
+	@Override
+	public void onSuccess(BasicEvent event) {
+		getListener().onGetUserSuccess(new User(event.getJSONObject()));
+	}
 
-    private void validate()
-    {
-        if (null == textValue || textValue.isEmpty())
-        {
-            throw new IllegalArgumentException("Text value cannot be empty");
-        }
-        if (null == getUserByAttribute || getUserByAttribute.isEmpty())
-        {
-            getUserByAttribute = "emailAddress";
-        }
-    }
+	@Override
+	public void onFailure(BasicEvent event) {
+		getListener().onGetUserFailure(event.getException());
+	}
 
-    @NonNull
-    private JSONObject getJSONObject() throws Exception
-    {
-        long companyId = LiferayServerContext.getCompanyId();
+	private void validate() {
+		if (null == textValue || textValue.isEmpty()) {
+			throw new IllegalArgumentException("Text value cannot be empty");
+		}
+		if (null == getUserByAttribute || getUserByAttribute.isEmpty()) {
+			getUserByAttribute = "emailAddress";
+		}
+	}
 
-        UserConnector connector = getUserConnector();
+	@NonNull
+	private JSONObject getJSONObject() throws Exception {
+		long companyId = LiferayServerContext.getCompanyId();
 
-        switch(getUserByAttribute)
-        {
-            case "userId" : return connector.getUserById(Long.parseLong(textValue));
-            case "screenName" : return connector.getUserByScreenName(companyId, textValue);
-            case "emailAddress":
-            default : return connector.getUserByEmailAddress(companyId, textValue);
-        }
-    }
+		UserConnector connector = getUserConnector();
 
-    public UserConnector getUserConnector()
-    {
-        return ServiceProvider.getInstance().getUserConnector(getSession());
-    }
+		switch (getUserByAttribute) {
+			case "userId":
+				return connector.getUserById(Long.parseLong(textValue));
+			case "screenName":
+				return connector.getUserByScreenName(companyId, textValue);
+			case "emailAddress":
+			default:
+				return connector.getUserByEmailAddress(companyId, textValue);
+		}
+	}
+
+	public UserConnector getUserConnector() {
+		return ServiceProvider.getInstance().getUserConnector(getSession());
+	}
 }
