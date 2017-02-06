8d31c82f4f9deb7b0769c5aab0061d46d9d11cf0
Victor Galan <victor.galan@liferay.com>
2017-02-06 15:39:46 +0100

    LSR-956 Add DDLList

---
+++ ios/Samples/Showcase-ObjectiveC/LiferayScreens-Showcase-ObjectiveC/DDLListScreenletViewController.h
@@ -0,0 +1,13 @@
+//
+//  DDLListScreenletViewController.h
+//  LiferayScreens-Showcase-ObjectiveC
+//
+//  Created by Victor Galán on 06/02/2017.
+//  Copyright © 2017 liferay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDLListScreenletViewController : UIViewController
+
+@end
---
+++ ios/Samples/Showcase-ObjectiveC/LiferayScreens-Showcase-ObjectiveC/DDLListScreenletViewController.m
@@ -0,0 +1,105 @@
+//
+//  DDLListScreenletViewController.m
+//  LiferayScreens-Showcase-ObjectiveC
+//
+//  Created by Victor Galán on 06/02/2017.
+//  Copyright © 2017 liferay. All rights reserved.
+//
+
+@import LiferayScreens;
+#import "NSString+Utils.h"
+#import "LiferayLogger.h"
+#import "DDLListScreenletViewController.h"
+
+@interface DDLListScreenletViewController () <DDLListScreenletDelegate,
+		UIPickerViewDelegate, UIPickerViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
+@property (weak, nonatomic) IBOutlet DDLListScreenlet *screenlet;
+@property (weak, nonatomic) IBOutlet UITextField *recordSetIdTextField;
+@property (weak, nonatomic) IBOutlet UITextField *labelFieldsTextField;
+@property (weak, nonatomic) IBOutlet UIButton *loadButton;
+
+@property (nonatomic, copy) NSArray *pickerDataNames;
+@property (nonatomic, copy) NSArray *pickerDataClassNames;
+
+@property (nonatomic) NSString *selectedObcClassName;
+
+@end
+
+@implementation DDLListScreenletViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+	self.pickerDataNames = @[
+        @"No order",
+		@"Id",
+        @"Create date",
+        @"Modified Date"
+    ];
+
+    self.pickerDataClassNames = @[
+		@"",
+		@"com.liferay.dynamic.data.lists.util.comparator.DDLRecordIdComparator",
+        @"com.liferay.dynamic.data.lists.util.comparator.DDLRecordCreateDateComparator",
+        @"com.liferay.dynamic.data.lists.util.comparator.DDLRecordModifiedDateComparator"
+	];
+
+    self.selectedObcClassName = @"";
+
+    self.screenlet.delegate = self;
+    self.pickerView.delegate = self;
+    self.pickerView.dataSource = self;
+
+    self.labelFieldsTextField.text = [LiferayServerContext stringPropertyForKey:@"ddlLabelField"];
+    self.recordSetIdTextField.text = [NSString
+    		stringWithFormat:@"%lld", [LiferayServerContext longPropertyForKey:@"ddlRecordSetId"]];
+}
+
+- (IBAction)loadList:(id)sender {
+    if ([self.recordSetIdTextField.text isNumeric]) {
+        self.screenlet.recordSetId = self.recordSetIdTextField.text.longLongValue;
+        self.screenlet.labelFields = self.labelFieldsTextField.text;
+        self.screenlet.obcClassName = self.selectedObcClassName;
+        [self.screenlet loadList];
+    }
+}
+
+- (void)screenlet:(DDLListScreenlet *)screenlet onDDLListError:(NSError *)error {
+    LiferayLog(error);
+}
+
+- (void)screenlet:(DDLListScreenlet *)screenlet onDDLSelectedRecord:(DDLRecord *)record {
+	LiferayLog(record);
+}
+
+- (void)screenlet:(DDLListScreenlet *)screenlet
+		onDDLListResponseRecords:(NSArray<DDLRecord *> *)records {
+
+    LiferayLog(records);
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return self.pickerDataNames.count;
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row
+		inComponent:(NSInteger)component {
+
+    self.selectedObcClassName = self.pickerDataClassNames[row];
+}
+
+- (NSAttributedString *)pickerView:(UIPickerView *)pickerView
+		attributedTitleForRow:(NSInteger)row forComponent:(NSInteger)component {
+
+    NSDictionary *attrs = @{NSFontAttributeName : [UIFont systemFontOfSize:3]};
+
+    return [[NSAttributedString alloc] initWithString:self.pickerDataNames[row] attributes:attrs];
+}
+
+@end
