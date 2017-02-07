1a524fd3e9011c1e95baf60dbab768b985a3cc14
Javier Gamarra <javier.gamarra@liferay.com>
2017-02-07 14:27:33 +0100

    LSR-958 gallery interactor using connector instead of service directly

--- android/library/src/main/java/com/liferay/mobile/screens/ddl/form/connector/DLAppConnector62.java
+++ android/library/src/main/java/com/liferay/mobile/screens/ddl/form/connector/DLAppConnector62.java
@@ -1,8 +1,10 @@
 package com.liferay.mobile.screens.ddl.form.connector;
 
+import com.liferay.mobile.android.http.file.UploadData;
 import com.liferay.mobile.android.service.JSONObjectWrapper;
 import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.android.v62.dlapp.DLAppService;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -21,5 +23,27 @@ public class DLAppConnector62 implements DLAppConnector {
 		String s, String s1, byte[] bytes, JSONObjectWrapper serviceContextWrapper) throws Exception {
 		return dlAppService.addFileEntry(repositoryId, folderId, name, mimeType, fileName, s, s1, bytes,
 			serviceContextWrapper);
+	}
+
+	@Override
+	public JSONObject addFileEntry(Long repositoryId, Long folderId, String name, String mimeType, String fileName,
+		String s, String s1, UploadData bytes, JSONObjectWrapper serviceContextWrapper) throws Exception {
+		return null;
+	}
+
+	@Override
+	public JSONArray getFileEntries(long repositoryId, long folderId, JSONArray mimeTypes, int startRow, int endRow,
+		JSONObjectWrapper comparatorJSONWrapper) throws Exception {
+		return null;
+	}
+
+	@Override
+	public Integer getFileEntriesCount(long repositoryId, long folderId, JSONArray mimeTypes) throws Exception {
+		return null;
+	}
+
+	@Override
+	public void deleteFileEntry(long fileEntryId) throws Exception {
+
 	}
 }
--- android/library/src/main/java/com/liferay/mobile/screens/ddl/form/connector/DLAppConnector70.java
+++ android/library/src/main/java/com/liferay/mobile/screens/ddl/form/connector/DLAppConnector70.java
@@ -1,8 +1,10 @@
 package com.liferay.mobile.screens.ddl.form.connector;
 
+import com.liferay.mobile.android.http.file.UploadData;
 import com.liferay.mobile.android.service.JSONObjectWrapper;
 import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.android.v7.dlapp.DLAppService;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -20,5 +22,28 @@ public class DLAppConnector70 implements DLAppConnector {
 		String s, String s1, byte[] bytes, JSONObjectWrapper serviceContextWrapper) throws Exception {
 		return dlAppService.addFileEntry(repositoryId, folderId, name, mimeType, fileName, s, s1, bytes,
 			serviceContextWrapper);
+	}
+
+	@Override
+	public JSONObject addFileEntry(Long repositoryId, Long folderId, String name, String mimeType, String fileName,
+		String s, String s1, UploadData uploadData, JSONObjectWrapper serviceContextWrapper) throws Exception {
+		return dlAppService.addFileEntry(repositoryId, folderId, name, mimeType, fileName, s, s1, uploadData,
+			serviceContextWrapper);
+	}
+
+	@Override
+	public JSONArray getFileEntries(long repositoryId, long folderId, JSONArray mimeTypes, int startRow, int endRow,
+		JSONObjectWrapper comparatorJSONWrapper) throws Exception {
+		return dlAppService.getFileEntries(repositoryId, folderId, mimeTypes, startRow, endRow, comparatorJSONWrapper);
+	}
+
+	@Override
+	public Integer getFileEntriesCount(long repositoryId, long folderId, JSONArray mimeTypes) throws Exception {
+		return dlAppService.getFileEntriesCount(repositoryId, folderId, mimeTypes);
+	}
+
+	@Override
+	public void deleteFileEntry(long fileEntryId) throws Exception {
+		dlAppService.deleteFileEntry(fileEntryId);
 	}
 }
