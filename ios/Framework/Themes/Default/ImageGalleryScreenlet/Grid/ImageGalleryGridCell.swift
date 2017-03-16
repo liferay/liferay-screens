392d1732de4942b0935dd23389e5eeb5f8620f76
Sarai Díaz <dgarciasarai@gmail.com>
2017-03-16 17:16:02 +0100

    Update copyright

--- ios/Framework/Themes/Default/ImageGalleryScreenlet/Grid/ImageGalleryGridCell.swift
+++ ios/Framework/Themes/Default/ImageGalleryScreenlet/Grid/ImageGalleryGridCell.swift
@@ -1,11 +1,16 @@
-//
-//  ImageGalleryGridCell.swift
-//  LiferayScreens
-//
-//  Created by liferay on 20/07/16.
-//  Copyright © 2016 Liferay. All rights reserved.
-//
-
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 import UIKit
 
 open class ImageGalleryGridCell: UICollectionViewCell {
