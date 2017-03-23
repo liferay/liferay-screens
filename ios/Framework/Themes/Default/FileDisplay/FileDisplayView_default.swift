21c5023ee6f9d0598ed860b17dc2188be64ea85c
Sarai Díaz <dgarciasarai@gmail.com>
2017-03-23 23:53:41 +0100

    LSR-987 Update FileDisplayView to use WKWebView

--- ios/Framework/Themes/Default/FileDisplay/FileDisplayView_default.swift
+++ ios/Framework/Themes/Default/FileDisplay/FileDisplayView_default.swift
@@ -11,7 +11,7 @@
 * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
 * details.
 */
-import UIKit
+import WebKit
 
 
 open class FileDisplayView_default: BaseScreenletView, FileDisplayViewModel {
@@ -19,22 +19,53 @@ open class FileDisplayView_default: BaseScreenletView, FileDisplayViewModel {
 
 	//MARK: Outlets
 
-	@IBOutlet weak var webView: UIWebView?
+	open var webView: WKWebView?
 
 
 	//MARK: FileDisplayViewModel
-
-	open var url: URL? {
-		didSet {
-			if let url = url {
-				webView?.loadRequest(URLRequest(url: url))
-			}
-		}
-	}
 
 	open var title: String? {
 		didSet {
 			self.presentingViewController?.title = title
 		}
+	}
+
+	open var url: URL? {
+		get {
+			return url
+		}
+		set {
+			webView?.load(URLRequest(url: newValue!))
+		}
+	}
+
+
+	//MARK: BaseScreenletView
+
+	open override func onCreated() {
+		super.onCreated()
+
+		webView = WKWebView(frame: self.frame)
+
+		webView?.injectViewportMetaTag()
+
+		addWebView()
+	}
+
+	open func addWebView() {
+		webView?.translatesAutoresizingMaskIntoConstraints = false
+
+		addSubview(webView!)
+
+		let top = NSLayoutConstraint(item: webView!, attribute: .top, relatedBy: .equal,
+		                             toItem: self, attribute: .top, multiplier: 1, constant: 0)
+		let bottom = NSLayoutConstraint(item: webView!, attribute: .bottom, relatedBy: .equal,
+		                                toItem: self, attribute: .bottom, multiplier: 1, constant: 0)
+		let leading = NSLayoutConstraint(item: webView!, attribute: .leading, relatedBy: .equal,
+		                                 toItem: self, attribute: .leading, multiplier: 1, constant: 0)
+		let trailing = NSLayoutConstraint(item: webView!, attribute: .trailing, relatedBy: .equal,
+		                                  toItem: self, attribute: .trailing, multiplier: 1, constant: 0)
+
+		NSLayoutConstraint.activate([top, bottom, leading, trailing])
 	}
 }
