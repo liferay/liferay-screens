f2727ac150424a054ec5df79d451a2977c8ffcdb
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-17 18:53:34 +0200

    LSR-1005 Add javascript interface to manage the clicks on the images

--- android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/portlet/PortletDisplayView.java
+++ android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/portlet/PortletDisplayView.java
@@ -1,15 +1,20 @@
 package com.liferay.mobile.screens.viewsets.defaultviews.portlet;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import com.liferay.mobile.screens.R;
 import com.liferay.mobile.screens.base.BaseScreenlet;
+import com.liferay.mobile.screens.context.LiferayScreensContext;
 import com.liferay.mobile.screens.portlet.view.PortletDisplayViewModel;
 import com.liferay.mobile.screens.util.LiferayLogger;
+import com.liferay.mobile.screens.viewsets.defaultviews.imagegallery.DetailImageActivity;
 
 /**
  * @author Sarai Díaz García
@@ -55,6 +60,7 @@ public class PortletDisplayView extends FrameLayout implements PortletDisplayVie
 	public void showFinishOperation(String url) {
 		if (webView != null) {
 			webView.getSettings().setJavaScriptEnabled(true);
+			webView.addJavascriptInterface(new PortletDisplayInterface(getContext()), "android");
 			webView.loadUrl(url);
 
 			webView.setWebViewClient(new WebViewClient() {
@@ -101,5 +107,21 @@ public class PortletDisplayView extends FrameLayout implements PortletDisplayVie
 	@Override
 	public void setScreenlet(BaseScreenlet screenlet) {
 		this.screenlet = screenlet;
+	}
+
+	public class PortletDisplayInterface {
+		Context context;
+
+		PortletDisplayInterface(Context c) {
+			context = c;
+		}
+
+		@JavascriptInterface
+		public void showItem(final String src) {
+			Intent intent = new Intent(getContext(), DetailImageActivity.class);
+			intent.putExtra(DetailImageActivity.GALLERY_SCREENLET_IMAGE_DETAILED_URL, src);
+			Activity activity = LiferayScreensContext.getActivityFromContext(getContext());
+			activity.startActivity(intent);
+		}
 	}
 }
