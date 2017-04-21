355daa998afa6e1787298bddc09650aac8b0cb53
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-21 10:03:15 +0200

    LSR-1004 Create view controller for the new feature: portlet screenlet

---
+++ ios/Samples/Showcase-swift/LiferayScreens-Showcase-Swift/ViewControllers/Portlet/PortletDisplayViewController.swift
@@ -0,0 +1,50 @@
+/**
+* Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*/
+import UIKit
+import LiferayScreens
+
+
+class PortletDisplayViewController: UIViewController, PortletDisplayScreenletDelegate {
+
+
+	//MARK: IBOutlet
+
+	@IBOutlet var screenlet: PortletDisplayScreenlet? {
+		didSet {
+			screenlet?.delegate = self
+			screenlet?.presentingViewController = self
+		}
+	}
+
+
+	//MARK: UIViewController
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		screenlet?.portletUrl = LiferayServerContext.stringPropertyForKey("portletDisplayUrl")
+		screenlet?.load(withJsFile: "gallery")
+	}
+
+
+	//MARK: PortletDisplayScreenletDelegate
+
+	func screenlet(_ screenlet: PortletDisplayScreenlet, onPortletUrlResponse url: String) {
+		LiferayLogger.logDelegateMessage(args: url as AnyObject?)
+	}
+
+	func screenlet(_ screenlet: PortletDisplayScreenlet, onPortletError error: NSError) {
+		LiferayLogger.logDelegateMessage(args: error)
+	}
+}
