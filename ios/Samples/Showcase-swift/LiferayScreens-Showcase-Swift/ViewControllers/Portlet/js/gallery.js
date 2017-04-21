3c9fb91c563a87ff4c28a84e584d042877ec8e88
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-21 10:03:57 +0200

    LSR-1004 Add custom javascript file to load with the portlet content

---
+++ ios/Samples/Showcase-swift/LiferayScreens-Showcase-Swift/ViewControllers/Portlet/js/gallery.js
@@ -0,0 +1,10 @@
+var cards = document.getElementsByClassName('card');
+
+for (let i = 0; i < cards.length; i++) {
+	cards[i].addEventListener('click', function(event) {
+		let card = cards[i];
+		let imgSrc = card.querySelector('img').src;
+		event.stopPropagation();
+		window.webkit.messageHandlers.gallery.postMessage(imgSrc)
+	})
+}
