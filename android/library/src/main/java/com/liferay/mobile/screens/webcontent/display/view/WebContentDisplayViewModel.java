cf81a74422b4b81bbfc010b1552fe8ec9d78334d
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-25 14:29:02 +0200

    LSR-1008 Add new view model method to customize the WebContent with custom CSS

--- android/library/src/main/java/com/liferay/mobile/screens/webcontent/display/view/WebContentDisplayViewModel.java
+++ android/library/src/main/java/com/liferay/mobile/screens/webcontent/display/view/WebContentDisplayViewModel.java
@@ -28,4 +28,11 @@ public interface WebContentDisplayViewModel extends BaseViewModel {
 	 * @param html {@link WebContent} content
 	 */
 	void showFinishOperation(WebContent html);
+
+	/**
+	 * Customize the {@link WebContent} html with custom CSS.
+	 *
+	 * @return css style
+	 */
+	String getCustomCssStyle();
 }
