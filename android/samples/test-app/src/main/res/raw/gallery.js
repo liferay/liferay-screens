88a37d809e28f541323093a7009ef5299d5706c5
Sarai <dgarciasarai@gmail.com>
2017-05-24 11:26:22 +0200

    LSR-1005 Remove click event adition twice in gallery.js

--- android/samples/test-app/src/main/res/raw/gallery.js
+++ android/samples/test-app/src/main/res/raw/gallery.js
@@ -5,6 +5,6 @@ for (let i = 0; i < cards.length; i++) {
 		let card = cards[i];
 		let imgSrc = card.querySelector('img').src;
 		event.stopPropagation();
-		card.querySelector('img').addEventListener('click', android.showItem(imgSrc))
+		android.showItem(imgSrc);
 	})
 }
