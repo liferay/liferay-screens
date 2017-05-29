02062bfdae31f73ac895bd03261c62142cf5d13f
Victor Galan <victor.galan@liferay.com>
2017-05-29 15:12:01 +0200

    LSR-1004 Attach script on navigation

--- ios/Samples/Showcase-swift/LiferayScreens-Showcase-Swift/ViewControllers/Portlet/js/gallery.js
+++ ios/Samples/Showcase-swift/LiferayScreens-Showcase-Swift/ViewControllers/Portlet/js/gallery.js
@@ -1,10 +1,32 @@
-var cards = document.getElementsByClassName('card');
+const addClickToCards = () => {
+	var cards = document.getElementsByClassName('card');
 
-for (let i = 0; i < cards.length; i++) {
-	cards[i].addEventListener('click', function(event) {
-		let card = cards[i];
-		let imgSrc = card.querySelector('img').src;
-		event.stopPropagation();
-		window.webkit.messageHandlers.gallery.postMessage(imgSrc)
-	})
+	for (let i = 0; i < cards.length; i++) {
+		cards[i].addEventListener('click', function(event) {
+			let card = cards[i];
+			let imgSrc = card.querySelector('img').src;
+			event.stopPropagation();
+			window.webkit.messageHandlers.gallery.postMessage(imgSrc)
+		})
+	}
 }
+
+addClickToCards()
+
+window.Liferay = new Proxy(window.Liferay || {}, {
+	 set: function(target, name, value) {
+		if (name === "SPA") {
+			target[name] = new Proxy(value, {
+				 set: function(target, name, value) {
+					if (name === "app") {
+						value.events_.endNavigate.push({ fn: () => addClickToCards()})
+					}
+					target[name] = value
+				}
+			})
+		}
+		else {
+			target[name] = value
+		}
+	 }
+})
