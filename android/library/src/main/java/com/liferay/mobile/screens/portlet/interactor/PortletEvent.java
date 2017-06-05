bf93cb994c172edaddd0ecf67794402f00efd100
Sarai <dgarciasarai@gmail.com>
2017-06-05 10:24:29 +0200

    LSR-1005 Create PortletEvent used in PortletDisplayInteractor

---
+++ android/library/src/main/java/com/liferay/mobile/screens/portlet/interactor/PortletEvent.java
@@ -0,0 +1,11 @@
+package com.liferay.mobile.screens.portlet.interactor;
+
+import com.liferay.mobile.screens.base.interactor.event.CacheEvent;
+import org.json.JSONObject;
+
+public class PortletEvent extends CacheEvent {
+
+	public PortletEvent(JSONObject jsonObject) {
+		super(jsonObject);
+	}
+}
