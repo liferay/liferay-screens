2489f939ba9e3a83954a4b15b48f8dd99b00ce9b
Victor Galan <victor.galan@liferay.com>
2017-06-05 18:35:23 +0200

    LSR-1005 Rename to css

--- android/library/src/main/res/raw/bigger_pagination.js
+++
@@ -1,3 +0,0 @@
-document.getElementsByTagName('html')[0].innerHTML +=
-    '<style> .pagination { font-size: 14pt; } .pagination > li { padding: 5px; } ' +
-    '.pagination > li > a { padding: 10px 20px; } + </style>';
