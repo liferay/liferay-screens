9a36e78d26816c921797c9898a46371cf6b010f8
Sarai <dgarciasarai@gmail.com>
2017-06-19 15:30:11 +0200

    LSR-1005 Update screens javascript library (same as iOS)

--- android/library/src/main/res/raw/screens.js
+++ android/library/src/main/res/raw/screens.js
@@ -1,15 +1,45 @@
-window.Screens = {
+var screens = {
 	screensScripts_: [],
-	addScreensScript: (screensScript) => {
-		window.Screens.screensScripts_.push(screensScript);
+	addScreensScript: function(screensScript) {
+		this.screensScripts_.push(screensScript);
 	},
 
-	reloadScripts: () => {
-		window.Screens.screensScripts_.forEach(fn => fn());
+	reloadScripts: function() {
+		this.screensScripts_.forEach(fn => fn());
+	},
+
+	isAndroid: function() {
+		if (navigator.userAgent.indexOf('Android') !== -1) {
+			return false;
+		}
+		return true;
+	},
+
+	postMessage: function(namespace, message) {
+		if (this.isAndroid) {
+			android.postMessage(namespace, message);
+		}
+		else {
+			window.webkit.messageHandlers[namespace].postMessage(message);
+		}
 	}
 };
 
+window.Screens = Object.create(screens);
 
-Liferay.SPA.app.on('endNavigate', () => {
+window.Liferay.on('endNavigate', () => {
 	window.Screens.reloadScripts();
-})
+});
+
+
+/*Attach a proxy to the Liferay object so we can inject our custom session*/
+window.Liferay = new Proxy(window.Liferay, {
+	set: function(target, name, value) {
+		if (name === "Session") {
+			target[name] = new Liferay.SessionBase({ autoExtend: true, sessionLength: 30 * 60, warningLength: 60 });
+		}
+		else {
+			target[name] = value;
+	    }
+	}
+});
