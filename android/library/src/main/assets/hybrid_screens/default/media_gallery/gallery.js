a8e561d65f07d3719b2ba929028db78ed3ec98b5
Sarai <dgarciasarai@gmail.com>
2017-06-19 15:29:41 +0200

    LSR-1005 Update gallery javascript to load clicked image correctly

--- android/library/src/main/assets/hybrid_screens/default/media_gallery/gallery.js
+++ android/library/src/main/assets/hybrid_screens/default/media_gallery/gallery.js
@@ -2,11 +2,11 @@ function addClickToCards() {
 	var cards = document.getElementsByClassName('card');
     var allImgSrc = "";
 	for (let i = 0; i < cards.length; i++) {
-		allImgSrc += cards[i].querySelector('img').src + ",";;
+		allImgSrc += cards[i].querySelector('img').src + ",";
     	cards[i].addEventListener('click', function(event) {
-    	allImgSrc += i;
+    	let allAndCurrent = allImgSrc + i;
     	event.stopPropagation();
-    	android.postMessage("gallery", allImgSrc);
+    	android.postMessage("gallery", allAndCurrent);
     })}
 }
 
