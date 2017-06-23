5dc089f6571967882013d3ac582c4a2f0fc03c07
Victor Galan <victor.galan@liferay.com>
2017-06-23 13:48:06 +0200

    LSR-1004 Change the session length, more than that doesn’t seem to work

--- ios/Framework/Core/Portlet/js/Screens.js
+++ ios/Framework/Core/Portlet/js/Screens.js
@@ -53,11 +53,11 @@ window.Liferay.on('endNavigate', () => {
 // Attach a proxy to the Liferay object so we can inject our custom session 
 window.Liferay = new Proxy(window.Liferay, {
 	set: function(target, name, value) {
+
 		if (name === "Session") {
-			target[name] = new Liferay.SessionBase({ autoExtend: true, sessionLength: 30 * 60, warningLength: 60 });
+			target[name] = new Liferay.SessionBase({ autoExtend: true, sessionLength: 5 * 60, warningLength: 60 });
 		}
-		else {
-			target[name] = value;
-	    }
+
+		target[name] = value;
 	}
 });
