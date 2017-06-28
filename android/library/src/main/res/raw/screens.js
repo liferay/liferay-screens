4e8bc814e6aa1c5ca3474d05244236e37eb9cd8c
Sarai Díaz <dgarciasarai@gmail.com>
2017-06-28 09:56:41 +0200

    LSR-1005 Update screens javascript

--- android/library/src/main/res/raw/screens.js
+++ android/library/src/main/res/raw/screens.js
@@ -9,38 +9,38 @@ var screens = {
 	},
 
 	isAndroid: function() {
-		if (navigator.userAgent.indexOf('Android') !== -1) {
+		if (navigator.userAgent.indexOf('Android') === -1) {
 			return false;
 		}
 		return true;
 	},
 
 	postMessage: function(namespace, message) {
-		if (this.isAndroid) {
+		if (this.isAndroid()) {
 			android.postMessage(namespace, message);
 		}
 		else {
-			window.webkit.messageHandlers[namespace].postMessage(message);
+			window.webkit.messageHandlers.screensDefault.postMessage([namespace, message]);
 		}
 	},
 
 	listPortlets: function() {
-        let portlets = window.Liferay.Portlet.list;
+		let portlets = window.Liferay.Portlet.list;
 
-        let parsedPortlets = portlets.map(portlet => {
-            let portletSplitted = portlet.split('_');
-            let length = portletSplitted.length;
-            if (length >= 3 && portletSplitted[length - 2] === 'INSTANCE') {
-                return portletSplitted.slice(0, length - 2).join('_');
-            }
-            else {
-                return portlet;
-            }
-        })
-        .filter((x, idx, arr) => arr.indexOf(x) === idx);
+		let parsedPortlets = portlets.map(portlet => {
+			let portletSplitted = portlet.split('_');
+			let length = portletSplitted.length;
+			if (length >= 3 && portletSplitted[length - 2] === 'INSTANCE') {
+				return portletSplitted.slice(0, length - 2).join('_');
+			}
+			else {
+				return portlet;
+			}
+		})
+		.filter((x, idx, arr) => arr.indexOf(x) === idx).join(',');
 
-        this.postMessage("screensInternal", parsedPortlets.join(','));
-    }
+		this.postMessage("screensInternal.listPortlets", parsedPortlets);
+	}
 };
 
 window.Screens = Object.create(screens);
@@ -53,9 +53,11 @@ window.Liferay.on('endNavigate', () => {
 /*Attach a proxy to the Liferay object so we can inject our custom session*/
 window.Liferay = new Proxy(window.Liferay, {
 	set: function(target, name, value) {
+
 		if (name === "Session") {
-			target[name] = new Liferay.SessionBase({ autoExtend: true, sessionLength: 5 * 60, warningLength: 60 });
+			target[name] = new Liferay.SessionBase({ autoExtend: true, sessionLength: 4 * 60, warningLength: 60 });
 		}
+
 		target[name] = value;
 	}
 });
