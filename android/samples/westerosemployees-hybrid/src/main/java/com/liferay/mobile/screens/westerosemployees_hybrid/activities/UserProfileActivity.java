570c8afd71e3d1c36128daed58cac9c80f0d718d
Francisco Micó <fmico@fmico.info>
2017-07-11 16:39:51 +0200

    LSR-1019 Add PortletDisplayScreenlet with User profile

--- android/samples/westerosemployees-hybrid/src/main/java/com/liferay/mobile/screens/westerosemployees_hybrid/activities/UserProfileActivity.java
+++ android/samples/westerosemployees-hybrid/src/main/java/com/liferay/mobile/screens/westerosemployees_hybrid/activities/UserProfileActivity.java
@@ -3,87 +3,42 @@ package com.liferay.mobile.screens.westerosemployees_hybrid.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-import com.liferay.mobile.screens.asset.AssetEntry;
-import com.liferay.mobile.screens.asset.display.AssetDisplayInnerScreenletListener;
-import com.liferay.mobile.screens.asset.display.AssetDisplayScreenlet;
-import com.liferay.mobile.screens.base.BaseScreenlet;
 import com.liferay.mobile.screens.context.SessionContext;
-import com.liferay.mobile.screens.context.User;
 import com.liferay.mobile.screens.context.storage.CredentialsStorageBuilder;
-import com.liferay.mobile.screens.userportrait.UserPortraitScreenlet;
+import com.liferay.mobile.screens.portlet.PortletConfiguration;
+import com.liferay.mobile.screens.portlet.PortletDisplayScreenlet;
 import com.liferay.mobile.screens.westerosemployees_hybrid.R;
 
 public class UserProfileActivity extends WesterosActivity
-	implements View.OnClickListener, AssetDisplayInnerScreenletListener {
+        implements View.OnClickListener {
 
-	private TextView userNameText;
-	private TextView jobTitleText;
-	private TextView emailText;
-	private TextView screenNameText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_display);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.user_display);
+        userProfileChangeDisplayScreenlet();
+    }
 
-		bindViews();
+    private void userProfileChangeDisplayScreenlet() {
+        PortletDisplayScreenlet portletDisplayScreenlet = (PortletDisplayScreenlet) findViewById(R.id.portlet_user_profile);
+        PortletConfiguration configuration = new PortletConfiguration.Builder("/web/guest/userprofile").disableTheme().addRawCss(R.raw.user_profile_portlet_css).load();
 
-		AssetDisplayScreenlet screenlet = ((AssetDisplayScreenlet) findViewById(R.id.asset_display_screenlet));
-		screenlet.setClassName("com.liferay.portal.kernel.model.User");
-		screenlet.setClassPK(SessionContext.getCurrentUser().getId());
-		screenlet.setInnerListener(this);
-		screenlet.loadAsset();
-	}
+        portletDisplayScreenlet.setPortletConfiguration(configuration);
+        portletDisplayScreenlet.load();
+    }
 
-	@Override
-	public void onConfigureChildScreenlet(AssetDisplayScreenlet screenlet, BaseScreenlet innerScreenlet,
-		AssetEntry assetEntry) {
-	}
 
-	@Override
-	public View onRenderCustomAsset(AssetEntry assetEntry) {
-		if (assetEntry instanceof User) {
-			View view = getLayoutInflater().inflate(R.layout.user_profile, null);
-			User user = new User(assetEntry.getValues());
+    @Override
+    public void onClick(View v) {
+        SessionContext.logout();
+        SessionContext.removeStoredCredentials(CredentialsStorageBuilder.StorageType.AUTO);
 
-			view.findViewById(R.id.sign_out_button).setOnClickListener(this);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
 
-			UserPortraitScreenlet userPortraitScreenlet =
-				(UserPortraitScreenlet) view.findViewById(R.id.user_portrait_screenlet);
-			userNameText = (TextView) view.findViewById(R.id.user_name_text);
-			jobTitleText = (TextView) view.findViewById(R.id.user_job_title_text);
-			emailText = (TextView) view.findViewById(R.id.user_email_text);
-			screenNameText = (TextView) view.findViewById(R.id.user_screen_name_text);
-
-			userPortraitScreenlet.loadLoggedUserPortrait();
-
-			userNameText.setText(user.getFirstName() + " " + user.getLastName());
-			jobTitleText.setText(user.getJobTitle());
-			emailText.setText(user.getEmail());
-			screenNameText.setText(user.getScreenName());
-
-			return view;
-		}
-		return null;
-	}
-
-	@Override
-	public void onClick(View v) {
-		SessionContext.logout();
-		SessionContext.removeStoredCredentials(CredentialsStorageBuilder.StorageType.AUTO);
-
-		Intent intent = new Intent(this, MainActivity.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		startActivity(intent);
-		finish();
-	}
-
-	private void bindViews() {
-		userNameText = (TextView) findViewById(R.id.user_name_text);
-		jobTitleText = (TextView) findViewById(R.id.user_job_title_text);
-		emailText = (TextView) findViewById(R.id.user_email_text);
-		screenNameText = (TextView) findViewById(R.id.user_screen_name_text);
-	}
 }
