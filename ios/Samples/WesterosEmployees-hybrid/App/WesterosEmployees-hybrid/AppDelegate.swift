7a47b48e35560f21be9f41e82550660a8f9076f3
Francisco Micó <fmico@fmico.info>
2017-07-14 12:59:15 +0200

    LSR-1020 Initial commit, base architecture for Westeros Employees Hybrid

---
+++ ios/Samples/WesterosEmployees-hybrid/App/WesterosEmployees-hybrid/AppDelegate.swift
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+import UIKit
+import LiferayScreens
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        AssetClasses.set(AssetClassNameKey_JournalArticle, newId: 29591)
+        return true
+    }
+    
+}
+
