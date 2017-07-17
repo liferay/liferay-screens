f881ca4b83b7515d669e36dce62d5e34e1232daf
Francisco Micó <fmico@fmico.info>
2017-07-17 11:43:45 +0200

    LSR-1019 Fix Sing Out button

--- android/samples/westerosemployees-hybrid/src/main/java/com/liferay/mobile/screens/westerosemployees_hybrid/activities/UserProfileActivity.java
+++ android/samples/westerosemployees-hybrid/src/main/java/com/liferay/mobile/screens/westerosemployees_hybrid/activities/UserProfileActivity.java
@@ -17,6 +17,7 @@ public class UserProfileActivity extends WesterosActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_display);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
 
         loadUserProfile();
     }
