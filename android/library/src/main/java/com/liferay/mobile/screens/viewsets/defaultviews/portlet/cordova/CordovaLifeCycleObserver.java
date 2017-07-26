31b8b4872e968c76287d72ffbb560d37c1adbdbe
Victor Galan <victor.galan@liferay.com>
2017-07-26 15:04:47 +0200

    LSR-1005 Add LifeCycleListener and LifeCycleObserver to notify activity events to cordova

---
+++ android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/portlet/cordova/CordovaLifeCycleObserver.java
@@ -0,0 +1,71 @@
+package com.liferay.mobile.screens.viewsets.defaultviews.portlet.cordova;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import com.liferay.mobile.screens.viewsets.defaultviews.portlet.ScreensWebView;
+
+/**
+ * @author Víctor Galán Grande
+ */
+
+public class CordovaLifeCycleObserver {
+
+	private CordovaLifeCycleListener listener;
+
+	public CordovaLifeCycleObserver() {
+
+	}
+
+	public void setListener(CordovaLifeCycleListener listener) {
+		this.listener = listener;
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (listener != null) {
+			listener.onActivityResult(requestCode, resultCode, data);
+		}
+	}
+
+	public void onPause() {
+		if (listener != null) {
+			listener.onPause();
+		}
+	}
+
+	public void onStop() {
+		if (listener != null) {
+			listener.onStop();
+		}
+	}
+
+	public void onStart() {
+		if (listener != null) {
+			listener.onStart();
+		}
+	}
+
+	public void onResume() {
+		if (listener != null) {
+			listener.onResume();
+		}
+	}
+
+	public void onDestroy() {
+		if (listener != null) {
+			listener.onDestroy();
+		}
+	}
+
+	public void onSaveInstanceState(Bundle outState) {
+		if (listener != null) {
+			listener.onSaveInstanceState(outState);
+		}
+	}
+
+	public void onConfigurationChanged(Configuration newConfig) {
+		if (listener != null) {
+			listener.onConfigurationChanged(newConfig);
+		}
+	}
+}
