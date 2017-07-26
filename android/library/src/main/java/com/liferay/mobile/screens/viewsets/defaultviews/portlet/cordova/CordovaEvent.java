c423ba4a701069ebf7aa0fc488f065acb181d9ad
Victor Galan <victor.galan@liferay.com>
2017-07-26 15:03:45 +0200

    LSR-1005 Add event to communicate cordova plugin and ScreensCordovaWebView

---
+++ android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/portlet/cordova/CordovaEvent.java
@@ -0,0 +1,28 @@
+package com.liferay.mobile.screens.viewsets.defaultviews.portlet.cordova;
+
+import com.liferay.mobile.screens.base.interactor.event.BasicEvent;
+
+/**
+ * @author Víctor Galán Grande
+ */
+
+public class CordovaEvent {
+
+	public enum CordovaEventType { PAGE_STARTED, PAGE_FINISHED }
+
+	private CordovaEventType eventType;
+	private String param;
+
+	public CordovaEvent(CordovaEventType eventType, String param) {
+		this.eventType = eventType;
+		this.param = param;
+	}
+
+	public CordovaEventType getEventType() {
+		return eventType;
+	}
+
+	public String getParam() {
+		return param;
+	}
+}
