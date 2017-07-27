0090cdbec84b4bcec44c11e31fcc51926ae9645d
Sarai Díaz <dgarciasarai@gmail.com>
2017-07-27 15:22:39 +0200

    LSR-1022 Remove unused usings

--- xamarin/Framework/LiferayScreens.Android/Additions/Auth/Login/LoginScreenlet.cs
+++ xamarin/Framework/LiferayScreens.Android/Additions/Auth/Login/LoginScreenlet.cs
@@ -1,15 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Java.Lang;
 
 namespace Com.Liferay.Mobile.Screens.Auth.Login
 {
