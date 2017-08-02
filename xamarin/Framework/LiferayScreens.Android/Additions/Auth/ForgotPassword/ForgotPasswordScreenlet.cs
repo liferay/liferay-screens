1a0bacbd1228d3695505d49e019f29956d976c58
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-02 10:08:47 +0200

    LSR-1027 ForgotPasswordScreenlet binding for Android solutions

---
+++ xamarin/Framework/LiferayScreens.Android/Additions/Auth/ForgotPassword/ForgotPasswordScreenlet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Com.Liferay.Mobile.Screens.Auth.Forgotpassword
+{
+    public partial class ForgotPasswordScreenlet
+    {
+        protected override Java.Lang.Object CreateInteractor(string p0)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnUserAction(string p0, Java.Lang.Object p1, params Java.Lang.Object[] p2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
