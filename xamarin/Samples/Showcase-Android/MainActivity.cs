a49e1724d4d4383cb2551affab95e97d1a77d669
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-02 15:50:31 +0200

    LSR-1026 Organize Main activity and MainView

--- xamarin/Samples/Showcase-Android/MainActivity.cs
+++ xamarin/Samples/Showcase-Android/MainActivity.cs
@@ -1,46 +1,37 @@
-﻿using Android.App;
-using Android.Widget;
+﻿using System;
+using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
 using Com.Liferay.Mobile.Screens.Auth.Login;
 using Com.Liferay.Mobile.Screens.Context;
+using Java.Lang;
 
 namespace ShowcaseAndroid
 {
-    [Activity(Label = "AndroidSample.Android", Theme =
-    "@style/default_theme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity, ILoginListener
+    [Activity(Label = "MainActivity", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, View.IOnClickListener
     {
-        private LoginScreenlet loginScreenlet;
 
-        public void OnLoginFailure(Java.Lang.Exception p0)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            Toast.MakeText(this, "Login failed", ToastLength.Short);
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.MainView);
+
+            FindViewById(Resource.Id.login_screenlet).SetOnClickListener(this);
         }
 
-        public void OnLoginSuccess(User p0)
+        /* IOnClickListener */
+
+        public void OnClick(View v)
         {
-            Toast.MakeText(this, "Login successful", ToastLength.Short);
-        }
-
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
-            SetContentView(Resource.Layout.LoginView);
-
-            loginScreenlet = (LoginScreenlet)FindViewById(Resource.Id.login_screenlet);
-            loginScreenlet.SetListener(this);
-
-            setDefaultValues();
-        }
-
-        private void setDefaultValues()
-        {
-            EditText login = (EditText)this.loginScreenlet.FindViewById(Resource.Id.liferay_login);
-            EditText password = (EditText)this.loginScreenlet.FindViewById(Resource.Id.liferay_password);
-
-            login.SetText("test@liferay.com", TextView.BufferType.Editable);
-            password.SetText("test1", TextView.BufferType.Editable);
+            switch (v.Id)
+            {
+                case Resource.Id.login_screenlet:
+                    StartActivity(typeof(LoginActivity));
+                    break;
+            }
         }
     }
 }
