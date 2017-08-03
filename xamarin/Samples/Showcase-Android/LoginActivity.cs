8d1002d561fa63728952fbfdb5ca1b3240e0ec0b
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-03 11:41:15 +0200

    LSR-1026 LoginScreenlet refactor

--- xamarin/Samples/Showcase-Android/LoginActivity.cs
+++ xamarin/Samples/Showcase-Android/LoginActivity.cs
@@ -9,40 +9,40 @@ namespace ShowcaseAndroid
     [Activity(Label = "LoginActivity")]
     public class LoginActivity : Activity, ILoginListener
     {
-        private LoginScreenlet loginScreenlet;
+        LoginScreenlet loginScreenlet;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.LoginView);
 
-            loginScreenlet = (LoginScreenlet)FindViewById(Resource.Id.login_screenlet);
+            loginScreenlet = (LoginScreenlet) FindViewById(Resource.Id.login_screenlet);
             loginScreenlet.Listener = this;
 
             setDefaultValues();
         }
 
-        /* LoginListener */
+        /* ILoginListener */
 
         public void OnLoginFailure(Java.Lang.Exception p0)
         {
-            Toast.MakeText(this, "Login failed", ToastLength.Short).Show();
+            Toast.MakeText(this, "Login failed: " + p0.Message, ToastLength.Short).Show();
         }
 
         public void OnLoginSuccess(User p0)
         {
-            Toast.MakeText(this, "Login successful", ToastLength.Short).Show();
+            Toast.MakeText(this, "Login successful: " + p0.Id, ToastLength.Short).Show();
         }
 
         /* Private methods */
 
         void setDefaultValues()
         {
-            EditText login = (EditText)loginScreenlet.FindViewById(Resource.Id.liferay_login);
-            EditText password = (EditText)loginScreenlet.FindViewById(Resource.Id.liferay_password);
+            EditText login = (EditText) loginScreenlet.FindViewById(Resource.Id.liferay_login);
+            EditText password = (EditText) loginScreenlet.FindViewById(Resource.Id.liferay_password);
 
-            login.SetText("test@liferay.com", TextView.BufferType.Editable);
-            password.SetText("test1", TextView.BufferType.Editable);
+            login.SetText(GetString(Resource.String.liferay_default_user_name), TextView.BufferType.Editable);
+            password.SetText(GetString(Resource.String.liferay_default_password), TextView.BufferType.Editable);
         }
     }
 }
