9070b3f247f279e85d9de787a5c40f55aeff4d4f
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-07 10:06:12 +0200

    LSR-1027 ForgotPasswordScreenlet binding for iOS solutions

---
+++ xamarin/Framework/LiferayScreens.iOS/Auth/Common/AnonymousBasicAuthType.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using ObjCRuntime;
+using System;
+
+namespace LiferayScreens
+{
+    interface IAnonymousBasicAuthType {}
+
+    // @protocol AnonymousBasicAuthType
+    [BaseType(typeof(NSObject))]
+    [Protocol, Model]
+    interface AnonymousBasicAuthType
+    {
+        // @required @property (copy, nonatomic) NSString * _Nullable anonymousApiUserName;
+        [Abstract]
+        [NullAllowed, Export("anonymousApiUserName")]
+        string AnonymousApiUserName { get; set; }
+
+        // @required @property (copy, nonatomic) NSString * _Nullable anonymousApiPassword;
+        [Abstract]
+        [NullAllowed, Export("anonymousApiPassword")]
+        string AnonymousApiPassword { get; set; }
+    }
+}
---
+++ xamarin/Framework/LiferayScreens.iOS/Themes/Default/ForgotPasswordView_default.cs
@@ -0,0 +1,59 @@
+﻿using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using System;
+using UIKit;
+
+namespace LiferayScreens
+{
+    // @interface ForgotPasswordView_default : BaseScreenletView <ForgotPasswordViewModel, BasicAuthBasedType>
+    [BaseType(typeof(BaseScreenletView))]
+    interface ForgotPasswordView_default : IForgotPasswordViewModel, IBasicAuthBasedType
+    {
+        // @property (nonatomic, weak) UITextField * _Nullable userNameField __attribute__((iboutlet));
+        [NullAllowed, Export("userNameField", ArgumentSemantic.Weak)]
+        UITextField UserNameField { get; set; }
+
+        // @property (nonatomic, weak) UIButton * _Nullable requestPasswordButton __attribute__((iboutlet));
+        [NullAllowed, Export("requestPasswordButton", ArgumentSemantic.Weak)]
+        UIButton RequestPasswordButton { get; set; }
+
+        // @property (copy, nonatomic) NSString * _Nullable userName;
+        [NullAllowed, Export("userName")]
+        string UserName { get; set; }
+
+        // @property (copy, nonatomic) NSString * _Nullable basicAuthMethod;
+        [NullAllowed, Export("basicAuthMethod")]
+        string BasicAuthMethod { get; set; }
+
+        // @property (nonatomic) BOOL saveCredentials;
+        [Export("saveCredentials")]
+        bool SaveCredentials { get; set; }
+
+        // -(void)onCreated;
+        [Export("onCreated")]
+        void OnCreated();
+
+        // -(void)onSetTranslations;
+        [Export("onSetTranslations")]
+        void OnSetTranslations();
+
+        // -(void)onStartInteraction;
+        [Export("onStartInteraction")]
+        void OnStartInteraction();
+
+        // -(void)onFinishInteraction:(id _Nullable)result error:(NSError * _Nullable)error;
+        [Export("onFinishInteraction:error:")]
+        void OnFinishInteraction([NullAllowed] NSObject result, [NullAllowed] NSError error);
+
+        // -(id<ProgressPresenter> _Nonnull)createProgressPresenter __attribute__((warn_unused_result));
+        //[Export("createProgressPresenter")]
+        //[Verify(MethodToProperty)]
+        //ProgressPresenter CreateProgressPresenter { get; }
+
+        // -(instancetype _Nonnull)initWithFrame:(CGRect)frame __attribute__((objc_designated_initializer));
+        [Export("initWithFrame:")]
+        [DesignatedInitializer]
+        IntPtr Constructor(CGRect frame);
+    }
+}
