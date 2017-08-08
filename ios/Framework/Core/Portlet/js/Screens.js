91a46964af175e5b2a3ef66ef4a212867fda6653
Victor Galan <victor.galan@liferay.com>
2017-08-08 15:32:33 +0200

    LSR-1004 Format js files

--- ios/Framework/Core/Portlet/js/Screens.js
+++ ios/Framework/Core/Portlet/js/Screens.js
@@ -1,87 +1,87 @@
 const log = console.log;
 console.log = message => {
-  log(message);
-  if (window.Screens) {
-    window.Screens.postMessage("screensinternal.consoleMessage", message);
-  }
+	log(message);
+	if (window.Screens) {
+		window.Screens.postMessage("screensinternal.consoleMessage", message);
+	}
 };
 
 var screens = {
-  screensScripts_: [],
-  addScreensScript: function(screensScript) {
-    this.screensScripts_.push(screensScript);
-  },
+	screensScripts_: [],
+	addScreensScript: function (screensScript) {
+		this.screensScripts_.push(screensScript);
+	},
 
-  reloadScripts: function() {
-    this.screensScripts_.forEach(fn => fn());
-  },
+	reloadScripts: function () {
+		this.screensScripts_.forEach(fn => fn());
+	},
 
-  isAndroid: function() {
-    if (navigator.userAgent.indexOf("Android") === -1) {
-      return false;
-    }
-    return true;
-  },
+	isAndroid: function () {
+		if (navigator.userAgent.indexOf("Android") === -1) {
+			return false;
+		}
+		return true;
+	},
 
-  postMessage: function(namespace, message) {
-    if (this.isAndroid()) {
-      android.postMessage(namespace, message);
-    } else {
-      if (window.cordova) {
-        document.addEventListener("deviceready", () => {
-          cordova.exec(null, null, "ScreensBridgePlugin", "postMessage", [
-            namespace,
-            message
-          ]);
-        });
-      } else if (window.webkit) {
-        window.webkit.messageHandlers.screensDefault.postMessage([
-          namespace,
-          message
-        ]);
-      }
-    }
-  },
+	postMessage: function (namespace, message) {
+		if (this.isAndroid()) {
+			android.postMessage(namespace, message);
+		} else {
+			if (window.cordova) {
+				document.addEventListener("deviceready", () => {
+					cordova.exec(null, null, "ScreensBridgePlugin", "postMessage", [
+						namespace,
+						message
+					]);
+				});
+			} else if (window.webkit) {
+				window.webkit.messageHandlers.screensDefault.postMessage([
+					namespace,
+					message
+				]);
+			}
+		}
+	},
 
-  listPortlets: function() {
-    let portlets = window.Liferay.Portlet.list;
+	listPortlets: function () {
+		let portlets = window.Liferay.Portlet.list;
 
-    let parsedPortlets = portlets
-      .map(portlet => {
-        let portletSplitted = portlet.split("_");
-        let length = portletSplitted.length;
-        if (length >= 3 && portletSplitted[length - 2] === "INSTANCE") {
-          return portletSplitted.slice(0, length - 2).join("_");
-        } else {
-          return portlet;
-        }
-      })
-      .filter((x, idx, arr) => arr.indexOf(x) === idx)
-      .join(",");
+		let parsedPortlets = portlets
+			.map(portlet => {
+				let portletSplitted = portlet.split("_");
+				let length = portletSplitted.length;
+				if (length >= 3 && portletSplitted[length - 2] === "INSTANCE") {
+					return portletSplitted.slice(0, length - 2).join("_");
+				} else {
+					return portlet;
+				}
+			})
+			.filter((x, idx, arr) => arr.indexOf(x) === idx)
+			.join(",");
 
-    this.postMessage("screensinternal.listportlets", parsedPortlets);
-  }
+		this.postMessage("screensinternal.listportlets", parsedPortlets);
+	}
 };
 
 window.Screens = Object.create(screens);
 
 if (window.Liferay) {
-  window.Liferay.on("endNavigate", () => {
-    window.Screens.reloadScripts();
-  });
+	window.Liferay.on("endNavigate", () => {
+		window.Screens.reloadScripts();
+	});
 
-  // Attach a proxy to the Liferay object so we can inject our custom session
-  window.Liferay = new Proxy(window.Liferay, {
-    set: function(target, name, value) {
-      if (name === "Session") {
-        target[name] = new Liferay.SessionBase({
-          autoExtend: true,
-          sessionLength: 5 * 60,
-          warningLength: 60
-        });
-      }
+	// Attach a proxy to the Liferay object so we can inject our custom session
+	window.Liferay = new Proxy(window.Liferay, {
+		set: function (target, name, value) {
+			if (name === "Session") {
+				target[name] = new Liferay.SessionBase({
+					autoExtend: true,
+					sessionLength: 5 * 60,
+					warningLength: 60
+				});
+			}
 
-      target[name] = value;
-    }
-  });
+			target[name] = value;
+		}
+	});
 }
--- ios/Samples/Showcase-swift/LiferayScreens-Showcase-Swift/ViewControllers/Portlet/js/gallery.js
+++ ios/Samples/Showcase-swift/LiferayScreens-Showcase-Swift/ViewControllers/Portlet/js/gallery.js
@@ -2,22 +2,20 @@ const addClickToCards = () => {
 	var cards = document.getElementsByClassName('card');
 
 	for (let i = 0; i < cards.length; i++) {
-		cards[i].addEventListener('click', function(event) {
+		cards[i].addEventListener('click', function (event) {
 			let card = cards[i];
 			let imgSrc = card.querySelector('img').src;
 			event.stopPropagation();
+			if (isShow) {
+				isShow = false
+				NetworkActivityIndicator.hide();
+			}
+			else {
+				isShow = true;
+				NetworkActivityIndicator.show();
+			}
 
-
-								  if (isShow) {
-								  isShow = false
-								  NetworkActivityIndicator.hide();
-								  }
-								  else {
-								  isShow = true;
-								  NetworkActivityIndicator.show();
-								  }
-
-		window.Screens.postMessage("gallery", imgSrc.slice(7));
+			window.Screens.postMessage('gallery', imgSrc.slice(7));
 
 		})
 	}
