31552d4633adedb1376e617407f27cd57bc47a8d
Victor Galan <victor.galan@liferay.com>
2017-08-08 16:13:13 +0200

    LSR-1005 SF

--- android/library/src/main/assets/gallery.js
+++ android/library/src/main/assets/gallery.js
@@ -1,15 +1,15 @@
 function addClickToCards() {
 	var cards = document.getElementsByClassName('card');
-    var allImgSrc = "";
+	var allImgSrc = '';
 	for (var i = 0; i < cards.length; i++) {
-        var currentImage = cards[i];
+		var currentImage = cards[i];
 		allImgSrc += currentImage.querySelector('img').src + ",";
 		(function (i) {
-			currentImage.addEventListener('click', function(event) {
-    			var allAndCurrent = allImgSrc + i;
-    			event.stopPropagation();
-				android.postMessage("gallery", allAndCurrent);
-    		});
+			currentImage.addEventListener('click', function (event) {
+				var allAndCurrent = allImgSrc + i;
+				event.stopPropagation();
+				android.postMessage('gallery', allAndCurrent);
+			});
 		})(i)
 	}
 }
