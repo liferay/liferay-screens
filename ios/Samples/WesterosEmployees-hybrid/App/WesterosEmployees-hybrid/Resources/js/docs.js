a47fb7f2b7eab05cf4efd3c8a12d1ca2156b5e0b
Francisco Micó <fmico@fmico.info>
2017-08-16 12:42:40 +0200

    LSR-1020 Refactor url icons

--- ios/Samples/WesterosEmployees-hybrid/App/WesterosEmployees-hybrid/Resources/js/docs.js
+++ ios/Samples/WesterosEmployees-hybrid/App/WesterosEmployees-hybrid/Resources/js/docs.js
@@ -1,29 +1,29 @@
 function modifyItem() {
-    
+
     var urlIcons = {
-    pdf: '/documents/33300/39232/pdf.png/8c743026-cc37-07c3-1681-d7c9f4083b59',
-    img: '/documents/33300/39232/image.png/c9b5b54f-3996-756b-622b-fc02c56e0475',
-    mus: '/documents/33300/39232/music.png/f5ea120d-cbee-2a7c-b1e5-b249f84796c5',
-    def: '/documents/33300/39232/file.png/c48b31b9-e94a-866a-19d5-f8debacc9ec8'
+        documentPdf: '/documents/33300/39232/pdf.png/8c743026-cc37-07c3-1681-d7c9f4083b59',
+        images: '/documents/33300/39232/image.png/c9b5b54f-3996-756b-622b-fc02c56e0475',
+        music: '/documents/33300/39232/music.png/f5ea120d-cbee-2a7c-b1e5-b249f84796c5',
+        others: '/documents/33300/39232/file.png/c48b31b9-e94a-866a-19d5-f8debacc9ec8'
     };
-    
+
     var icons = {
-    pdf: urlIcons.pdf,
-    png: urlIcons.img,
-    jpg: urlIcons.img,
-    jpge: urlIcons.img,
-    mp3: urlIcons.mus,
-    def: urlIcons.def
+        pdf: urlIcons.documentPdf,
+        png: urlIcons.images,
+        jpg: urlIcons.images,
+        jpge: urlIcons.images,
+        mp3: urlIcons.music,
+        def: urlIcons.others
     };
-    
+
     var documentItems = document.querySelectorAll('.item-doc');
-    
+
     for (var i = 0; i < documentItems.length; i++) {
         addClick(documentItems[i]);
-        
+
         var ext = documentItems[i].getAttribute('data-extension');
         documentItems[i].getElementsByTagName('img')[0].src = icons[ext] || icons.def;
-        
+
         if (i != documentItems.length - 1) {
             addSeparator(documentItems[i]);
         }
@@ -33,8 +33,8 @@ function modifyItem() {
 function addClick(documentItems) {
     var dataId = documentItems.getAttribute('data-id');
     documentItems.addEventListener('click', function(event) {
-                                   window.Screens.postMessage('doc-item', dataId);
-                                   });
+        window.Screens.postMessage('doc-item', dataId);
+    });
 }
 
 function addSeparator(documentItems) {
--- ios/Samples/WesterosEmployees-hybrid/App/WesterosEmployees-hybrid/Resources/js/last_changes.js
+++ ios/Samples/WesterosEmployees-hybrid/App/WesterosEmployees-hybrid/Resources/js/last_changes.js
@@ -1,34 +1,34 @@
 function modifyItem() {
-    
+
     var itemClassName = 'com.liferay.document.library.kernel.model.DLFileEntry';
-    
+
     var urlIcons = {
-    pdf: '/documents/33300/39232/pdf.png/8c743026-cc37-07c3-1681-d7c9f4083b59',
-    img: '/documents/33300/39232/image.png/c9b5b54f-3996-756b-622b-fc02c56e0475',
-    mus: '/documents/33300/39232/music.png/f5ea120d-cbee-2a7c-b1e5-b249f84796c5',
-    def: '/documents/33300/39232/file.png/c48b31b9-e94a-866a-19d5-f8debacc9ec8'
+        documentPdf: '/documents/33300/39232/pdf.png/8c743026-cc37-07c3-1681-d7c9f4083b59',
+        images: '/documents/33300/39232/image.png/c9b5b54f-3996-756b-622b-fc02c56e0475',
+        music: '/documents/33300/39232/music.png/f5ea120d-cbee-2a7c-b1e5-b249f84796c5',
+        others: '/documents/33300/39232/file.png/c48b31b9-e94a-866a-19d5-f8debacc9ec8'
     };
-    
+
     var icons = {
-    pdf: urlIcons.pdf,
-    png: urlIcons.img,
-    jpg: urlIcons.img,
-    jpeg: urlIcons.img,
-    mp3: urlIcons.mus,
-    def: urlIcons.def
+        pdf: urlIcons.documentPdf,
+        png: urlIcons.images,
+        jpg: urlIcons.images,
+        jpge: urlIcons.images,
+        mp3: urlIcons.music,
+        def: urlIcons.others
     };
-    
+
     var lastChangeItems = document.querySelectorAll('.item-last-changes');
-    
+
     for (var i = 0; i < lastChangeItems.length; i++) {
-        
+
         addClick(lastChangeItems[i]);
-        
+
         if (lastChangeItems[i].getAttribute('data-class-name') == itemClassName) {
             var ext = lastChangeItems[i].getAttribute('data-extension');
             lastChangeItems[i].getElementsByTagName('img')[0].src = icons[ext] || icons.def;
         }
-        
+
         if (i != lastChangeItems.length - 1) {
             addSeparator(lastChangeItems[i]);
         }
@@ -39,8 +39,8 @@ function addClick(lastChangeItem) {
     var dataId = lastChangeItem.getAttribute('data-id');
     var dataExtension = lastChangeItem.getAttribute('data-extension');
     lastChangeItem.addEventListener('click', function(event) {
-                                    window.Screens.postMessage('last-changes-item', dataId + '|' + dataExtension);
-                                    });
+        window.Screens.postMessage('last-changes-item', dataId + '|' + dataExtension);
+    });
 }
 
 
