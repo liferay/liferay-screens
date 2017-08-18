47a263fab78ab351f459aea7b8c107b6c46eca52
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-18 11:10:53 +0200

    LSR-1035 DDLFormScreenlet binding for iOS solutions

---
+++ xamarin/Framework/LiferayScreens.iOS/DDL/Form/DDLFormScreenletDelegate.cs
@@ -0,0 +1,51 @@
+﻿using Foundation;
+using ObjCRuntime;
+
+namespace LiferayScreens
+{
+    // @protocol DDLFormScreenletDelegate <BaseScreenletDelegate>
+    [BaseType(typeof(BaseScreenletDelegate))]
+    [Protocol, Model]
+    interface DDLFormScreenletDelegate
+    {
+        // @optional -(void)screenlet:(DDLFormScreenlet * _Nonnull)screenlet onFormLoaded:(DDLRecord * _Nonnull)record;
+        [Export("screenlet:onFormLoaded:")]
+        void OnFormLoaded(DDLFormScreenlet screenlet, DDLRecord record);
+
+        // @optional -(void)screenlet:(DDLFormScreenlet * _Nonnull)screenlet onFormLoadError:(NSError * _Nonnull)error;
+        [Export("screenlet:onFormLoadError:")]
+        void OnFormLoadError(DDLFormScreenlet screenlet, NSError error);
+
+        // @optional -(void)screenlet:(DDLFormScreenlet * _Nonnull)screenlet onRecordLoaded:(DDLRecord * _Nonnull)record;
+        [Export("screenlet:onRecordLoaded:")]
+        void OnRecordLoaded(DDLFormScreenlet screenlet, DDLRecord record);
+
+        // @optional -(void)screenlet:(DDLFormScreenlet * _Nonnull)screenlet onRecordLoadError:(NSError * _Nonnull)error;
+        [Export("screenlet:onRecordLoadError:")]
+        void OnRecordLoadError(DDLFormScreenlet screenlet, NSError error);
+
+        // @optional -(void)screenlet:(DDLFormScreenlet * _Nonnull)screenlet onFormSubmitted:(DDLRecord * _Nonnull)record;
+        [Export("screenlet:onFormSubmitted:")]
+        void OnFormSubmitted(DDLFormScreenlet screenlet, DDLRecord record);
+
+        // @optional -(void)screenlet:(DDLFormScreenlet * _Nonnull)screenlet onFormSubmitError:(NSError * _Nonnull)error;
+        [Export("screenlet:onFormSubmitError:")]
+        void OnFormSubmitError(DDLFormScreenlet screenlet, NSError error);
+
+        // @optional -(void)screenlet:(DDLFormScreenlet * _Nonnull)screenlet onDocumentFieldUploadStarted:(DDMFieldDocument * _Nonnull)field;
+        [Export("screenlet:onDocumentFieldUploadStarted:")]
+        void OnDocumentFieldUploadStarted(DDLFormScreenlet screenlet, DDMFieldDocument field);
+
+        // @optional -(void)screenlet:(DDLFormScreenlet * _Nonnull)screenlet onDocumentField:(DDMFieldDocument * _Nonnull)field uploadedBytes:(uint64_t)bytes totalBytes:(uint64_t)total;
+        [Export("screenlet:onDocumentField:uploadedBytes:totalBytes:")]
+        void OnDocumentField(DDLFormScreenlet screenlet, DDMFieldDocument field, ulong bytes, ulong total);
+
+        // @optional -(void)screenlet:(DDLFormScreenlet * _Nonnull)screenlet onDocumentField:(DDMFieldDocument * _Nonnull)field uploadResult:(NSDictionary<NSString *,id> * _Nonnull)result;
+        [Export("screenlet:onDocumentField:uploadResult:")]
+        void OnDocumentField(DDLFormScreenlet screenlet, DDMFieldDocument field, NSDictionary<NSString, NSObject> result);
+
+        // @optional -(void)screenlet:(DDLFormScreenlet * _Nonnull)screenlet onDocumentField:(DDMFieldDocument * _Nonnull)field uploadError:(NSError * _Nonnull)error;
+        [Export("screenlet:onDocumentField:uploadError:")]
+        void OnDocumentField(DDLFormScreenlet screenlet, DDMFieldDocument field, NSError error);
+    }
+}
---
+++ xamarin/Framework/LiferayScreens.iOS/Themes/Default/DDL/DDLFormView_default.cs
@@ -0,0 +1,33 @@
+﻿using CoreGraphics;
+using Foundation;
+using System;
+
+namespace LiferayScreens
+{
+    // @interface DDLFormView_default : DDLFormTableView
+    [BaseType(typeof(DDLFormTableView))]
+    interface DDLFormView_default
+    {
+        // -(void)onCreated;
+        [Export("onCreated")]
+        void OnCreated();
+
+        // -(void)onFinishInteraction:(id _Nullable)result error:(NSError * _Nullable)error;
+        [Export("onFinishInteraction:error:")]
+        void OnFinishInteraction([NullAllowed] NSObject result, [NullAllowed] NSError error);
+
+        // -(id<ProgressPresenter> _Nonnull)createProgressPresenter __attribute__((warn_unused_result));
+        [Export("createProgressPresenter")]
+        //[Verify(MethodToProperty)]
+        ProgressPresenter CreateProgressPresenter { get; }
+
+        // -(void)changeEditable:(BOOL)editable;
+        [Export("changeEditable:")]
+        void ChangeEditable(bool editable);
+
+        // -(instancetype _Nonnull)initWithFrame:(CGRect)frame __attribute__((objc_designated_initializer));
+        [Export("initWithFrame:")]
+        [DesignatedInitializer]
+        IntPtr Constructor(CGRect frame);
+    }
+}
