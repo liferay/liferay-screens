f262395fc83391fd917141e07900e7761ab4842a
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-22 15:23:47 +0200

    LSR-1026 Handle click in selected row (for now, showing an Alert)

--- xamarin/Samples/Showcase-iOS/ViewController/SelectScreenletViewController.cs
+++ xamarin/Samples/Showcase-iOS/ViewController/SelectScreenletViewController.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using UIKit;
+using ShowcaseiOS.SelectScreenlets;
 
 namespace ShowcaseiOS.ViewController
 {
     public partial class SelectScreenletViewController : UITableViewController
     {
+        protected string[] screenlets = { "DDLFormScreenlet" };
+
         public SelectScreenletViewController(IntPtr handle) : base(handle) { }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+
+            TableView.Source = new ScreenletsTableSource(screenlets, this);
         }
 
         public override void DidReceiveMemoryWarning()
