dbeedb96c81b338ddf827d06b0e2e139aaa716d0
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-25 10:10:01 +0200

    LSR-1041 AssetListScreenlet for Android solutions

---
+++ xamarin/Framework/LiferayScreens.Android/Additions/Asset/List/AssetListScreenlet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.Liferay.Mobile.Screens.Asset.List
+{
+    public partial class AssetListScreenlet
+    {
+        protected override Java.Lang.Object CreateInteractor(string p0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
