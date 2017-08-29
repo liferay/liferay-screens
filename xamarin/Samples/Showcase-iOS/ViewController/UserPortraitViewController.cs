8c2fada0cf61b9dd6050d7c3fd29d5b1e6b7c60a
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-29 17:07:43 +0200

    LSR-1026 Complete UserPortraitScreenlet example for Showcase-iOS

--- xamarin/Samples/Showcase-iOS/ViewController/UserPortraitViewController.cs
+++ xamarin/Samples/Showcase-iOS/ViewController/UserPortraitViewController.cs
@@ -1,23 +1,61 @@
-﻿using System;
-using UIKit;
+﻿using Foundation;
 using LiferayScreens;
+using System;
+using UIKit;
 
 namespace ShowcaseiOS.ViewController
 {
-    public partial class UserPortraitViewController : UIViewController
+    public partial class UserPortraitViewController : UIViewController, IUserPortraitScreenletDelegate
     {
         public UserPortraitViewController(IntPtr handle) : base(handle) { }
 
 		public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+
+            this.userPortraitScreenlet.OfflinePolicy = "remote-first";
             this.userPortraitScreenlet.Editable = true;
+
+            this.userPortraitScreenlet.Delegate = this;
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            this.userPortraitScreenlet.LoadLoggedUserPortrait();
         }
 
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
         }
+
+        /* IUserPortraitScreenletDelegate */
+
+        [Export("screenlet:onUserPortraitError:")]
+        public virtual void Screenlet(UserPortraitScreenlet screenlet, NSError error)
+        {
+            System.Diagnostics.Debug.WriteLine($"User portrait failed: {error.DebugDescription}");
+        }
+
+        [Export("screenlet:onUserPortraitResponseImage:")]
+        public virtual UIImage Screenlet(UserPortraitScreenlet screenlet, UIImage image)
+        {
+            System.Diagnostics.Debug.WriteLine($"User portrait succesful: {image.DebugDescription}");
+            return image;
+        }
+
+        [Export("screenlet:onUserPortraitUploaded:")]
+        public virtual void Screenlet(UserPortraitScreenlet screenlet, NSDictionary<NSString, NSObject> attributes)
+        {
+            System.Diagnostics.Debug.WriteLine($"User portrait uploaded successfullu: {attributes.DebugDescription}");
+        }
+
+        [Export("screenlet:onUserPortraitUploadError:")]
+        public virtual void ScreenletUploadError(UserPortraitScreenlet screenlet, NSError error)
+        {
+            System.Diagnostics.Debug.WriteLine($"User portrait uploaded failed: {error.DebugDescription}");
+        }
     }
 }
-
