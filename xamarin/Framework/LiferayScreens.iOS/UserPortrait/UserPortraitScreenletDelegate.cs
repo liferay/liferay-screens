ed51d6f4ffb52321b81620e595ef9bb25b237ce3
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-29 18:57:35 +0200

    LSR-1041 Rename delegate methods

--- xamarin/Framework/LiferayScreens.iOS/UserPortrait/UserPortraitScreenletDelegate.cs
+++ xamarin/Framework/LiferayScreens.iOS/UserPortrait/UserPortraitScreenletDelegate.cs
@@ -11,18 +11,18 @@ namespace LiferayScreens
     {
         // @optional -(UIImage * _Nonnull)screenlet:(UserPortraitScreenlet * _Nonnull)screenlet onUserPortraitResponseImage:(UIImage * _Nonnull)image __attribute__((warn_unused_result));
         [Export("screenlet:onUserPortraitResponseImage:")]
-        UIImage ScreenletPortraitResponseImage(UserPortraitScreenlet screenlet, UIImage image);
+        UIImage Screenlet(UserPortraitScreenlet screenlet, UIImage image);
 
         // @optional -(void)screenlet:(UserPortraitScreenlet * _Nonnull)screenlet onUserPortraitError:(NSError * _Nonnull)error;
         [Export("screenlet:onUserPortraitError:")]
-        void ScreenletPortraitError(UserPortraitScreenlet screenlet, NSError error);
+        void Screenlet(UserPortraitScreenlet screenlet, NSError error);
 
         // @optional -(void)screenlet:(UserPortraitScreenlet * _Nonnull)screenlet onUserPortraitUploaded:(NSDictionary<NSString *,id> * _Nonnull)attributes;
         [Export("screenlet:onUserPortraitUploaded:")]
-        void ScreenletPortraitUploaded(UserPortraitScreenlet screenlet, NSDictionary<NSString, NSObject> attributes);
+        void Screenlet(UserPortraitScreenlet screenlet, NSDictionary<NSString, NSObject> attributes);
 
         // @optional -(void)screenlet:(UserPortraitScreenlet * _Nonnull)screenlet onUserPortraitUploadError:(NSError * _Nonnull)error;
         [Export("screenlet:onUserPortraitUploadError:")]
-        void ScreenletPortraitUploadError(UserPortraitScreenlet screenlet, NSError error);
+        void ScreenletUploadError(UserPortraitScreenlet screenlet, NSError error);
     }
 }
--- xamarin/Framework/LiferayScreens.iOS/WebContent/WebContentDisplayDelegate.cs
+++ xamarin/Framework/LiferayScreens.iOS/WebContent/WebContentDisplayDelegate.cs
@@ -11,14 +11,14 @@ namespace LiferayScreens
         // @optional -(NSString * _Nullable)screenlet:(WebContentDisplayScreenlet * _Nonnull)screenlet onWebContentResponse:(NSString * _Nonnull)html __attribute__((warn_unused_result));
         [Export("screenlet:onWebContentResponse:")]
         [return: NullAllowed]
-        string ScreenletWebContentResponse(WebContentDisplayScreenlet screenlet, string html);
+        string Screenlet(WebContentDisplayScreenlet screenlet, string html);
 
         // @optional -(void)screenlet:(WebContentDisplayScreenlet * _Nonnull)screenlet onRecordContentResponse:(DDLRecord * _Nonnull)record;
         [Export("screenlet:onRecordContentResponse:")]
-        void ScreenletRecordContentResponse(WebContentDisplayScreenlet screenlet, DDLRecord record);
+        void Screenlet(WebContentDisplayScreenlet screenlet, DDLRecord record);
 
         // @optional -(void)screenlet:(WebContentDisplayScreenlet * _Nonnull)screenlet onWebContentError:(NSError * _Nonnull)error;
         [Export("screenlet:onWebContentError:")]
-        void ScreenletWebContentError(WebContentDisplayScreenlet screenlet, NSError error);
+        void Screenlet(WebContentDisplayScreenlet screenlet, NSError error);
     }
 }
