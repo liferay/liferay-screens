4b12b9d2bb59747ccee4f6554e2230b9a2b9dbee
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-29 19:12:29 +0200

    LSR-1026 Complete WebContentDisplayScreenlet example for Showcase-iOS

--- xamarin/Samples/Showcase-iOS/ViewController/WebContentViewController.cs
+++ xamarin/Samples/Showcase-iOS/ViewController/WebContentViewController.cs
@@ -1,22 +1,49 @@
-﻿using System;
-
+﻿using Foundation;
+using LiferayScreens;
+using System;
 using UIKit;
 
 namespace ShowcaseiOS.ViewController
 {
-    public partial class WebContentViewController : UIViewController
+    public partial class WebContentViewController : UIViewController, IWebContentDisplayScreenletDelegate
     {
 		public WebContentViewController(IntPtr handle) : base(handle) { }
 
 		public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+
             this.webContentDisplayScreenlet.ArticleId = "57343";
+            this.webContentDisplayScreenlet.GroupId = 20143;
+            this.webContentDisplayScreenlet.TemplateId = 29644;
+
+            this.webContentDisplayScreenlet.Delegate = this;
         }
 
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
+        }
+
+        /* IWebContentDisplayScreenletDelegate */
+
+        [Export("screenlet:onRecordContentResponse:")]
+        public void Screenlet(WebContentDisplayScreenlet screenlet, DDLRecord record)
+        {
+            System.Diagnostics.Debug.WriteLine($"WebContent displays record successfully: {record.DebugDescription}");
+        }
+
+        [Export("screenlet:onWebContentResponse:")]
+        public string Screenlet(WebContentDisplayScreenlet screenlet, string html)
+        {
+            System.Diagnostics.Debug.WriteLine("WebContent displays successfully");
+            return html;
+        }
+
+        [Export("screenlet:onWebContentError:")]
+        public void Screenlet(WebContentDisplayScreenlet screenlet, NSError error)
+        {
+            System.Diagnostics.Debug.WriteLine($"WebContent failed: {error.DebugDescription}");
         }
     }
 }
