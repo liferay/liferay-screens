47e9aa8fd0205d87caaa7e0a626960c3056d2e50
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-30 17:05:42 +0200

    LSR-1026 Complete ImageDisplayScreenlet with listener methods

--- xamarin/Samples/Showcase-Android/Activities/ImageDisplayActivity.cs
+++ xamarin/Samples/Showcase-Android/Activities/ImageDisplayActivity.cs
@@ -1,11 +1,16 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.OS;
+using Android.Widget;
+using Com.Liferay.Mobile.Screens.Asset;
+using Com.Liferay.Mobile.Screens.Asset.Display;
 using Com.Liferay.Mobile.Screens.Dlfile.Display.Image;
+using Java.Lang;
 
 namespace ShowcaseAndroid
 {
     [Activity]
-    public class ImageDisplayActivity : Activity
+    public class ImageDisplayActivity : Activity, IAssetDisplayListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -14,10 +19,21 @@ namespace ShowcaseAndroid
 
             ImageDisplayScreenlet imageDisplayScreenlet =
                 (ImageDisplayScreenlet)FindViewById(Resource.Id.image_display_screenlet);
-            //FIX-ME: Need AssetDisplayListener
-            //imageDisplayScreenlet.Listener = this;
+            imageDisplayScreenlet.Listener = this;
 
             imageDisplayScreenlet.Load();
+        }
+
+        /* IAssetDisplayListener */
+
+        public void Error(Java.Lang.Exception p0, string p1)
+        {
+            Toast.MakeText(this, "Asset display failed: " + p0.Message, ToastLength.Short).Show();
+        }
+
+        public void OnRetrieveAssetSuccess(AssetEntry p0)
+        {
+            Toast.MakeText(this, "Asset display successful: " + p0.EntryId, ToastLength.Short).Show();
         }
     }
 }
