a34d367440568a4852082efb4f4b417fabb5e337
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-31 10:20:45 +0200

    LSR-1041 PdfDisplayScreenlet binding for iOS solutions

---
+++ xamarin/Framework/LiferayScreens.iOS/Themes/Default/FileDisplay/PdfDisplayView_default.cs
@@ -0,0 +1,38 @@
+﻿using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using System;
+using WebKit;
+
+namespace LiferayScreens
+{
+    // @interface PdfDisplayView_default : BaseScreenletView <FileDisplayViewModel>
+    [BaseType(typeof(BaseScreenletView))]
+    interface PdfDisplayView_default : IFileDisplayViewModel
+    {
+        // @property (nonatomic, strong) WKWebView * _Nullable webView;
+        [NullAllowed, Export("webView", ArgumentSemantic.Strong)]
+        WKWebView WebView { get; set; }
+
+        // @property (copy, nonatomic) NSString * _Nullable title;
+        [NullAllowed, Export("title")]
+        string Title { get; set; }
+
+        // @property (copy, nonatomic) NSURL * _Nullable url;
+        [NullAllowed, Export("url", ArgumentSemantic.Copy)]
+        NSUrl Url { get; set; }
+
+        // -(void)onCreated;
+        [Export("onCreated")]
+        void OnCreated();
+
+        // -(void)addWebView;
+        [Export("addWebView")]
+        void AddWebView();
+
+        // -(instancetype _Nonnull)initWithFrame:(CGRect)frame __attribute__((objc_designated_initializer));
+        [Export("initWithFrame:")]
+        [DesignatedInitializer]
+        IntPtr Constructor(CGRect frame);
+    }
+}
