3419abfed0886de8063f21992e65871038faa55f
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-31 12:33:20 +0200

    LSR-1041 VideoDisplayScreenlet binding for iOS solutions

---
+++ xamarin/Framework/LiferayScreens.iOS/FileDisplay/VideoDisplayScreenlet.cs
@@ -0,0 +1,25 @@
+﻿using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using System;
+
+namespace LiferayScreens
+{
+    // @interface VideoDisplayScreenlet : FileDisplayScreenlet
+    [BaseType(typeof(FileDisplayScreenlet))]
+    interface VideoDisplayScreenlet
+    {
+        // @property (copy, nonatomic) NSString * _Nonnull mimeTypes;
+        [Export("mimeTypes")]
+        string MimeTypes { get; set; }
+
+        // @property (readonly, copy, nonatomic) NSArray<NSString *> * _Nonnull supportedMimeTypes;
+        [Export("supportedMimeTypes", ArgumentSemantic.Copy)]
+        string[] SupportedMimeTypes { get; }
+
+        // -(instancetype _Nonnull)initWithFrame:(CGRect)frame themeName:(NSString * _Nullable)themeName __attribute__((objc_designated_initializer));
+        [Export("initWithFrame:themeName:")]
+        [DesignatedInitializer]
+        IntPtr Constructor(CGRect frame, [NullAllowed] string themeName);
+    }
+}
