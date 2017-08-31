ec7f9d92e9655ffe262161201b6b93813ea7001c
Sarai Díaz <dgarciasarai@gmail.com>
2017-08-31 12:51:50 +0200

    LSR-1041 AudioDisplayScreenlet binding for iOS solutions

---
+++ xamarin/Framework/LiferayScreens.iOS/FileDisplay/AudioDisplayScreenlet.cs
@@ -0,0 +1,25 @@
+﻿using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using System;
+
+namespace LiferayScreens
+{
+    // @interface AudioDisplayScreenlet : FileDisplayScreenlet
+    [BaseType(typeof(FileDisplayScreenlet))]
+    interface AudioDisplayScreenlet
+    {
+        // @property (copy, nonatomic) NSString * _Nonnull mimeTypes;
+        [Export("mimeTypes")]
+        string MimeTypes { get; set; }
+
+        // @property (readonly, copy, nonatomic) NSArray<NSString *> * _Nonnull supportedMimeTypes;
+        [Export("supportedMimeTypes", ArgumentSemantic.Copy)]
+        string[] SupportedMimeTypes { get; }
+
+        // -(instancetype _Nonnull)initWithFrame:(CGRect)frame themeName:(NSString * _Nullable)themeName __attribute__((objc_designated_initializer));
+        [Export("initWithFrame:themeName:")]
+        [DesignatedInitializer]
+        IntPtr Constructor(CGRect frame, [NullAllowed] string themeName);
+    }
+}
---
+++ xamarin/Framework/LiferayScreens.iOS/Themes/Default/FileDisplay/AudioDisplayView_default.cs
@@ -0,0 +1,50 @@
+﻿using AVFoundation;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using System;
+
+namespace LiferayScreens
+{
+    // @interface AudioDisplayView_default : BaseScreenletView <FileDisplayViewModel>
+    [BaseType(typeof(BaseScreenletView))]
+    interface AudioDisplayView_default : IFileDisplayViewModel
+    {
+        // @property (nonatomic) float volume;
+        [Export("volume")]
+        float Volume { get; set; }
+
+        // @property (nonatomic) NSInteger numberOfLoops;
+        [Export("numberOfLoops")]
+        nint NumberOfLoops { get; set; }
+
+        // @property (copy, nonatomic) NSURL * _Nullable url;
+        [NullAllowed, Export("url", ArgumentSemantic.Copy)]
+        NSUrl Url { get; set; }
+
+        // @property (copy, nonatomic) NSString * _Nullable title;
+        [NullAllowed, Export("title")]
+        string Title { get; set; }
+
+        // @property (nonatomic, strong) AVAudioPlayer * _Nullable audio;
+        [NullAllowed, Export("audio", ArgumentSemantic.Strong)]
+        AVAudioPlayer Audio { get; set; }
+
+        // @property (nonatomic, strong) NSTimer * _Nullable timer;
+        [NullAllowed, Export("timer", ArgumentSemantic.Strong)]
+        NSTimer Timer { get; set; }
+
+        // @property (nonatomic, strong) NSTimer * _Nullable duration;
+        [NullAllowed, Export("duration", ArgumentSemantic.Strong)]
+        NSTimer Duration { get; set; }
+
+        // -(void)onHide;
+        [Export("onHide")]
+        void OnHide();
+
+        // -(instancetype _Nonnull)initWithFrame:(CGRect)frame __attribute__((objc_designated_initializer));
+        [Export("initWithFrame:")]
+        [DesignatedInitializer]
+        IntPtr Constructor(CGRect frame);
+    }
+}
