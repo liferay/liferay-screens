8c5b4dc1af33208202b613bd520f276f04996c81
Sarai Díaz <dgarciasarai@gmail.com>
2017-09-04 11:43:56 +0200

    LSR-1026 Complete AssetDisplayScreenlet (iOS)
    FIXME: Display WebContent with AssetDisplayScreenlet

--- xamarin/Samples/Showcase-iOS/ViewController/AssetDisplayViewController.cs
+++ xamarin/Samples/Showcase-iOS/ViewController/AssetDisplayViewController.cs
@@ -14,40 +14,42 @@ namespace ShowcaseiOS.ViewController
         {
             base.ViewDidLoad();
 
-            this.assetDisplayScreenlet.Delegate = this;
+            //AssetDisplayScreenlet works with AssetEntryId o ClassName and ClassPK
+            this.assetDisplayScreenlet.AutoLoad = false;
 
-			//Blog
-			//this.assetDisplayScreenlet.AssetEntryId = 57367;
-			this.assetDisplayScreenlet.ClassPK = 57366;
-			this.assetDisplayScreenlet.ClassName = "com.liferay.blogs.kernel.model.BlogsEntry";
+            //Blog
+            //this.assetDisplayScreenlet.AssetEntryId = 40516;
+            //this.assetDisplayScreenlet.ClassPK = 40515;
+            //this.assetDisplayScreenlet.ClassName = "com.liferay.blogs.kernel.model.BlogsEntry";
 
-			//Image
-			//this.assetDisplayScreenlet.ClassPK = 54498;
-			//this.assetDisplayScreenlet.ClassName = "com.liferay.document.library.kernel.model.DLFileEntry";
-			//this.assetDisplayScreenlet.AssetEntryId = 54500;
+            //Image
+            //this.assetDisplayScreenlet.AssetEntryId = 54500;
+            //this.assetDisplayScreenlet.ClassPK = 54498;
+            //this.assetDisplayScreenlet.ClassName = "com.liferay.document.library.kernel.model.DLFileEntry";
 
-			//Audio
-			//this.assetDisplayScreenlet.ClassPK = 57433;
-			//this.assetDisplayScreenlet.ClassName = "com.liferay.document.library.kernel.model.DLFileEntry";
-			//this.assetDisplayScreenlet.AssetEntryId = 57435;
+            //Audio
+            this.assetDisplayScreenlet.AssetEntryId = 57435;
+            //this.assetDisplayScreenlet.ClassPK = 57433;
+            //this.assetDisplayScreenlet.ClassName = "com.liferay.document.library.kernel.model.DLFileEntry";
 
-			//Video
-			//this.assetDisplayScreenlet.ClassPK = 38943;
-			//this.assetDisplayScreenlet.ClassName = "com.liferay.document.library.kernel.model.DLFileEntry";
-			//this.assetDisplayScreenlet.AssetEntryId = 38945;
+            //Video
+            //this.assetDisplayScreenlet.AssetEntryId = 38945;
+            //this.assetDisplayScreenlet.ClassPK = 38943;
+            //this.assetDisplayScreenlet.ClassName = "com.liferay.document.library.kernel.model.DLFileEntry";
 
-			//PDF
-			//this.assetDisplayScreenlet.ClassPK = 38930;
-			//this.assetDisplayScreenlet.ClassName = "com.liferay.document.library.kernel.model.DLFileEntry";
-			//this.assetDisplayScreenlet.AssetEntryId = 38932;
+            //PDF
+            //this.assetDisplayScreenlet.AssetEntryId = 38932;
+            //this.assetDisplayScreenlet.ClassPK = 38930;
+            //this.assetDisplayScreenlet.ClassName = "com.liferay.document.library.kernel.model.DLFileEntry";
 
-			//WebContent - FALLO
-			//this.assetDisplayScreenlet.ClassPK = 39226;
-			//this.assetDisplayScreenlet.ClassName = "com.liferay.journal.model.JournalArticle";
-			//this.assetDisplayScreenlet.AssetEntryId = ;
+            //FIXME: WebContent failed
+            //this.assetDisplayScreenlet.AssetEntryId = 39228;
+            //this.assetDisplayScreenlet.ClassPK = 39226;
+            //this.assetDisplayScreenlet.ClassName = "com.liferay.journal.model.JournalArticle";
 
-			this.assetDisplayScreenlet.AutoLoad = false;
             this.assetDisplayScreenlet.Load();
+
+            this.assetDisplayScreenlet.Delegate = this;
         }
 
         public override void DidReceiveMemoryWarning()
@@ -55,26 +57,29 @@ namespace ShowcaseiOS.ViewController
             base.DidReceiveMemoryWarning();
         }
 
+        /* IAssetDisplayScreenletDelegate */
+
         [Export("screenlet:onAssetResponse:")]
         public virtual void Screenlet(AssetDisplayScreenlet screenlet, Asset asset)
         {
-            Debug.WriteLine($"Asset display response: {asset}");
+            Debug.WriteLine($"Asset display response: {asset.Attributes}");
         }
 
 		[Export("screenlet:onAssetError:")]
         public virtual void Screenlet(AssetDisplayScreenlet screenlet, NSError error)
         {
-            Debug.WriteLine($"Asset display error: {error}");
+            Debug.WriteLine($"Asset display error: {error.DebugDescription}");
 		}
 
 		[Export("screenlet:onConfigureScreenlet:onAsset:")]
         public virtual void Screenlet(AssetDisplayScreenlet screenlet, BaseScreenlet childScreenlet, Asset asset)
         {
-			Debug.WriteLine($"Configure Asset display: {asset}");
+            Debug.WriteLine($"Configure Asset display: {asset}");
 		}
 
 		[Export("screenlet:onAsset:")]
         public virtual UIView ScreenletCustomAsset(AssetDisplayScreenlet screenlet, Asset asset){
+            Debug.WriteLine($"Asset display custom asset: {asset.Attributes}");
             return screenlet;
         }
 
