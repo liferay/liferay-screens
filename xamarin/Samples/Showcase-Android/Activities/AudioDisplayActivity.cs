37f2d3f6f465ee8f3cff2dcf03e32bb6737a4d63
Sarai Díaz <dgarciasarai@gmail.com>
2017-09-07 16:33:41 +0200

    LSR-1026 SF

--- xamarin/Samples/Showcase-Android/Activities/AudioDisplayActivity.cs
+++ xamarin/Samples/Showcase-Android/Activities/AudioDisplayActivity.cs
@@ -1,14 +1,5 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Com.Liferay.Mobile.Screens.Asset;
 using Com.Liferay.Mobile.Screens.Asset.Display;
--- xamarin/Samples/Showcase-Android/Activities/CommentDisplayActivity.cs
+++ xamarin/Samples/Showcase-Android/Activities/CommentDisplayActivity.cs
@@ -15,30 +15,30 @@ namespace ShowcaseAndroid
             SetContentView(Resource.Layout.CommentDisplayView);
 
             CommentDisplayScreenlet commentDisplayScreenlet =
-                (CommentDisplayScreenlet)FindViewById(Resource.Id.comment_display_screenlet);
+                (CommentDisplayScreenlet) FindViewById(Resource.Id.comment_display_screenlet);
             commentDisplayScreenlet.Listener = this;
         }
 
         /* ICommentDisplayListener */
 
         public void Error(Java.Lang.Exception p0, string p1)
-		{
-			System.Diagnostics.Debug.WriteLine($"Comment display error: {p0}");
-		}
+        {
+            System.Diagnostics.Debug.WriteLine($"Comment display error: {p0.Message}");
+        }
 
-		public void OnDeleteCommentSuccess(CommentEntry p0)
-		{
+        public void OnDeleteCommentSuccess(CommentEntry p0)
+        {
             Toast.MakeText(this, "Comment delete success: " + p0.CommentId, ToastLength.Short).Show();
-		}
+        }
 
-		public void OnLoadCommentSuccess(CommentEntry p0)
-		{
-			Toast.MakeText(this, "Comment load success: " + p0.CommentId, ToastLength.Short).Show();
-		}
+        public void OnLoadCommentSuccess(CommentEntry p0)
+        {
+        	Toast.MakeText(this, "Comment load success: " + p0.CommentId, ToastLength.Short).Show();
+        }
 
-		public void OnUpdateCommentSuccess(CommentEntry p0)
-		{
-			Toast.MakeText(this, "Comment update success: " + p0.CommentId, ToastLength.Short).Show();
-		}
+        public void OnUpdateCommentSuccess(CommentEntry p0)
+        {
+        	Toast.MakeText(this, "Comment update success: " + p0.CommentId, ToastLength.Short).Show();
+        }
     }
 }
