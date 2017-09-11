6f10ffab824ef7dba7a8e35cd2e174e4c6eaa6e2
Sarai Díaz <dgarciasarai@gmail.com>
2017-09-11 12:41:55 +0200

    LSR-1036 Implement new video listener in VideoDisplayScreenlet

---
+++ android/library/src/main/java/com/liferay/mobile/screens/dlfile/display/video/VideoDisplayListener.java
@@ -0,0 +1,26 @@
+package com.liferay.mobile.screens.dlfile.display.video;
+
+import com.liferay.mobile.screens.asset.display.AssetDisplayListener;
+
+/**
+ * @author Víctor Galán Grande
+ */
+
+public interface VideoDisplayListener extends AssetDisplayListener {
+
+	/**
+	 * Called when the video is ready to be displayed.
+	 */
+	void onVideoPrepared();
+
+	/**
+	 * Called when there is an error displaying the video.
+	 * @param e exception error
+	 */
+	void onVideoError(Exception e);
+
+	/**
+	 * Called when the video is completed.
+	 */
+	void onVideoCompleted();
+}
