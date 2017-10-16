718be3cfecd26fd54bb0203c98f8f5ececa476d8
Sarai Díaz <dgarciasarai@gmail.com>
2017-10-16 10:08:59 +0200

    LSR-1026 Update Showcase-iOS

--- xamarin/Samples/Showcase-iOS/CustomViews/LoginView_demo.designer.cs
+++ xamarin/Samples/Showcase-iOS/CustomViews/LoginView_demo.designer.cs
@@ -14,5 +14,43 @@ namespace ShowcaseiOS
     [Register ("LoginView_demo")]
     partial class LoginView_demo
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton authorizeButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton loginButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        LiferayScreens.DefaultTextField passwordField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        LiferayScreens.DefaultTextField userNameField { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (authorizeButton != null) {
+                authorizeButton.Dispose ();
+                authorizeButton = null;
+            }
+
+            if (loginButton != null) {
+                loginButton.Dispose ();
+                loginButton = null;
+            }
+
+            if (passwordField != null) {
+                passwordField.Dispose ();
+                passwordField = null;
+            }
+
+            if (userNameField != null) {
+                userNameField.Dispose ();
+                userNameField = null;
+            }
+        }
     }
 }
