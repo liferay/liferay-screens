91a74275e82259492ca692d9b1813a3392551f0b
Javier Gamarra <javier.gamarra@liferay.com>
2018-01-15 12:33:58 +0100

    LSR-1088 move to buildType

---
+++ android/library/src/debug/java/com/liferay/mobile/screens/base/list/BaseListListener.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p/>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.screens.base.list;
+
+import android.view.View;
+import com.liferay.mobile.screens.base.interactor.listener.BaseCacheListener;
+import java.util.List;
+
+/**
+ * @author Javier Gamarra
+ */
+public interface BaseListListener<E> extends BaseCacheListener {
+
+	/**
+	 * Called when the call to retrieve a page of items fails.
+	 *
+	 * @param e exception
+	 */
+	void onListPageFailed(int startRow, Exception e);
+
+	/**
+	 * Called when the call to retrieve a page of items succeed.
+	 */
+	void onListPageReceived(int startRow, int endRow, List<E> entries, int rowCount);
+
+	/**
+	 * Called when a list item is selected.
+	 */
+	void onListItemSelected(E element, View view);
+}
---
+++ android/library/src/debug/java/com/liferay/mobile/screens/comment/list/CommentListListener.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p/>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.screens.comment.list;
+
+import com.liferay.mobile.screens.base.list.BaseListListener;
+import com.liferay.mobile.screens.comment.CommentEntry;
+
+/**
+ * @author Alejandro Hernández
+ */
+public interface CommentListListener extends BaseListListener<CommentEntry> {
+
+	/**
+	 * Called when the screenlet successfully deletes the comment.
+	 */
+	void onDeleteCommentSuccess(CommentEntry commentEntry);
+
+	/**
+	 * Called when the screenlet successfully updates the comment.
+	 */
+	void onUpdateCommentSuccess(CommentEntry commentEntry);
+}
