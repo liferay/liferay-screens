32ce783e269929277db84726bcdbb8a88079686a
Sarai Díaz García <dgarciasarai@gmail.com>
2018-01-25 16:43:05 +0100

    Load forfet in WebScreenlet with selected page

--- xamarin/Samples/AndorraTelecom-iOS/ViewControllers/ForfetViewController.cs
+++ xamarin/Samples/AndorraTelecom-iOS/ViewControllers/ForfetViewController.cs
@@ -1,10 +1,12 @@
 using System;
 using AndorraTelecomiOS.Util;
+using Foundation;
+using LiferayScreens;
 using UIKit;
 
 namespace AndorraTelecomiOS
 {
-    public partial class ForfetViewController : UIViewController
+    public partial class ForfetViewController : UIViewController, IWebScreenletDelegate
     {
         protected ForfetViewController(IntPtr handle) : base(handle) { }
 
@@ -16,7 +18,7 @@ namespace AndorraTelecomiOS
 
             AddLogoToNavigationItem();
 
-            AddMenuChangeLanguage();
+            LoadWebScreenlet();
         }
 
         public void AddLogoToNavigationItem()
@@ -25,10 +27,41 @@ namespace AndorraTelecomiOS
             NavigationItem.TitleView = ImageView;
         }
 
-        public void AddMenuChangeLanguage()
+        public void LoadWebScreenlet()
         {
-            var RightButton = NavItem.AddButtonChangeLanguage(LanguageHelper.ThreeLanguageLetters, this);
-            NavigationItem.SetRightBarButtonItem(RightButton, true);
+            var Url = LanguageHelper.Url(ForfetUrl);
+
+            var config = new WebScreenletConfigurationBuilder(Url)
+                .SetWithWebType(WebType.Other)
+                .AddJsWithLocalFile("js/forfet_js")
+                .AddCssWithLocalFile("css/forfet_css")
+                .Load();
+
+            webScreenlet.ThemeName = "andorra";
+            webScreenlet.Configuration = config;
+            webScreenlet.BackgroundColor = Colors.LightPurple;
+            webScreenlet.Delegate = this;
+            webScreenlet.Load();
+        }
+
+        /* IWebScreenletDelegate */
+
+        [Export("onWebLoad:url:")]
+        public virtual void OnWebLoad(WebScreenlet screenlet, string url)
+        {
+            Console.WriteLine($"WebScreenlet URL display successfully: {url}");
+        }
+
+        [Export("screenlet:onError:")]
+        public virtual void Screenlet(WebScreenlet screenlet, NSError error)
+        {
+            Console.WriteLine($"WebScreenlet URL display failed: {error.DebugDescription}");
+        }
+
+        [Export("screenlet:onScriptMessageNamespace:onScriptMessage:")]
+        public virtual void Screenlet(WebScreenlet screenlet, string namespace_, string Message)
+        {
+            Console.WriteLine($"WebScreenlet onScriptMessage -> namespace: {namespace_}, message: {Message}");
         }
     }
 }
