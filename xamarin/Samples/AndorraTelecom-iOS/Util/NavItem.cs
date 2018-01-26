f36a801719f3571f39dd13d2a91a47709e0cdc52
Sarai Díaz García <dgarciasarai@gmail.com>
2018-01-26 09:18:10 +0100

    Change UIAlertController colors

--- xamarin/Samples/AndorraTelecom-iOS/Util/NavItem.cs
+++ xamarin/Samples/AndorraTelecom-iOS/Util/NavItem.cs
@@ -37,6 +37,8 @@ namespace AndorraTelecomiOS.Util
             LanguageAlert.Title = LanguageBundle.LocalizedString("Language", null);
             LanguageAlert.Message = LanguageBundle.LocalizedString("Choose your preferred language", null);
             LanguageAlert.ModalPresentationStyle = UIModalPresentationStyle.FormSheet;
+            LanguageAlert.View.BackgroundColor = Colors.LightPurple;
+            LanguageAlert.View.TintColor = Colors.DarkPurple;
 
             var AvailableLanguages = LanguageHelper.ListLanguages;
             foreach (var Value in AvailableLanguages)
