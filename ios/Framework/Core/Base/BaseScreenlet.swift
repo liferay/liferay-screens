ab77899fc2286531beec9caf9ea0bd84f5443eab
Sarai Díaz García <dgarciasarai@gmail.com>
2018-02-22 11:46:46 +0100

    LSR-1078 Remove unnecessary SwiftLint line in BaseScreenlet

--- ios/Framework/Core/Base/BaseScreenlet.swift
+++ ios/Framework/Core/Base/BaseScreenlet.swift
@@ -11,7 +11,6 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-// swiftlint:disable superfluous_disable_command file_length
 import UIKit
 import QuartzCore
 
