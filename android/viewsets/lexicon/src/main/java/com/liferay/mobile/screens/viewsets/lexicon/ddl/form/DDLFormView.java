cec5ea5aa3fe4cf9c13ecfe97d420374617cfa2e
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-04-20 17:17:24 -0300

    LPS-79735 Add Edit Text Style

--- android/viewsets/lexicon/src/main/java/com/liferay/mobile/screens/viewsets/lexicon/ddl/form/DDLFormView.java
+++ android/viewsets/lexicon/src/main/java/com/liferay/mobile/screens/viewsets/lexicon/ddl/form/DDLFormView.java
@@ -34,10 +34,10 @@ public class DDLFormView extends com.liferay.mobile.screens.viewsets.defaultview
 		LEXICON_LAYOUT_IDS.put(Field.EditorType.INTEGER, R.layout.ddlfield_number_default);
 		LEXICON_LAYOUT_IDS.put(Field.EditorType.DECIMAL, R.layout.ddlfield_number_default);
 		LEXICON_LAYOUT_IDS.put(Field.EditorType.RADIO, R.layout.ddlfield_radio_default);
-		LEXICON_LAYOUT_IDS.put(Field.EditorType.SELECT, R.layout.ddlfield_text_area_default);
+		LEXICON_LAYOUT_IDS.put(Field.EditorType.SELECT, R.layout.ddlfield_select_default);
 		LEXICON_LAYOUT_IDS.put(Field.EditorType.TEXT, R.layout.ddlfield_text_default);
 		LEXICON_LAYOUT_IDS.put(Field.EditorType.TEXT_AREA, R.layout.ddlfield_text_area_default);
-		LEXICON_LAYOUT_IDS.put(Field.EditorType.DOCUMENT, R.layout.ddlfield_text_area_default);
+		LEXICON_LAYOUT_IDS.put(Field.EditorType.DOCUMENT, R.layout.ddlfield_document_default);
 		LEXICON_LAYOUT_IDS.put(Field.EditorType.GEO, R.layout.ddlfield_geo_default);
 
 	}
