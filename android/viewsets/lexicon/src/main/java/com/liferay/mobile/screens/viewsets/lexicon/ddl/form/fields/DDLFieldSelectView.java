8294656242150c051dcd242d881129523e3d6595
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-04-23 16:23:01 -0300

    LPS-79735 Add Select Option Style

---
+++ android/viewsets/lexicon/src/main/java/com/liferay/mobile/screens/viewsets/lexicon/ddl/form/fields/DDLFieldSelectView.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p/>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.screens.viewsets.lexicon.ddl.form.fields;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.AttributeSet;
+import com.liferay.mobile.screens.ddl.model.StringWithOptionsField;
+import com.liferay.mobile.screens.viewsets.lexicon.R;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Victor Oliveira
+ */
+public class DDLFieldSelectView
+	extends com.liferay.mobile.screens.viewsets.defaultviews.ddl.form.fields.DDLFieldSelectView
+	implements DialogInterface.OnClickListener {
+
+	public DDLFieldSelectView(Context context) {
+		super(context);
+	}
+
+	public DDLFieldSelectView(Context context, AttributeSet attributes) {
+		super(context, attributes);
+	}
+
+	public DDLFieldSelectView(Context context, AttributeSet attributes, int defaultStyle) {
+		super(context, attributes, defaultStyle);
+	}
+
+	@Override
+	protected int getAlertDialogStyle() {
+		return R.style.lexicon_theme_dialog;
+	}
+
+	@Override
+	protected int getAlertDialogLayout() {
+		return R.layout.ddlfield_select_dialog_lexicon;
+	}
+
+	@Override
+	protected void setupSingleChoice(AlertDialog.Builder builder,
+		DialogInterface.OnClickListener selectOptionHandler, String[] labels) {
+
+		List<StringWithOptionsField.Option> availableOptions = getField().getAvailableOptions();
+		ArrayList<StringWithOptionsField.Option> currentValue = getField().getCurrentValue();
+
+		int index = (currentValue.isEmpty()) ? -1: availableOptions.indexOf(currentValue.get(0));
+		builder.setSingleChoiceItems(labels, index, selectOptionHandler);
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		getField().selectOption(getField().getAvailableOptions().get(which));
+		refresh();
+		dialog.dismiss();
+	}
+
+	@Override
+	protected DialogInterface.OnClickListener getAlertDialogListener() {
+		return this;
+	}
+}
