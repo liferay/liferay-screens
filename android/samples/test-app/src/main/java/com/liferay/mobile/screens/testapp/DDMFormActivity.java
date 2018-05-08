3b80753bd27077160e869e56fdf4eff32f4852f4
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-05-08 17:45:49 +0200

    Load Form with ThingScreenlet (missing Parsers)

--- android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/DDMFormActivity.java
+++ android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/DDMFormActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.liferay.mobile.screens.thingscreenlet.screens.ThingScreenlet;
+import com.liferay.mobile.sdk.auth.BasicAuthentication;
+import okhttp3.Credentials;
 
 /**
  * @author Paulo Cruz
@@ -27,7 +29,9 @@ public class DDMFormActivity extends ThemeActivity {
     protected void onResume() {
         super.onResume();
 
-        //screenlet.load();
+        String url = "http://192.168.50.125:8080/o/api/p/form-instance/36582";
+
+        screenlet.load(url, Credentials.basic("test@liferay.com", "test"));
     }
 
     private void initScreenletFromIntent(Intent intent) {
