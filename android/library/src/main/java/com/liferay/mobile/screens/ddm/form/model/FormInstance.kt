4dc09986453ee12fa834d9dbdeb92210b76465b5
Sarai Díaz García <dgarciasarai@gmail.com>
2018-05-09 12:09:30 +0200

    Convert FormInstance to kotlin

---
+++ android/library/src/main/java/com/liferay/mobile/screens/ddm/form/model/FormInstance.kt
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.screens.ddm.form.model
+
+import com.liferay.mobile.screens.R
+import com.liferay.mobile.screens.ddl.model.DDMStructure
+import com.liferay.mobile.screens.thingscreenlet.screens.views.Detail
+import com.liferay.mobile.screens.thingscreenlet.screens.views.Scenario
+
+/**
+ * @author Paulo Cruz
+ * @author Sarai Díaz
+ */
+data class FormInstance(
+    val formInstanceId: Long,
+    val ddmStructure: DDMStructure,
+    val isRequiredAuthentication: Boolean,
+    val isRequiredCaptcha: Boolean,
+    val redirectURL: String?) {
+
+    companion object {
+        val DEFAULT_VIEWS: MutableMap<Scenario, Int> =
+            mutableMapOf(
+                Detail to R.layout.ddm_form_default
+            )
+    }
+}
