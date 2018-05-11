c0f927984d5923e807aed315376db17f018b94b6
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-05-11 13:21:47 +0200

    Render default fields (TextArea and TextField with MOCK)

---
+++ android/library/src/main/java/com/liferay/mobile/screens/ddl/model/Mocker.kt
@@ -0,0 +1,81 @@
+package com.liferay.mobile.screens.ddl.model
+
+
+/**
+ * @author Victor Oliveira
+ */
+
+val mockedJSONStructure = "{\n" +
+    "  \"description\": \"description\",\n" +
+    "  \"name\": \"name\",\n" +
+    "  \"pages\": [{\n" +
+    "    \"headline\": \"headline\",\n" +
+    "    \"text\": \"text\",\n" +
+    "    \"fields\": [{\n" +
+    "      \"isAutocomplete\": false,\n" +
+    "      \"isInline\": false,\n" +
+    "      \"isLocalizable\": false,\n" +
+    "      \"isMultiple\": false,\n" +
+    "      \"isReadOnly\": false,\n" +
+    "      \"isRepeatable\": false,\n" +
+    "      \"isRequired\": false,\n" +
+    "      \"isShowAsSwitcher\": false,\n" +
+    "      \"isShowLabel\": true,\n" +
+    "      \"isTransient\": false,\n" +
+    "      \"label\": \"label\",\n" +
+    "      \"predefinedValue\": \"\",\n" +
+    "      \"tip\": \"tip\",\n" +
+    "      \"dataSourceType\": \"text\",\n" +
+    "      \"dataType\": \"string\",\n" +
+    "      \"name\": \"fields name\",\n" +
+    "      \"placeholder\": \"placeholder\",\n" +
+    "      \"text\": \"field text\"\n" +
+    "      }]\n" +
+    "  }]\n" +
+    "}"
+
+fun getMockMapping(isAutocomplete: Boolean, isInline: Boolean, isLocalizable: Boolean,
+    isMultiple: Boolean, isReadOnly: Boolean, isRepeatable: Boolean, isRequired: Boolean,
+    isShowAsSwitcher: Boolean, isShowLabel: Boolean, isTransient: Boolean, label: String,
+    predefinedValue: String, tip: String, dataSourceType: String, dataType: String,
+    type: String, name: String, placeholder: String, text: String): Map<String, Any> {
+
+    return mapOf(
+        "isAutocomplete" to isAutocomplete,
+        "isInline" to isInline,
+        "isLocalizable" to isLocalizable,
+        "isMultiple" to isMultiple,
+        FormFieldKeys.READ_ONLY to isReadOnly,
+        FormFieldKeys.REPEATABLE to isRepeatable,
+        FormFieldKeys.REQUIRED to isRequired,
+        "isShowAsSwitcher" to isShowAsSwitcher,
+        FormFieldKeys.SHOW_LABEL to isShowLabel,
+        "isTransient" to isTransient,
+        FormFieldKeys.LABEL to label,
+        FormFieldKeys.PREDEFINED_VALUE to predefinedValue,
+        FormFieldKeys.TIP to tip,
+        "dataSourceType" to dataSourceType,
+        "dataType" to dataType,
+        "type" to type,
+        FormFieldKeys.NAME to name,
+        FormFieldKeys.PLACEHOLDER to placeholder,
+        FormFieldKeys.TEXT to text
+    )
+}
+
+fun getMockMapping(dataType: String, type: String, options: List<Map<String, String>> = listOf(),
+    showAsSwitcher: Boolean = false): Map<String, Any> {
+    return mapOf(
+        "dataType" to dataType,
+        "readOnly" to false,
+        "type" to type,
+        "required" to false,
+        "showLabel" to true,
+        "repeatable" to false,
+        "label" to "Label $type",
+        "name" to "Name $type",
+        "tip" to "Tip $type",
+        "placeHolder" to "",
+        "showAsSwitcher" to showAsSwitcher,
+        "options" to options)
+}
--- android/library/src/main/java/com/liferay/mobile/screens/ddm/form/model/FormInstance.kt
+++ android/library/src/main/java/com/liferay/mobile/screens/ddm/form/model/FormInstance.kt
@@ -15,17 +15,13 @@
 package com.liferay.mobile.screens.ddm.form.model
 
 import com.liferay.mobile.screens.R
-import com.liferay.mobile.screens.ddl.model.DDMStructure
-import com.liferay.mobile.screens.ddl.model.Field
-import com.liferay.mobile.screens.ddl.model.FormFieldKeys
-import com.liferay.mobile.screens.ddl.model.StringField
+import com.liferay.mobile.screens.ddl.model.*
 import com.liferay.mobile.screens.thingscreenlet.screens.views.Detail
 import com.liferay.mobile.screens.thingscreenlet.screens.views.Scenario
 import com.liferay.mobile.sdk.apio.model.Thing
 import org.json.JSONArray
 import org.json.JSONObject
 import java.util.*
-import kotlin.collections.ArrayList
 
 /**
  * @author Paulo Cruz
@@ -48,51 +44,22 @@ data class FormInstance @JvmOverloads constructor(
 
         val converter: (Thing) -> Any = { it: Thing ->
 
-            val jsonString = "{\n" +
-                "  \"description\": \"description\",\n" +
-                "  \"name\": \"name\",\n" +
-                "  \"pages\": [{\n" +
-                "    \"headline\": \"headline\",\n" +
-                "    \"text\": \"text\",\n" +
-                "    \"fields\": [{\n" +
-                "      \"isAutocomplete\": false,\n" +
-                "      \"isInline\": false,\n" +
-                "      \"isLocalizable\": false,\n" +
-                "      \"isMultiple\": false,\n" +
-                "      \"isReadOnly\": false,\n" +
-                "      \"isRepeatable\": false,\n" +
-                "      \"isRequired\": false,\n" +
-                "      \"isShowAsSwitcher\": false,\n" +
-                "      \"isShowLabel\": true,\n" +
-                "      \"isTransient\": false,\n" +
-                "      \"label\": \"label\",\n" +
-                "      \"predefinedValue\": \"\",\n" +
-                "      \"tip\": \"tip\",\n" +
-                "      \"additionalType\": \"text\",\n" +
-                "      \"dataType\": \"string\",\n" +
-                "      \"name\": \"fields name\",\n" +
-                "      \"placeholder\": \"placeholder\",\n" +
-                "      \"text\": \"field text\"\n" +
-                "      }]\n" +
-                "  }]\n" +
-                "}"
-
-            val json = JSONObject(jsonString)
+            val json = JSONObject(mockedJSONStructure)
             val formName = json["name"] as String
             val formDescription = json["description"] as String
             val jsonPages = json["pages"] as JSONArray
 
             val pages = mutableListOf<FormPage>()
 
-            for (i in 0..(jsonPages.length() - 1)) {
+            for (i in 0 until jsonPages.length()) {
                 val page = jsonPages.getJSONObject(i)
 
                 val headlinePage = page["headline"] as String
                 val textPage = page["text"] as String
-                val fields = page["fields"] as JSONArray
+                val jsonArrayFields = page["fields"] as JSONArray
 
-                for (j in 0..(fields.length() - 1)) {
-                    val field = fields.getJSONObject(i)
+                for (j in 0 until jsonArrayFields.length()) {
+                    val field = jsonArrayFields.getJSONObject(i)
 
                     val isAutocomplete = field["isAutocomplete"] as Boolean
                     val isInline = field["isInline"] as Boolean
@@ -107,54 +74,30 @@ data class FormInstance @JvmOverloads constructor(
                     val label = field["label"] as String
                     val predefinedValue = field["predefinedValue"] as String
                     val tip = field["tip"] as String
-                    val additionalType = field["additionalType"] as String
-                    val dataType  = field["dataType"] as String
+                    val dataSourceType = field["dataSourceType"] as String
+                    //val dataType  = field["dataType"] as String
                     val name = field["name"] as String
                     val placeholder = field["placeholder"] as String
                     val text = field["text"] as String
 
                     val mock = getMockMapping(isAutocomplete, isInline, isLocalizable, isMultiple, isReadOnly,
                         isRepeatable, isRequired, isShowAsSwitcher, isShowLabel, isTransient, label,
-                        predefinedValue, tip, dataType, additionalType,
+                        predefinedValue, tip, dataSourceType, Field.DataType.STRING.value, Field.EditorType.TEXT.value,
                         name, placeholder, text)
 
                     val stringField = StringField(mock, Locale.ENGLISH, Locale.ENGLISH)
-                    val fields2 = listOf(stringField)
 
-                    pages.add(FormPage(headlinePage, textPage, fields2))
-                    pages.add(FormPage(headlinePage, textPage, fields2))
+                    val fields = mutableListOf<Field<*>>(stringField)
+                    fields.addAll(getAllMockedFields())
+
+                    pages.add(FormPage(headlinePage, textPage, fields))
+                    pages.add(FormPage(headlinePage, textPage, fields))
                 }
             }
-            val ddmStructure = DDMStructure(formName,formDescription, pages)
+            val ddmStructure = DDMStructure(formName, formDescription, pages)
             FormInstance(formName, formDescription, ddmStructure)
-        }
-
-        private fun getMockMapping(isAutocomplete: Boolean, isInline: Boolean, isLocalizable: Boolean,
-            isMultiple: Boolean, isReadOnly: Boolean, isRepeatable: Boolean, isRequired: Boolean,
-            isShowAsSwitcher: Boolean, isShowLabel: Boolean, isTransient: Boolean, label: String,
-            predefinedValue: String, tip: String, dataType: String, type: String, name: String,
-            placeholder: String, text: String): Map<String, Any> {
-
-            return mapOf(
-                "isAutocomplete" to isAutocomplete,
-                "isInline" to isInline,
-                "isLocalizable" to isLocalizable,
-                "isMultiple" to isMultiple,
-                FormFieldKeys.READ_ONLY to isReadOnly,
-                FormFieldKeys.REPEATABLE to isRepeatable,
-                FormFieldKeys.REQUIRED to isRequired,
-                "isShowAsSwitcher" to isShowAsSwitcher,
-                FormFieldKeys.SHOW_LABEL to isShowLabel,
-                "isTransient" to isTransient,
-                FormFieldKeys.LABEL to label,
-                FormFieldKeys.PREDEFINED_VALUE to predefinedValue,
-                FormFieldKeys.TIP to tip,
-                "dataType" to dataType,
-                "type" to type,
-                FormFieldKeys.NAME to name,
-                FormFieldKeys.PLACEHOLDER to placeholder,
-                FormFieldKeys.TEXT to text
-            )
         }
     }
 }
+
+
