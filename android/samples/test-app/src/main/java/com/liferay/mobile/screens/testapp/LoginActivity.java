94a9be441b810998ccf925ddce5366df8b16b697
Victor Galan <victor.galan@liferay.com>
2018-05-31 14:35:30 +0000

    LSR-1107 Add OAuth2 example to test-app

--- android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/LoginActivity.java
+++ android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/LoginActivity.java
@@ -16,17 +16,21 @@ package com.liferay.mobile.screens.testapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import com.liferay.mobile.screens.auth.login.LoginListener;
 import com.liferay.mobile.screens.auth.login.LoginScreenlet;
+import com.liferay.mobile.screens.context.AuthenticationType;
 import com.liferay.mobile.screens.context.User;
 
 /**
  * @author Javier Gamarra
  */
-public class LoginActivity extends ThemeActivity implements LoginListener {
+public class LoginActivity extends ThemeActivity implements LoginListener, View.OnClickListener {
 
 	private LoginScreenlet loginScreenlet;
+	private LoginScreenlet loginScreenletRedirect;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +38,19 @@ public class LoginActivity extends ThemeActivity implements LoginListener {
 		setContentView(R.layout.login);
 
 		loginScreenlet = findViewById(R.id.login_screenlet);
+		loginScreenletRedirect = findViewById(R.id.login_screenlet_redirect);
+
 		loginScreenlet.setListener(this);
+		loginScreenletRedirect.setListener(this);
 
 		setDefaultValues();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		loginScreenlet.resumeOAuth2RedirectFlow(data);
 	}
 
 	@Override
@@ -54,6 +68,36 @@ public class LoginActivity extends ThemeActivity implements LoginListener {
 
 	}
 
+	@Override
+	public void onClick(View v) {
+		int tag = Integer.parseInt(v.getTag().toString());
+		AuthenticationType type = AuthenticationType.values()[tag];
+
+		configureLoginWithAuthType(type);
+	}
+
+	private void configureLoginWithAuthType(AuthenticationType type) {
+
+		boolean isRedirect = AuthenticationType.OAUTH2REDIRECT.equals(type);
+
+		loginScreenlet.setVisibility(isRedirect ? View.GONE : View.VISIBLE);
+		loginScreenletRedirect.setVisibility(isRedirect ? View.VISIBLE : View.GONE);
+
+		switch (type) {
+			case COOKIE:
+				loginScreenlet.setCookieExpirationTime(20);
+				loginScreenlet.setShouldHandleCookieExpiration(true);
+			case OAUTH2REDIRECT:
+				loginScreenletRedirect.setOauth2ClientId(getString(R.string.client_id_redirect));
+				loginScreenletRedirect.setOauth2RedirectUrl(getString(R.string.redirect_uri));
+			case OAUTH2USERNAMEANDPASSWORD:
+				loginScreenlet.setOauth2ClientId(getString(R.string.client_id));
+				loginScreenlet.setOauth2ClientSecret(getString(R.string.client_secret));
+		}
+
+		loginScreenlet.setAuthenticationType(type);
+	}
+
 	private void setDefaultValues() {
 		EditText login = (EditText) loginScreenlet.findViewById(R.id.liferay_login);
 		login.setText(getString(R.string.liferay_default_user_name));
@@ -61,4 +105,5 @@ public class LoginActivity extends ThemeActivity implements LoginListener {
 		EditText password = (EditText) loginScreenlet.findViewById(R.id.liferay_password);
 		password.setText(getString(R.string.liferay_default_password));
 	}
+
 }
