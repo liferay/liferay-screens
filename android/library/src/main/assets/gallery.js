a54f1c44692fda2eee54371f275bde11b078c3e6
Sarai Díaz García <dgarciasarai@gmail.com>
2018-06-05 21:49:58 +0200

    LSR-1123 Fix javascript for image detail (WebScreenlet gallery)

--- android/library/src/main/assets/gallery.js
+++ android/library/src/main/assets/gallery.js
@@ -1,13 +1,18 @@
 function addClickToCards() {
-	var cards = document.getElementsByClassName('card');
+    var cards = document.getElementsByClassName('card');
     var allImgSrc = '';
-	for (var i = 0; i < cards.length; i++) {
-		allImgSrc += cards[i].querySelector('img').src + ',';
-    	cards[i].addEventListener('click', function(event) {
-            var allAndCurrent = allImgSrc + i;
-            event.stopPropagation();
-            window.Screens.postMessage('gallery', allAndCurrent);
-    })}
+    for (var i = 0; i < cards.length; i++) {
+        allImgSrc += cards[i].querySelector('img').src + ',';
+        addEvent(cards[i], i, allImgSrc);
+    }
+}
+
+function addEvent(image, index, allImgSrc) {
+    image.addEventListener('click', function(event) {
+        var allAndCurrent = allImgSrc + index;
+        event.stopPropagation();
+        window.Screens.postMessage('gallery', allAndCurrent);
+    })
 }
 
 addClickToCards();
