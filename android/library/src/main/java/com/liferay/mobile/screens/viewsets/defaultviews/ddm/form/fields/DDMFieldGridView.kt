352d1050af2465c08e9ad748207a6fda9aadb089
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-06-27 12:23:31 -0300

    LPS-82863 Add label and hint to Grid Field

--- android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/ddm/form/fields/DDMFieldGridView.kt
+++ android/library/src/main/java/com/liferay/mobile/screens/viewsets/defaultviews/ddm/form/fields/DDMFieldGridView.kt
@@ -19,11 +19,14 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.liferay.mobile.screens.R
 import com.liferay.mobile.screens.ddl.form.view.DDLFieldViewModel
 import com.liferay.mobile.screens.ddl.model.Option
 import com.liferay.mobile.screens.ddm.form.model.Grid
 import com.liferay.mobile.screens.ddm.form.model.GridField
+import com.liferay.mobile.screens.thingscreenlet.delegates.bindNonNull
+import com.liferay.mobile.screens.viewsets.defaultviews.util.ThemeUtil
 import java.util.*
 
 /**
@@ -33,6 +36,9 @@ open class DDMFieldGridView @JvmOverloads constructor(context: Context, attrs: A
     defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr), DDLFieldViewModel<GridField> {
     private lateinit var field: GridField
     private lateinit var parentView: View
+    private val labelTextView: TextView by bindNonNull(R.id.liferay_ddm_label)
+    private val hintTextView: TextView by bindNonNull(R.id.liferay_ddm_hint)
+    private val gridLinearLayout: LinearLayout by bindNonNull(R.id.liferay_ddm_grid)
 
     override fun getField(): GridField {
         return field
@@ -41,22 +47,42 @@ open class DDMFieldGridView @JvmOverloads constructor(context: Context, attrs: A
     override fun setField(field: GridField) {
         this.field = field
 
+        setupLabelLayout()
         refresh()
     }
 
+    private fun setupLabelLayout() {
+        if (field.isShowLabel && field.label.isNotEmpty()) {
+            labelTextView.text = field.label
+            labelTextView.visibility = View.VISIBLE
+
+            if (this.field.isRequired) {
+                val requiredAlert = ThemeUtil.getRequiredSpannable(context)
+                labelTextView.append(requiredAlert)
+            }
+        }
+
+        if (field.tip.isNotEmpty()) {
+            hintTextView.text = field.tip
+            hintTextView.visibility = View.VISIBLE
+        }
+    }
+
     override fun refresh() {
-        this.removeAllViews()
+        gridLinearLayout.removeAllViews()
 
         field.rows.forEach { row ->
 
             val inflater = LayoutInflater.from(context)
-            val view = inflater.inflate(R.layout.ddmfield_grid_row_default, null, false)
+            val layoutIdentifier = ThemeUtil.getLayoutIdentifier(context, "ddmfield_grid_row")
 
-            this.addView(view)
+            val view = inflater.inflate(layoutIdentifier, gridLinearLayout, false)
+
+            gridLinearLayout.addView(view)
 
             val ddmFieldGridRowView = view as DDMFieldGridRowView
             ddmFieldGridRowView.rowLabelEditText.setText(row.label)
-            ddmFieldGridRowView.setOptions(field.columns as ArrayList<Option>)
+            ddmFieldGridRowView.setOptions(row.label, field.columns as ArrayList<Option>)
 
             ddmFieldGridRowView.columnLabelEditText.setOnValueChangedListener { dialog, which ->
                 val option = field.columns[which]
