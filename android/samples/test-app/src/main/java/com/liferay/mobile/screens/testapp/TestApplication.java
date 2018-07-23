68fb11ee83a960a57370ba18ec0a68203d873f38
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-07-23 11:05:49 -0300

    Fix multidex when API lower than 20

--- android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/TestApplication.java
+++ android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/TestApplication.java
@@ -1,12 +1,12 @@
 package com.liferay.mobile.screens.testapp;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 import com.squareup.leakcanary.LeakCanary;
 
 /**
  * @author Javier Gamarra
  */
-public class TestApplication extends Application {
+public class TestApplication extends MultiDexApplication {
 
 	@Override
 	public void onCreate() {
