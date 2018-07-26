1e37c3f8a6a7132701ea0221ee63adea868ee625
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-07-26 13:26:29 -0300

    LPS-83015 Add Success Page Lexicon Layout

--- android/library/src/main/java/com/liferay/mobile/screens/ddm/form/view/SuccessPageActivity.kt
+++ android/library/src/main/java/com/liferay/mobile/screens/ddm/form/view/SuccessPageActivity.kt
@@ -1,7 +1,10 @@
 package com.liferay.mobile.screens.ddm.form.view
 
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 import android.widget.TextView
 import com.liferay.mobile.screens.R
 import com.liferay.mobile.screens.ddm.form.model.SuccessPage
@@ -9,13 +12,13 @@ import com.liferay.mobile.screens.ddm.form.model.SuccessPage
 /**
  * @author Victor Oliveira
  */
-class SuccessPageActivity: AppCompatActivity() {
+class SuccessPageActivity : AppCompatActivity() {
 
-    private val headlineTextView : TextView by lazy {
+    private val headlineTextView: TextView by lazy {
         findViewById<TextView>(R.id.headline_text_view)
     }
 
-    private val textTextView : TextView by lazy {
+    private val textTextView: TextView by lazy {
         findViewById<TextView>(R.id.text_text_view)
     }
 
@@ -24,6 +27,14 @@ class SuccessPageActivity: AppCompatActivity() {
         setContentView(R.layout.ddm_form_success_page_default)
 
         val successPage = intent.extras["successPage"] as? SuccessPage
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(baseContext, R.color.success_darker_green_default)
+        }
+
+        supportActionBar?.hide()
 
         successPage?.let {
             headlineTextView.text = it.headline
