b56f840e6e58d63b2c11e3a5817450b91c9a7def
Paulo Cruz <paulo.cruz@liferay.com>
2018-10-22 16:46:17 -0300

    LPS-86287 Fix getCustomLayout on ThingScreenlet

--- android/library/src/main/java/com/liferay/mobile/screens/thingscreenlet/screens/ThingScreenlet.kt
+++ android/library/src/main/java/com/liferay/mobile/screens/thingscreenlet/screens/ThingScreenlet.kt
@@ -142,7 +142,11 @@ class ThingScreenlet @JvmOverloads constructor(
 		is Event.Click -> screenletEvents?.onClickEvent(layout as BaseView, event.view, event.thing) as? T
 		is Event.FetchLayout -> {
 			(screenletEvents?.onGetCustomLayout(this, event.view, event.thing, event.scenario)
-				?: layoutIds[event.thing.type[0]]?.get(event.scenario)) as? T
+				?: layoutIds[
+					layoutIds.keys.firstOrNull { key ->
+						event.thing.type.contains(key)
+					}
+				]?.get(event.scenario)) as? T
 		}
 		is Event.CustomEvent -> {
 			screenletEvents?.onCustomEvent(event.name,this, event.view, event.thing) as? T
