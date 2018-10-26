935633abb2a07d445eb63f60115fcbb3f5cc30b5
Sarai Díaz García <dgarciasarai@gmail.com>
2018-10-26 10:02:31 +0200

    Run detektIdeaFormat for css and js files

--- android/samples/westerosemployees-hybrid/src/main/res/raw/detail_js.js
+++ android/samples/westerosemployees-hybrid/src/main/res/raw/detail_js.js
@@ -1,9 +1,9 @@
 function modifyItem() {
-    var ratings = document.querySelectorAll('.rating-label');
-    ratings[0].innerHTML = '';
+  var ratings = document.querySelectorAll('.rating-label');
+  ratings[0].innerHTML = '';
 
-    var contentRatingScore = document.getElementById('zyfa_column1_0_ratingScoreContent');
-    contentRatingScore.appendChild(ratings[1]);
+  var contentRatingScore = document.getElementById('zyfa_column1_0_ratingScoreContent');
+  contentRatingScore.appendChild(ratings[1]);
 }
 
 modifyItem();
--- android/samples/westerosemployees-hybrid/src/main/res/raw/user_profile_portlet_js.js
+++ android/samples/westerosemployees-hybrid/src/main/res/raw/user_profile_portlet_js.js
@@ -1,13 +1,9 @@
 function callOffice() {
-    window.plugins.CallNumber.callNumber(
-        function(result) {
-            console.log('Success:' + result);
-        },
-        function(result) {
-            console.log('Error:' + result);
-        },
-        '900900900',
-        true);
+  window.plugins.CallNumber.callNumber(function (result) {
+    console.log('Success:' + result);
+  }, function (result) {
+    console.log('Error:' + result);
+  }, '900900900', true);
 }
 
 window.callOffice = callOffice;
