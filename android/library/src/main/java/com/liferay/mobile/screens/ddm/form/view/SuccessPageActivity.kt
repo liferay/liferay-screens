56e17d5cad511b72c34fe978b282c6c68cfcfe92
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-11-05 14:39:52 -0300

    LPS-86941 Create SuccessPage activity

---
+++ android/library/src/main/java/com/liferay/mobile/screens/ddm/form/view/SuccessPageActivity.kt
@@ -0,0 +1,45 @@
+package com.liferay.mobile.screens.ddm.form.view
+
+import android.os.Build
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+import android.widget.TextView
+import com.liferay.mobile.screens.R
+import com.liferay.mobile.screens.ddl.form.util.FormConstants
+import com.liferay.mobile.screens.ddm.form.model.SuccessPage
+
+/**
+ * @author Victor Oliveira
+ */
+class SuccessPageActivity : AppCompatActivity() {
+
+	private val headlineTextView: TextView by lazy {
+		findViewById<TextView>(R.id.headline_text_view)
+	}
+
+	private val textTextView: TextView by lazy {
+		findViewById<TextView>(R.id.text_text_view)
+	}
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.ddm_form_success_page_default)
+
+		val successPage = intent.extras[FormConstants.SUCCESS_PAGE] as? SuccessPage
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			val window = window
+			window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+			window.statusBarColor = ContextCompat.getColor(baseContext, R.color.success_darker_green_default)
+		}
+
+		supportActionBar?.hide()
+
+		successPage?.let {
+			headlineTextView.text = it.headline
+			textTextView.text = it.text
+		}
+	}
+}
