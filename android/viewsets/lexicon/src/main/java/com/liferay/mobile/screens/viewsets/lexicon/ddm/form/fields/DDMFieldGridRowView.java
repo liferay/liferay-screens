702b64a001c676ef5982f6579a87d83ffd6c1f8e
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-11-06 09:55:50 -0300

    LPS-86953 Add lexicon Grid view

---
+++ android/viewsets/lexicon/src/main/java/com/liferay/mobile/screens/viewsets/lexicon/ddm/form/fields/DDMFieldGridRowView.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.screens.viewsets.lexicon.ddm.form.fields;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * @author Victor Oliveira
+ */
+public class DDMFieldGridRowView
+    extends com.liferay.mobile.screens.viewsets.defaultviews.ddm.form.fields.DDMFieldGridRowView {
+
+    public DDMFieldGridRowView(Context context) {
+        super(context);
+    }
+
+    public DDMFieldGridRowView(Context context, AttributeSet attributes) {
+        super(context, attributes);
+    }
+
+    public DDMFieldGridRowView(Context context, AttributeSet attributes, int defaultStyle) {
+        super(context, attributes, defaultStyle);
+    }
+}
