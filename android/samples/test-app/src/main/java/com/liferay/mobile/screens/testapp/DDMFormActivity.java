36306f24ee1a49cf673147a0587b2463c85e943a
Paulo Cruz <paulo.cruz@liferay.com>
2018-11-13 11:00:58 -0300

    LPS-86953 Use server_context to store formInstanceId

--- android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/DDMFormActivity.java
+++ android/samples/test-app/src/main/java/com/liferay/mobile/screens/testapp/DDMFormActivity.java
@@ -1,6 +1,5 @@
 package com.liferay.mobile.screens.testapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -17,12 +16,8 @@ import kotlin.jvm.functions.Function1;
  */
 public class DDMFormActivity extends ThemeActivity {
 
-    public static final String FORM_INSTANCE_ID_KEY = "formInstanceId";
-
     private ThingScreenlet screenlet;
     private ModalProgressBarWithLabel modalProgress;
-
-    private long formInstanceId = 36205;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,14 +32,10 @@ public class DDMFormActivity extends ThemeActivity {
         }
     }
 
-    private void initScreenletFromIntent(Intent intent) {
-        if (intent.hasExtra(FORM_INSTANCE_ID_KEY)) {
-            formInstanceId = intent.getLongExtra(FORM_INSTANCE_ID_KEY, 0);
-        }
-    }
-
     private String getResourcePath() {
-        String serverUrl = "http://10.0.2.2:8080";
+        String serverUrl = getResources().getString(R.string.liferay_server);
+        Long formInstanceId = Long.valueOf(getResources().getString(R.string.liferay_form_instance_id));
+
         return serverUrl + String.format(FormConstants.URL_TEMPLATE, formInstanceId);
     }
 
