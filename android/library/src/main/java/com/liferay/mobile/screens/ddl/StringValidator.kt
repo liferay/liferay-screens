fd27e5df27e343565d338ab6a70ae7c54639afdf
Victor Oliveira <victorlaertedoliveira@gmail.com>
2018-12-04 17:25:00 -0300

    MOBILE-1002 Refactor StringValidator to follow same pattern of NumberValidator (companion object)

--- android/library/src/main/java/com/liferay/mobile/screens/ddl/StringValidator.kt
+++ android/library/src/main/java/com/liferay/mobile/screens/ddl/StringValidator.kt
@@ -21,64 +21,65 @@ import android.util.Patterns
 
 abstract class StringValidator(override val errorMessage: String) : Validator {
 	abstract fun validate(string: String): Boolean
-}
 
-class ContainsValidation(val value: String, errorMessage: String) : StringValidator(errorMessage) {
-	override fun validate(string: String): Boolean = string.contains(value)
-}
+	companion object {
+		@JvmStatic
+		fun parseStringValidation(validationMap: Map<String, String>): StringValidator {
+			if (validationMap.isEmpty()) {
+				return DummyValidation()
+			}
 
-class NotContainsValidation(val value: String, errorMessage: String) : StringValidator(errorMessage) {
-	override fun validate(string: String): Boolean = !string.contains(value)
-}
+			val errorMessage = validationMap["errorMessage"] ?: ""
+			val expression = validationMap["expression"] ?: ""
 
-class IsEmailValidation(errorMessage: String) : StringValidator(errorMessage) {
-	override fun validate(string: String): Boolean = Patterns.EMAIL_ADDRESS.matcher(string).matches()
-}
-
-class IsUrlValidation(errorMessage: String) : StringValidator(errorMessage) {
-	override fun validate(string: String): Boolean = Patterns.WEB_URL.matcher(string).matches()
-}
-
-class RegexValidation(val regex: Regex, errorMessage: String) : StringValidator(errorMessage) {
-	override fun validate(string: String): Boolean = regex.matches(string)
-}
-
-class DummyValidation : StringValidator("") {
-	override fun validate(string: String): Boolean = true
-}
-
-class StringValidatorParser {
-
-	fun parseStringValidation(validationMap: Map<String, String>): StringValidator {
-		if (validationMap.isEmpty()) {
-			return DummyValidation()
+			return when {
+				expression.startsWith("isEmailAddress(") -> IsEmailValidation(errorMessage)
+				expression.startsWith("isURL(") -> IsUrlValidation(errorMessage)
+				expression.startsWith("contains(") ->
+					executeRegexAndGetFirstResult("""contains\(\w+, "(\w*)"\)""", expression) {
+						ContainsValidation(it, errorMessage)
+					}
+				expression.startsWith("NOT(contains(") ->
+					executeRegexAndGetFirstResult("""NOT\(contains\(\w+, "(\w*)"\)\)""", expression) {
+						NotContainsValidation(it, errorMessage)
+					}
+				expression.startsWith("match(") ->
+					executeRegexAndGetFirstResult("""match\(\w+, "(\w*)"\)""", expression) {
+						RegexValidation(it.toRegex(), errorMessage)
+					}
+				else -> DummyValidation()
+			}
 		}
 
-		val errorMessage = validationMap["errorMessage"] ?: ""
-		val expression = validationMap["expression"] ?: ""
-
-		return when {
-			expression.startsWith("isEmailAddress(") -> IsEmailValidation(errorMessage)
-			expression.startsWith("isURL(") -> IsUrlValidation(errorMessage)
-			expression.startsWith("contains(") ->
-				executeRegexAndGetFirstResult("""contains\(\w+, "(\w*)"\)""", expression) {
-					ContainsValidation(it, errorMessage)
-				}
-			expression.startsWith("NOT(contains(") ->
-				executeRegexAndGetFirstResult("""NOT\(contains\(\w+, "(\w*)"\)\)""", expression) {
-					NotContainsValidation(it, errorMessage)
-				}
-			expression.startsWith("match(") ->
-				executeRegexAndGetFirstResult("""match\(\w+, "(\w*)"\)""", expression) {
-					RegexValidation(it.toRegex(), errorMessage)
-				}
-			else -> DummyValidation()
+		private fun executeRegexAndGetFirstResult(stringRegex: String, expression: String,
+			and: (String) -> StringValidator): StringValidator {
+			val result = stringRegex.toRegex().find(expression)
+			return result?.groups?.get(1)?.value?.let(and) ?: DummyValidation()
 		}
 	}
 
-	private fun executeRegexAndGetFirstResult(stringRegex: String, expression: String,
-		and: (String) -> StringValidator): StringValidator {
-		val result = stringRegex.toRegex().find(expression)
-		return result?.groups?.get(1)?.value?.let(and) ?: DummyValidation()
+
+	class ContainsValidation(val value: String, errorMessage: String) : StringValidator(errorMessage) {
+		override fun validate(string: String): Boolean = string.contains(value)
+	}
+
+	class NotContainsValidation(val value: String, errorMessage: String) : StringValidator(errorMessage) {
+		override fun validate(string: String): Boolean = !string.contains(value)
+	}
+
+	class IsEmailValidation(errorMessage: String) : StringValidator(errorMessage) {
+		override fun validate(string: String): Boolean = Patterns.EMAIL_ADDRESS.matcher(string).matches()
+	}
+
+	class IsUrlValidation(errorMessage: String) : StringValidator(errorMessage) {
+		override fun validate(string: String): Boolean = Patterns.WEB_URL.matcher(string).matches()
+	}
+
+	class RegexValidation(val regex: Regex, errorMessage: String) : StringValidator(errorMessage) {
+		override fun validate(string: String): Boolean = regex.matches(string)
+	}
+
+	class DummyValidation : StringValidator("") {
+		override fun validate(string: String): Boolean = true
 	}
 }
