89a438946505d962a592faadb84a066be3699238
Marcelo Mello <marcelo.mello@liferay.com>
2018-12-06 13:13:06 -0300

    MOBILE-1001 Parcelable implementation for objects already exists at test-app and code simplification in Scenarios

--- android/library/src/main/java/com/liferay/mobile/screens/thingscreenlet/screens/views/Scenarios.kt
+++ android/library/src/main/java/com/liferay/mobile/screens/thingscreenlet/screens/views/Scenarios.kt
@@ -18,7 +18,7 @@ import android.annotation.SuppressLint
 import android.os.Parcel
 import android.os.Parcelable
 
-interface Scenario : Parcelable {
+interface Scenario : Parcelable{
 	companion object {
 		var stringToScenario: ((String) -> Scenario?)? = null
 	}
@@ -26,12 +26,9 @@ interface Scenario : Parcelable {
 
 @SuppressLint("ParcelCreator")
 object Detail : Scenario {
-	override fun writeToParcel(dest: Parcel?, flags: Int) {
-	}
+	override fun writeToParcel(dest: Parcel?, flags: Int) {}
 
-	override fun describeContents(): Int {
-		return 0
-	}
+    override fun describeContents() = 0
 
 	object CREATOR : Parcelable.Creator<Detail> {
 		override fun createFromParcel(parcel: Parcel): Detail {
@@ -49,9 +46,7 @@ object Row : Scenario {
 	override fun writeToParcel(dest: Parcel?, flags: Int) {
 	}
 
-	override fun describeContents(): Int {
-		return 0
-	}
+	override fun describeContents() = 0
 
 	object CREATOR : Parcelable.Creator<Row> {
 		override fun createFromParcel(parcel: Parcel): Row {
@@ -71,9 +66,7 @@ data class Custom(val name: String) : Scenario {
 		dest?.writeString(name)
 	}
 
-	override fun describeContents(): Int {
-		return 0
-	}
+    override fun describeContents() = 0
 
 	companion object CREATOR : Parcelable.Creator<Custom> {
 		override fun createFromParcel(parcel: Parcel): Custom {
