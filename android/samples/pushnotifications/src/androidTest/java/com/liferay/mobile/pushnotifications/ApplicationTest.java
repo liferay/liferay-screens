ab3d29b8bb2daf485e83519f952cbfa620f6ca24
Paulo Cruz <paulo.cruz@liferay.com>
2019-01-22 18:17:47 -0300

    MOBILE-1081 Fix build problems

--- android/samples/pushnotifications/src/androidTest/java/com/liferay/mobile/pushnotifications/ApplicationTest.java
+++ android/samples/pushnotifications/src/androidTest/java/com/liferay/mobile/pushnotifications/ApplicationTest.java
@@ -1,13 +1,18 @@
 package com.liferay.mobile.pushnotifications;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
-    }
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+	@Test
+	public void test() {
+		// TODO
+	}
+
 }
