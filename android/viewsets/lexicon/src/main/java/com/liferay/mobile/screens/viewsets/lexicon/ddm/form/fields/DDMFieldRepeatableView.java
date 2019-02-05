bb861a3b43301ad9d1a6ddc4db75ff2bbd594861
Victor Oliveira <victorlaertedoliveira@gmail.com>
2019-02-05 12:01:35 -0300

    MOBILE-1121 SF

--- android/viewsets/lexicon/src/main/java/com/liferay/mobile/screens/viewsets/lexicon/ddm/form/fields/DDMFieldRepeatableView.java
+++ android/viewsets/lexicon/src/main/java/com/liferay/mobile/screens/viewsets/lexicon/ddm/form/fields/DDMFieldRepeatableView.java
@@ -1,26 +1,26 @@
 package com.liferay.mobile.screens.viewsets.lexicon.ddm.form.fields;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 
 import com.liferay.mobile.screens.viewsets.lexicon.R;
 
 import com.liferay.mobile.screens.viewsets.lexicon.util.FormViewUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class DDMFieldRepeatableView
     extends com.liferay.mobile.screens.viewsets.defaultviews.ddm.form.fields.DDMFieldRepeatableView {
 
-    public DDMFieldRepeatableView(@NotNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public DDMFieldRepeatableView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
-    public DDMFieldRepeatableView(@NotNull Context context, @Nullable AttributeSet attrs) {
+    public DDMFieldRepeatableView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public DDMFieldRepeatableView(@NotNull Context context) {
+    public DDMFieldRepeatableView(@NonNull Context context) {
         super(context);
     }
 
