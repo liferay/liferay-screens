a53b74ab4ddab6a51e49ce667083eaa849405245
Paulo Cruz <paulo.cruz@liferay.com>
2019-02-05 11:46:59 -0300

    MOBILE-1123 SF

--- android/library/src/test/java/com/liferay/mobile/screens/ddm/GridFieldTest.kt
+++ android/library/src/test/java/com/liferay/mobile/screens/ddm/GridFieldTest.kt
@@ -19,77 +19,77 @@ import java.util.*
 @RunWith(Enclosed::class)
 class GridFieldTest {
 
-    @RunWith(RobolectricTestRunner::class)
-    class WhenPerformingIsValid {
+	@RunWith(RobolectricTestRunner::class)
+	class WhenPerformingIsValid {
 
-        @Test
-        fun shouldReturnTrueWhenTheFieldIsRequiredAndRowsAreCompleted() {
-            createGridField(required = true).apply {
-                currentValue = Grid(mutableMapOf(
-                    "item1" to "medium",
-                    "item2" to "medium",
-                    "item3" to "high"
-                ))
-            }.also {
-                assertTrue(it.isValid)
-            }
-        }
+		@Test
+		fun shouldReturnTrueWhenTheFieldIsRequiredAndRowsAreCompleted() {
+			createGridField(required = true).apply {
+				currentValue = Grid(mutableMapOf(
+					"item1" to "medium",
+					"item2" to "medium",
+					"item3" to "high"
+				))
+			}.also {
+				assertTrue(it.isValid)
+			}
+		}
 
-        @Test
-        fun shouldReturnFalseWhenTheFieldIsRequiredAndRowsAreNotCompleted() {
-            createGridField(required = true).apply {
-                currentValue = Grid(mutableMapOf(
-                    "item1" to "medium",
-                    "item2" to "low"
-                ))
-            }.also {
-                assertFalse(it.isValid)
-            }
-        }
+		@Test
+		fun shouldReturnFalseWhenTheFieldIsRequiredAndRowsAreNotCompleted() {
+			createGridField(required = true).apply {
+				currentValue = Grid(mutableMapOf(
+					"item1" to "medium",
+					"item2" to "low"
+				))
+			}.also {
+				assertFalse(it.isValid)
+			}
+		}
 
-        @Test
-        fun shouldReturnTrueWhenTheFieldIsNotRequiredAndRowsAreCompleted() {
-            createGridField(required = false).apply {
-                currentValue = Grid(mutableMapOf(
-                    "item1" to "medium",
-                    "item2" to "medium",
-                    "item3" to "high"
-                ))
-            }.also {
-                assertTrue(it.isValid)
-            }
-        }
+		@Test
+		fun shouldReturnTrueWhenTheFieldIsNotRequiredAndRowsAreCompleted() {
+			createGridField(required = false).apply {
+				currentValue = Grid(mutableMapOf(
+					"item1" to "medium",
+					"item2" to "medium",
+					"item3" to "high"
+				))
+			}.also {
+				assertTrue(it.isValid)
+			}
+		}
 
-        @Test
-        fun shouldReturnTrueWhenTheFieldIsNotRequiredAndRowsAreNotCompleted() {
-            createGridField(required = false).apply {
-                currentValue = Grid(mutableMapOf(
-                    "item1" to "medium"
-                ))
-            }.also {
-                assertTrue(it.isValid)
-            }
-        }
-    }
+		@Test
+		fun shouldReturnTrueWhenTheFieldIsNotRequiredAndRowsAreNotCompleted() {
+			createGridField(required = false).apply {
+				currentValue = Grid(mutableMapOf(
+					"item1" to "medium"
+				))
+			}.also {
+				assertTrue(it.isValid)
+			}
+		}
+	}
 
-    @Ignore
-    companion object {
-        private fun createGridField(required: Boolean): GridField {
-            return GridField().apply {
-                columns = listOf(
-                    Option("Low", "low", "low"),
-                    Option("Medium", "medium", "medium"),
-                    Option("High", "high", "high")
-                )
+	@Ignore
+	companion object {
+		private fun createGridField(required: Boolean): GridField {
+			return GridField().apply {
+				columns = listOf(
+					Option("Low", "low", "low"),
+					Option("Medium", "medium", "medium"),
+					Option("High", "high", "high")
+				)
 
-                rows = listOf(
-                    Option("Item 1", "item1", "item1"),
-                    Option("Item 2", "item2", "item2"),
-                    Option("Item 3", "item3", "item3")
-                )
+				rows = listOf(
+					Option("Item 1", "item1", "item1"),
+					Option("Item 2", "item2", "item2"),
+					Option("Item 3", "item3", "item3")
+				)
 
-                isRequired = required
-            }
-        }
-    }
+				isRequired = required
+			}
+		}
+	}
 }
